1ccfdfd74439e7f44cdc918c14747c2488cacf6a
Yan Zhao <zhao.y4@northeastern.edu>
2025-11-19 09:47:22 -0800

    Time: 0 ms (100%), Space: 18 MB (31.52%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,13 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        result = 1
+        while n > 0:
+            if n % 2 == 1:   # n 是奇数
+                result *= x
+            x *= x            # x 翻倍
+            n //= 2           # n 减半
+        return result
