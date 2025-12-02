ad2a10e0afc7c1843f8a3b333a1fa15fa2725afc
Yan Zhao <zhao.y4@northeastern.edu>
2025-12-01 17:13:43 -0800

    Time: 0 ms (100%), Space: 17.6 MB (86.12%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,36 +3,29 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n=len(nums)
-        if n<=1:
-            return nums
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
+        if n <= 1:
+            return
         
-        # 倒叙遍历
-        i=n-2
-        flag=True
-        while i>=0:               #[3,2,1] nums[i]=2
-            if nums[i]<nums[i+1]:  
-                break
-            else:
-                if i==0:
-                    flag=False
-            i-=1
-        if not flag:
-            nums.sort()
-            return nums
-
-        #此时i是第一个降点
-        # 找到第一个比降点大得数字
-        if i>=0: 
-            j=n-1            
-            while j>i:     
-                if nums[j]>nums[i]: 
-                    break
-                j-=1
-        #此时得j就是右边第一个比i大的数
-        # 交换降点和比他大得数字
-        nums[i],nums[j]=nums[j],nums[i]
-        #降点原始位置后面升序
-        # 千万不要切片, 切片就会生成新的列表对象!!!!!!!!!!!!!!!!!!!!
+        i = n - 2
+        
+        # 1. 找从右往左第一个降点
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        
+        if i < 0:  # 全递减，如 3 2 1
+            nums.reverse()
+            return
+        
+        # 2. 从右找到第一个比 nums[i] 大的
+        j = n - 1
+        while nums[j] <= nums[i]:
+            j -= 1
+        
+        # 3. 交换
+        nums[i], nums[j] = nums[j], nums[i]
+        
+        # 4. 将 i+1 到末尾反转（保证是最小升序）
         nums[i + 1:] = reversed(nums[i + 1:])
-        return nums
