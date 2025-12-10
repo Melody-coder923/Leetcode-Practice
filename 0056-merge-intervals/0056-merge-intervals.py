1601763597277fc3dc45c104fd15b6760495ddda
Yan Zhao <zhao.y4@northeastern.edu>
2025-12-09 22:09:35 -0800

    Time: 2 ms (93.95%), Space: 21 MB (65.33%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,16 +1,16 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if not intervals:
-            return []
-
+        if len(intervals)<=1:
+            return intervals
         intervals.sort(key=lambda x: x[0])
-        res = [intervals[0]]  # 第一个区间直接加入
-
-        for cur in intervals[1:]:
-            last = res[-1]
-            if cur[0] <= last[1]:  # 有重叠
-                last[1] = max(last[1], cur[1])  # 合并
+        res=[intervals[0]]
+        for i in range(1,len(intervals)):
+            last= res[-1]
+            curr=intervals[i]
+            if curr[0]<=last[1]:
+                last[1]=max(last[1],curr[1])
             else:
-                res.append(cur)  # 无重叠，直接加入
-
+                res.append(curr)
         return res
+            
+         
