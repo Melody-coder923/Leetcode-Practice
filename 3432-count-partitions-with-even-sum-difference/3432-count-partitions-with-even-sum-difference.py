afdcbb8c3c7c3a96953dca1155b3a8b27bec2e25
Yan Zhao <zhao.y4@northeastern.edu>
2025-12-13 21:26:30 -0800

    Time: 0 ms (100%), Space: 17.6 MB (98.79%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.py
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.py
@@ -1,5 +1,6 @@
 class Solution:
     def countPartitions(self, nums: List[int]) -> int:
+        #total 是偶数 → 一定是偶数,total 是奇数 → 一定是奇数,跟 i、left、right 全部无关了
         total = sum(nums)
         n = len(nums)
 
