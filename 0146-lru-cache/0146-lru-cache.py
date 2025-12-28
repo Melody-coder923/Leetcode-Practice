b109a32c42a3aa232bf1c583e0e8b72bbd77db83
Yan Zhao <zhao.y4@northeastern.edu>
2025-12-28 17:10:28 -0500

    Time: 131 ms (63.63%), Space: 77.7 MB (91.19%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -9,11 +9,11 @@ class LRUCache:
     def __init__(self, capacity: int):
         self.capacity=capacity
         self.size=0
-        self.map={}
         self.head=Node(0,0)
         self.tail=Node(0,0)
         self.head.next=self.tail
         self.tail.prev=self.head
+        self.map={}
         
     # 从链表中断开节点（仅断链，不删 map，不改 size）
     def detach(self,node):
@@ -26,21 +26,24 @@ class LRUCache:
     def add(self,node, is_new:bool=True):
         node.next=self.tail
         node.prev=self.tail.prev
+
         self.tail.prev.next=node
         self.tail.prev=node
         if is_new:
             self.size+=1
-        
+      
     # 删除节点（用于超容量删除）
     def del_node(self,node):
         self.detach(node)
         del self.map[node.key]
         self.size-=1
+        
    
     # 移动节点到尾部（最近使用，不改变 size）
     def move_to_tail(self,node):
         self.detach(node)
-        self.add(node, is_new=False)
+        self.add(node,is_new=False)
+
    
     # get 操作
     def get(self,key):
@@ -49,15 +52,17 @@ class LRUCache:
         self.move_to_tail(self.map[key])
         return self.map[key].val
         
-   
     # put 操作
     def put(self,key,value):
         if key in self.map:
-            self.map[key].val=value
             self.move_to_tail(self.map[key])
+            self.map[key].val=value
         else:
-            if self.size == self.capacity:
+            if self.size==self.capacity:
                 self.del_node(self.head.next)
-            new_node = Node(key, value)
-            self.add(new_node, is_new=True)
-            self.map[key] = new_node
+            new_node=Node(key,value)
+            self.add(new_node,is_new=True)
+            self.map[key]=new_node
+            
+
+       
