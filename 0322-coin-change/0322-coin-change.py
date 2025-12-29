7de885d0c3726456ae19bc639f4dc3c19bc6585a
Yan Zhao <zhao.y4@northeastern.edu>
2025-12-29 11:24:32 -0500

    Time: 1381 ms (8.14%), Space: 19.1 MB (30.43%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,11 +1,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        """dp[i] 表示凑出金额 i 所需的最少硬币数"""
-        dp = [float('inf')] * (amount + 1)
-        dp[0] = 0
-
-        for coin in coins:
-            for i in range(coin, amount + 1):
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-
-        return dp[amount] if dp[amount] != float('inf') else -1
+        #物品: coins -i (可重复)
+        #背包: amount -j 
+        m=len(coins)
+        dp= [[float("inf")] * (amount+1) for _ in range(m+1)]
+        for i in range(m+1):
+            dp[i][0]=0
+        
+        for i in range(1,m+1):
+            for j in range(amount+1):
+                dp[i][j]=dp[i-1][j]
+                if j>=coins[i-1] and dp[i][j-coins[i-1]]!=float("inf"):
+                    dp[i][j]=min(dp[i][j],dp[i][j-coins[i-1]]+1)
+        return dp[m][amount] if dp[m][amount]!=float("inf") else -1
