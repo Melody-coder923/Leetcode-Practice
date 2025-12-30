e5ec998805656cdd4e9c901234e94080b341b793
Yan Zhao <zhao.y4@northeastern.edu>
2025-12-30 18:43:03 -0500

    Time: 3 ms (27.87%), Space: 17.3 MB (92.94%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -7,9 +7,9 @@ class Solution:
             return strs[0]
         
         for i in range(len(strs[0])):
-            c=strs[0][i]
-            for char in strs[1:]:
-                if i==len(char) or c!=char[i]:
-                    return char[:i]
-
+            c = strs[0][i]
+            for word in strs[1:]:
+                if i==len(word) or c!=word[i]:
+                     return strs[0][:i]
+        
         return strs[0]
