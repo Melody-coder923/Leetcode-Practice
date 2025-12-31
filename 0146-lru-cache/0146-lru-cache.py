c8995da5aabc2ec4987cc0a3256e7faa329a1957
Yan Zhao <zhao.y4@northeastern.edu>
2025-12-31 00:24:11 -0500

    Time: 92 ms (95.26%), Space: 77.9 MB (82.99%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -5,23 +5,25 @@ class Node:
         self.prev=None
         self.next=None
 
+
 class LRUCache:
     def __init__(self, capacity: int):
-        self.capacity=capacity
-        self.size=0
-        self.head=Node(0,0)
-        self.tail=Node(0,0)
-        self.head.next=self.tail
-        self.tail.prev=self.head
-        self.map={}
+       self.capacity=capacity
+       self.size=0
+       self.head=Node(0,0)
+       self.tail=Node(0,0)
+       self.head.next=self.tail
+       self.tail.prev=self.head
+       self.map={}
+
 
     # 从链表中断开节点（仅断链，不删 map，不改 size）
     def detach(self,node):
-       prev_node=node.prev
-       next_node=node.next
-       prev_node.next=next_node
-       next_node.prev=prev_node
-
+        prev_node=node.prev
+        next_node=node.next
+        prev_node.next=next_node
+        next_node.prev=prev_node
+      
     # 将节点插入到尾部（最近使用）
     def add(self,node, is_new:bool=True):
         node.next=self.tail
@@ -32,33 +34,37 @@ class LRUCache:
 
         if is_new:
             self.size+=1
-
+      
     # 删除节点（用于超容量删除）
     def del_node(self,node):
         self.detach(node)
         del self.map[node.key]
         self.size-=1
-        
+    
    
     # 移动节点到尾部（最近使用，不改变 size）
     def move_to_tail(self,node):
         self.detach(node)
         self.add(node,is_new=False)
-
+        
     # get 操作
     def get(self,key):
         if key in self.map:
             self.move_to_tail(self.map[key])
             return self.map[key].val
-        return -1
+        return -1        
+
     # put 操作
     def put(self,key,value):
         if key in self.map:
-            self.move_to_tail(self.map[key])
             self.map[key].val=value
+            self.move_to_tail(self.map[key])
         else:
             new_node=Node(key,value)
             if self.capacity==self.size:
                 self.del_node(self.head.next)
+            
             self.add(new_node,is_new=True)
             self.map[key]=new_node
+                
+        
