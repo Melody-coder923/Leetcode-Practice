ec73586d63dde08f34d52ec8f77a4c2df7bb4e7e
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-02 07:23:53 -0500

    Time: 11 ms (80.49%), Space: 18.5 MB (80.86%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
@@ -1,17 +1,20 @@
 class Solution:
     def longestMountain(self, arr: List[int]) -> int:
-        # 核心是找山 上升+top+下降 且长度>=3
+        # 核心是形状识别:找山 上升+top+下降 且长度>=3
         n=len(arr)
         res=0
         i=1
         while i<n:
             upper=0
             lower=0
+            #三种状态之一: 平地
             while i<n and arr[i]==arr[i-1]:
                 i+=1
+            #三种状态之一: 上升
             while i<n and arr[i]>arr[i-1]:
                 upper+=1
                 i+=1
+            #三种状态之一: 下降
             while i<n and arr[i]<arr[i-1]:
                 lower+=1
                 i+=1  
