c949f4c50edd46fb6aa13bea126dbc1971c7717d
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-03 03:44:29 -0500

    Time: 4 ms (10.68%), Space: 17.4 MB (90.79%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -2,14 +2,13 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
-        
+
         if len(strs)<=1:
             return strs[0]
         
-        for i in range(len(strs[0])):
-            c = strs[0][i]
-            for word in strs[1:]:
-                if i==len(word) or c!=word[i]:
-                     return strs[0][:i]
-        
+        for i in range(len(strs[0])): #0,1,2
+            char=strs[0][i] # f l o w e r
+            for word in strs[1:]: # flow, flight
+                if i==len(word) or char!=word[i]:
+                    return strs[0][:i]
         return strs[0]
