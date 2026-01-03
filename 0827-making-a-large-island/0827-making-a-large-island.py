011d3b0fb9491c334d25e9d41890bdc9393bd6c2
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-03 00:27:06 -0500

    Time: 815 ms (62.04%), Space: 27.6 MB (77.82%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -1,36 +1,38 @@
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
-        n,m=len(grid),len(grid[0])
-        def dfs(x,y,island_id):
-            if x<0 or y<0 or x>=n or y>=m or grid[x][y]!=1:
+        # step1: 求岛屿面积, 编号
+        n=len(grid)
+        island={} #key: island_id -> 面积
+        def cal(island_id,x,y):
+            if x<0 or x>=n or y<0 or y>=n or grid[x][y]!=1:
                 return 0
             grid[x][y]=island_id
-            return dfs(x+1,y,island_id)+dfs(x-1,y,island_id)+dfs(x,y+1,island_id)+dfs(x,y-1,island_id)+1
-            
-        island = {}
-        island_id = 2
+            return cal(island_id,x+1,y)+cal(island_id,x-1,y)+cal(island_id,x,y+1)+cal(island_id,x,y-1)+1
+
+        island_id=2
         for i in range(n):
-            for j in range(m):
+            for j in range(n):
                 if grid[i][j]==1:
-                    island[island_id] = dfs(i,j,island_id)
-                    island_id += 1
+                    island[island_id]=cal(island_id,i,j)
+                    island_id+=1
 
+        # step2: 以0为中心辐射,看看哪个0连接后岛屿面积最大 -max
         def search(x,y):
+            area=0
             seen=set()
-            total=1
-            for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-                nx, ny = x + dx, y + dy
-                if 0<=nx<n and 0<=ny<m:
-                    island_id = grid[nx][ny]
-                    if island_id > 1 and island_id not in seen:
-                        total+=island[island_id]
+            directions=[(1,0),(-1,0),(0,1),(0,-1)]
+            for dx,dy in directions:
+                nx,ny= x+dx,y+dy
+                if 0<=nx<n and 0<=ny<n:
+                    island_id=grid[nx][ny]
+                    if island_id>1 and island_id not in seen:
+                        area+=island[island_id]
                         seen.add(island_id)
-            return total
-        maxarea = max(island.values() or [0])
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j]==0:
-                    maxarea=max(maxarea,search(i,j))
-        return maxarea
+            return area
 
-        
+        maxarea= max(island.values() or [0])
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j]==0:
+                    maxarea=max(maxarea,search(i,j)+1)
+        return maxarea
