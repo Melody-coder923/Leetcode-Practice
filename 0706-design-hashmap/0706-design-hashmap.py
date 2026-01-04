a2a4b86f23de9e4f7bcd0243f5a020cd376cb072
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-04 13:05:55 -0500

    Time: 30 ms (81.58%), Space: 20.7 MB (84.62%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,34 +1,32 @@
 class MyHashMap:
 
     def __init__(self):
-        self.size = 1000
-        self.buckets = [[] for _ in range(self.size)]
-
+        self.buckets=[[] for _ in range(1000)]
+        
     def put(self, key: int, value: int) -> None:
-        index=key%self.size
-        bucket=self.buckets[index]
-        for pair in bucket:
-            if pair[0] == key:        
-                pair[1] = value 
-                return 
-        bucket.append([key,value])
+        index=key%len(self.buckets)
+        for i, (k, v) in enumerate(self.buckets[index]):
+            if k == key:
+                self.buckets[index][i] = (key, value)
+                return
+        self.buckets[index].append((key, value))
 
     def get(self, key: int) -> int:
-        index=key%self.size
-        bucket=self.buckets[index]
-        for pair in bucket:
-            if pair[0]==key:
-                return pair[1]
-        return -1 
+        index=key%len(self.buckets)
+        for k, v in self.buckets[index]:
+            if k == key:
+                return v
+        return -1
+
 
     def remove(self, key: int) -> None:
-        index=key%self.size
-        bucket=self.buckets[index]
-        for i, pair in enumerate(bucket):
-            if pair[0]==key:
-                bucket.pop(i)
+        index=key%len(self.buckets)
+        for i, (k, v) in enumerate(self.buckets[index]):
+            if k == key:
+                self.buckets[index].pop(i)
                 return
-
+        
+        
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
 # obj.put(key,value)
