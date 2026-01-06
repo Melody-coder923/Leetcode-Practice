25c639095ff9f8fb247fc26cb00eb29b4fbb84e7
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-06 07:20:31 -0800

    Time: 0 ms (100%), Space: 19.5 MB (15.9%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -4,10 +4,12 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n=len(matrix)
+        #先对角线交换 
         for i in range(n):
             for j in range(i+1,n):
                 matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+        #再横向反转
+        for lst in matrix:
+            lst.reverse()
         
-        for row in range(n):
-            matrix[row].reverse()
-        
+            
