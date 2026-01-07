73dcad4184d8396a307c739b6faa4f80bb80e640
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-06 16:34:32 -0800

    Time: 0 ms (100%), Space: 19.5 MB (12.84%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -1,37 +1,42 @@
 class Solution:
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        res = 0
-        for i in range(m - 2):
-            for j in range(n - 2):
-                # 剪枝 1：中心必须是 5
-                if grid[i+1][j+1] != 5:
-                    continue
+        m,n=len(grid),len(grid[0])
+        if m<3 or n<3:
+            return 0
 
-                nums = {
-                    grid[i][j], grid[i][j+1], grid[i][j+2],
-                    grid[i+1][j], grid[i+1][j+1], grid[i+1][j+2],
-                    grid[i+2][j], grid[i+2][j+1], grid[i+2][j+2],
-                }
+        def helper(i,j):
+            # 中间是5
+            if grid[i+1][j+1]!=5:
+                return False
+            
+            # collection set==set(1-9)
+            compare=set(i for i in range(1,10))
+            collection = set([
+                grid[i][j], grid[i][j+1], grid[i][j+2],
+                grid[i+1][j], grid[i+1][j+1], grid[i+1][j+2],
+                grid[i+2][j], grid[i+2][j+1], grid[i+2][j+2]
+            ])     #set里面只能是一个对象
+            if compare != collection:
+                return False
 
-                # 剪枝 2：必须是 1~9
-                if nums != set(range(1, 10)):
-                    continue
+            # 每一行,每一列,每个对角线是15
+            if (
+                grid[i][j] + grid[i+1][j+1] + grid[i+2][j+2] != 15 or
+                grid[i][j+2] + grid[i+1][j+1] + grid[i+2][j] != 15 or
+                grid[i][j] + grid[i][j+1] + grid[i][j+2] != 15 or
+                grid[i+1][j] + grid[i+1][j+1] + grid[i+1][j+2] != 15 or
+                grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2] != 15 or
+                grid[i][j] + grid[i+1][j] + grid[i+2][j] != 15 or
+                grid[i][j+1] + grid[i+1][j+1] + grid[i+2][j+1] != 15 or
+                grid[i][j+2] + grid[i+1][j+2] + grid[i+2][j+2] != 15
+                ): #要加括号
+                return False
+            return True
 
-                # 剪枝 3：检查和
-                if (
-                    grid[i][j] + grid[i][j+1] + grid[i][j+2] == 15 and
-                    grid[i+1][j] + grid[i+1][j+1] + grid[i+1][j+2] == 15 and
-                    grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2] == 15 and
-                    grid[i][j] + grid[i+1][j] + grid[i+2][j] == 15 and
-                    grid[i][j+1] + grid[i+1][j+1] + grid[i+2][j+1] == 15 and
-                    grid[i][j+2] + grid[i+1][j+2] + grid[i+2][j+2] == 15 and
-                    grid[i][j] + grid[i+1][j+1] + grid[i+2][j+2] == 15 and
-                    grid[i][j+2] + grid[i+1][j+1] + grid[i+2][j] == 15
-                ):
-                    res += 1
-
-        return res
-
-
-        
+        count=0
+        for i in range(m-2):
+            for j in range(n-2):
+                if helper(i,j):
+                    count+=1
+        return count
+      
