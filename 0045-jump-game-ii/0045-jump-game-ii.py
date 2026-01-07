dbcb87794970127b9f78b81c3f50bb2f46470543
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-06 17:21:45 -0800

    Time: 7 ms (80.14%), Space: 20.3 MB (5.93%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,19 +1,25 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         """
-            end        ：当前这一步能到的最远边界
-            farthest  ：在当前区间内，下一步能到的最远位置
-            steps     ：已经用了多少步
+        2, 3, 1, 1, 4
+        0  1  2  3  4
+        c     c
+              end
+
+        1. end: 第一范围    step 
+        2. 超过end范围,属于下个范围, 更换end=farest, step+1
+        3. farest: 范围内最远跳的位置 下次end
         """
-        n = len(nums)
-        steps = 0
-        end = 0
-        farthest = 0
 
-        for i in range(n - 1):
-            farthest = max(farthest, i + nums[i])
-            if i == end:
-                steps += 1
-                end = farthest
-
+        end=0
+        farest=0
+        n=len(nums)
+        steps=0
+        for i in range(n-1):
+            farest=max(farest,nums[i]+i)
+            if farest>=n-1:
+                return steps+1
+            if i==end:
+                steps+=1
+                end=farest
         return steps
