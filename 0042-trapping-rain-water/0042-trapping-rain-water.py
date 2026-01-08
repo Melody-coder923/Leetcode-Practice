056217c3491de972ecfa4cf9ac37554c43c83052
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-08 09:19:44 -0800

    Time: 7 ms (87.03%), Space: 21.6 MB (5.03%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,23 +1,22 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        stack = []
-        area = 0
-        #栈中对应的高度是「单调递减」的
-        #栈里放的是“还没等到右墙的柱子”， 一旦遇到更高的柱子，就不断结算它们上面的水。
-        for cur in range(len(height)):
-            #开始接水
-            while stack and height[cur] > height[stack[-1]]:
-                bottom = stack.pop()
+        stack=[0]
+        area=0
+        #高-低-高可以接水 - 单调递减栈, 直到遇到高的开始接水
+        for i in range(1,len(height)):
+           #找到比栈顶更高的了,开始节水
+            while stack and height[stack[-1]]<height[i]:
+                bottom=stack.pop()
                 if not stack:
                     break
-                
-                left = stack[-1]
-                width = cur - left - 1
-                bounded_height = min(height[left], height[cur]) - height[bottom]
-
+                left=stack[-1]
+                width = i - left - 1 #left 和 i 两端柱子本身不能算水槽宽度,只有中间位置才能算,所以是-1
+                #限制高-当前的高度
+                bounded_height = min(height[left], height[i]) - height[bottom]
                 area += width * bounded_height
-
-            stack.append(cur)
-
+            #递减就继续加如栈
+            stack.append(i)
         return area
+                
 
+                
