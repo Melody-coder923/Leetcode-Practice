5a6aa705fec8d3646e5d8036b29ad33379eea687
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-08 21:04:49 -0800

    Time: 55 ms (82.8%), Space: 22.8 MB (17.51%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,35 +1,37 @@
-from collections import defaultdict, deque
+from collections import deque, defaultdict
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # Edge Case: endWord not in wordList -> return 0
         wordList=set(wordList)
         if endWord not in wordList:
             return 0
-        
-        wordLen=len(beginWord)
-
-        # 映射型字典 : *ot: [hot,dot,lot]
-        map = defaultdict(list)
+        # the shortest transformation sequence - BFS level 统计层
+        n=len(beginWord)
+        dic=defaultdict(list)
+       
+        # Step1: beginWord 把每个字母替换为"*" hot-> *ot  h*t ho*
+        # 建立dic[list] ---- key-value:   [ *ot] : hot, dot, lot  key和value都是从wordList来的
         for word in wordList:
-            for i in range(wordLen): 
-                key=word[:i]+"*"+word[i+1:]
-                map[key].append(word)    
-        # BFS 最短路径
+            for i in range(n): 
+                check= word[:i]+"*"+word[i+1:]
+                dic[check].append(word)
 
-        q = deque([(beginWord, 1)])
-        visited=set([beginWord])
+        # Step2: BFS deque=([(beginWord,level)]) +visited
+        q=deque()
+        # tuple (word, level)
+        q.append((beginWord,1))
+        visited=set()
+        visited.add(beginWord)
         while q:
-            word,level=q.popleft()
-            for i in range(wordLen):
-                intermediate= word[:i]+"*"+word[i+1:]
-
-                for neighbour in map[intermediate]:
-                    if neighbour==endWord:
-                        return level+1
-                    if  neighbour not in visited:
-                        visited.add(neighbour)
-                        q.append((neighbour,level+1))
-                map[intermediate]=[]
-
+            word,level=q.popleft() #hot
+            if word==endWord:
+                return level
+            for i in range(n):
+                new_word=word[:i]+"*"+word[i+1:]
+                if new_word in dic:
+                    for neighbour in dic[new_word]:
+                        if neighbour not in visited:
+                            visited.add(neighbour)
+                            q.append((neighbour,level+1))
+                    dic[new_word]=[]
         return 0
-
-    
