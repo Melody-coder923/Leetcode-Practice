a35b365d0c681273c16dd9f6bdd7a024c15d6607
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-10 16:52:18 -0800

    Time: 15 ms (41.8%), Space: 21 MB (6.08%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,32 +1,34 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        n = len(arr)
-        # 1️⃣ 找到 x 的插入位置 idx
-        idx = bisect.bisect_left(arr, x)  # 返回第一个 >= x 的位置
+        res=[]
+        # Step1: 定位x的在arr里面的index
+        # 第一个大于等于x的index, 如果 x 比数组中所有元素都大（在右边，超出数组范围)返回len(arr)
+        idx= bisect.bisect_left(arr,x) 
+        n=len(arr)
+        if idx>=n: 
+            return arr[n-k:]
 
-        # 2️⃣ 初始化左右指针
-        left = idx - 1  # 插入点左边
-        right = idx     # 插入点右边
-
-        res = []
-
-        # 3️⃣ 扩展左右指针，选出 k 个最接近 x 的元素
-        while len(res) < k:
-            if left < 0:  # 左边越界，只能从右边取
+        #Step2: 扩大范围
+        right=idx
+        left=idx-1
+    
+        while len(res)<k: 
+            #base case:
+            if left<0:
                 res.append(arr[right])
-                right += 1
-            elif right >= n:  # 右边越界，只能从左边取
-                res.append(arr[left])
-                left -= 1
-            else:
-                # x 离左边更近或者相等 → 取左边
-                if x - arr[left] <= arr[right] - x:
-                    res.append(arr[left])
-                    left -= 1
-                else:
-                    res.append(arr[right])
-                    right += 1
+                right+=1
+                continue
 
-        # 4️⃣ 最后返回升序结果
-        res.sort()
-        return res
+            if right>=n:
+                res.append(arr[left])
+                left-=1
+                continue
+    
+            #判断距离谁更近,加谁, left x          right
+            if x-arr[left] <= arr[right]-x:
+                res.append(arr[left])
+                left-=1
+            else:
+                res.append(arr[right])
+                right+=1
+        return sorted(res)
