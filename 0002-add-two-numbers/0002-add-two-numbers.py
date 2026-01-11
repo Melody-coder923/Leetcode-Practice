806ddede0094f643f133d90be4505795b501257a
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-10 22:11:44 -0800

    Time: 8 ms (18.69%), Space: 19.5 MB (6.23%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,38 +5,21 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # edge cases
-        if not l1:
-            return l2
-        if not l2:
-            return l1
-        
-        # 2 4 3 5,8
-        # 5 6 4
-        # while 遍历, or 
-        dummy=ListNode(-1) # 新建
-        p=dummy
+        dummy_head = ListNode(0)
+        current = dummy_head
         carry=0
-        while l1 or l2 or carry:
-            if l1: 
-                a=l1.val
-            else:
-                a=0
-            if l2:
-                b=l2.val
-            else:
-                b=0
-            total=a+b+carry # 2+5+0=7, 4+6+0=10, 3+4+1=8, 5
-            carry= total//10 #0 , 1 , 0, 0
-            mod= total%10  # 7 , 0 , 8, 5
-            p.next= ListNode(mod) # dummy-> 7 -> 0-> 8 ->5
-            p=p.next # dummy->7 -> 0-> 8 ->5
-        
-            if l1:  # 5,8
-                l1=l1.next #5
-            if l2:
-                l2=l2.next
-            if carry:
-                p.next=ListNode(carry)
 
-        return dummy.next
+        while l1 or l2 or carry:
+            val1=l1.val if l1 else 0
+            val2=l2.val if l2 else 0
+
+            total=val1+val2+carry
+            carry=total//10
+            digit=total%10
+
+            current.next=ListNode(digit)
+            current=current.next
+
+            if l1: l1=l1.next
+            if l2: l2=l2.next
+        return dummy_head.next 
