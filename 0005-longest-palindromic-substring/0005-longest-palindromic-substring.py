003bcd9345d6d9722497edb739e43a2662047475
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-11 23:52:39 -0800

    Time: 197 ms (94.99%), Space: 19.3 MB (22.17%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,29 +1,25 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # Step1: 判断是不是回文 , 返回回文
-        # Step2: 奇数和偶数分别出发
+        # 最长-> 过程比较取最长 ->满足回文
+        n=len(s)
+        # Step1: 判断是不是回文 function->返回回文的字符串
         def is_Palindrom(left,right):
-            if left<0 or right>=len(s):
+            if left<0 or right>=n:
                 return ""
-            while left>=0 and right<len(s) and s[left]==s[right]:
+            while left>=0 and right<n and s[left]==s[right]:
                 left-=1
                 right+=1
-            #终止条件是 b a b  
-            #          0 1 2
-            #       -1       3
             return s[left+1:right]
+        # Step2: 比较最长 : 情况1:  baab  i,i+1 情况2 bab  i,i
+        maxLen=0
         res=""
-        max_len=float("-inf")
-        for i in range(len(s)):
-            s1=is_Palindrom(i,i)
-            s2=is_Palindrom(i,i+1)
-            if len(s1)>max_len:
+        for i in range(n):
+            s1=is_Palindrom(i,i+1)
+            s2=is_Palindrom(i,i)
+            if len(s1)>maxLen:
+                maxLen=len(s1)
                 res=s1
-                max_len=len(s1)
-            if len(s2)>max_len:
+            if len(s2)>maxLen:
+                maxLen=len(s2)
                 res=s2
-                max_len=len(s2)
         return res
-
-
-        
