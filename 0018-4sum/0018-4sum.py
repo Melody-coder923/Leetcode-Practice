0c6029164d6151c15951b91113bb8fcea36f62b9
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-11 22:57:41 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,36 +1,44 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        # Edge Case:
         n=len(nums)
-        if n<=3:
+        if n<4:
             return []
+
+        # step1: 排序 ->方便指导挪动 
         nums.sort()
         res=[]
-        
-        for i in range(n-3):
-            if i>0 and nums[i]==nums[i-1]:
-                continue
-            if nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target:
+        # step2: 四个数: 先固定一个外循环 i
+        # 2 2 2 2 2 n
+        #   i 
+        #     j 
+        for i in range(n-3):    
+            if i>0 and nums[i]==nums[i-1]:   
+                continue 
+            if nums[0]+nums[1]+nums[2]+nums[3]>target:
                 break
-            if nums[i]+nums[n-1]+nums[n-2]+nums[n-3]<target:
+            if nums[0]+nums[n-1]+nums[n-2]+nums[n-3]<target:
                 continue
-            
-            for j in range(i+1,n-2):
+        #step3: 再固定一个, j=i+1
+            for j in range(i+1,n-2): 
                 if j>i+1 and nums[j]==nums[j-1]:
                     continue
-                left,right=j+1,n-1
+        #step4: while left<right: 朝着target目标, 控制数组大小去移动 
+                left=j+1
+                right=n-1
                 while left<right:
                     total=nums[i]+nums[j]+nums[left]+nums[right]
                     if total==target:
-                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        res.append([nums[i],nums[j],nums[left],nums[right]])
                         while left<right and nums[left]==nums[left+1]:
                             left+=1
                         while left<right and nums[right]==nums[right-1]:
                             right-=1
                         left+=1
                         right-=1
-
                     elif total>target:
                         right-=1
+                    
                     else:
                         left+=1
         return res
