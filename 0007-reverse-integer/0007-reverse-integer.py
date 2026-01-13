2fe969d0475a5c51007404c0dd9826b054eb9977
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-13 08:34:33 -0800

    Time: 41 ms (43.09%), Space: 19.4 MB (8.12%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,15 +1,15 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        #step1: 符号
-        sign = -1 if x < 0 else 1
-        #step2:% 取前面数 ,// 带到下一轮 
-        res=0
-        x=abs(x)
-        while x>0: #123, 12, 1
-            mod=x%10 # 3,2, 1
-            x=x//10 # 12,1, 0
-            res= res*10+mod # 3 , 32, 321
-        res=res*sign
+        sign=-1 if x<0 else 1
+
+        num=abs(x)
+        new_num=0
+        while num>0:
+            keep=num%10 #3,2 , 1
+            num=num//10  # 12,1 0
+            new_num=new_num*10+keep # 3, 2, 1
+        
+        res=new_num*sign 
         if res<-2**31 or res>2**31-1:
             return 0
         return res
