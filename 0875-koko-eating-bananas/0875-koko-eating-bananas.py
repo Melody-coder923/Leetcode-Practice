14853678664a0a738cce866a58642e732f88dce7
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-14 06:02:40 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def canfinish(k):
+            hour=0
+            for pile in piles:     
+                if pile<=k:
+                    hour+=1
+                else:
+                    carry=pile//k
+                    hour+=carry+1
+            return hour<=h
+                    
+
+
+        n=len(piles)
+        r=max(piles)+1#最多吃多少根
+        l=1
+        while l<r:
+            mid=l+(r-l)//2 #1, 12 ->mid=7, 4 , 2, 3
+            if canfinish(mid):
+                r=mid  #r=7 r=4 
+            else:
+                l=mid+1 # l=3, 4
+        return l
+
+
+
+             
+
+
