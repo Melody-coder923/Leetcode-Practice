5d414bd081265db67173ca8ab75eae5ed099f2ef
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-14 22:32:50 -0800

    Time: 49 ms (5.45%), Space: 19.2 MB (10.78%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -3,7 +3,7 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        l, r = 1, n   # 版本从1开始，r开区间所以+1
+        l, r = 1, n+1   # 版本从1开始，r开区间所以+1
     
         while l < r:
             mid = l + (r - l) // 2  # 防溢出
