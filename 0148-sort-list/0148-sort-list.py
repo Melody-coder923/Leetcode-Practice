c71761130cf5ef272184380a0d0dbbccca55c505
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-18 21:32:02 -0800

    Time: 209 ms (22.81%), Space: 40.7 MB (18.43%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -4,49 +4,39 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    # divide conquer
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def mergeList(l1,l2):
+            dummy=ListNode(0)
+            cur=dummy
+            while l1 and l2:
+                if l1.val<=l2.val:
+                    cur.next=l1
+                    cur=cur.next
+                    l1=l1.next
+                else:
+                    cur.next=l2
+                    cur=cur.next
+                    l2=l2.next
+            if l1:
+                cur.next=l1
+            if l2:
+                cur.next=l2
+            return dummy.next
+
+        #base case
         if not head or not head.next:
             return head
-        
+        # divide
         fast=slow=head
-        prev=None
+        prev=None 
         while fast and fast.next:
-            prev=slow
             fast=fast.next.next
+            prev=slow
             slow=slow.next
+        # cut the front and back linkedlist
+        prev.next=None
 
-        if prev:
-            prev.next = None
-        
-        left_head = head
-        right_head = slow
-
-        left=self.sortList(left_head)
-        right=self.sortList(right_head)
-
-        return self.mergeList(left,right)
-
-
-
-    def mergeList(self,left,right):
-        dummy=ListNode(0)
-        cur=dummy
-
-        while left and right:
-            if left.val<=right.val:
-                cur.next=left
-                cur=cur.next
-                left=left.next
-            else:
-                cur.next=right
-                cur=cur.next
-                right=right.next
-        
-        if left:
-            cur.next=left
-        if right:
-            cur.next=right
-        
-        return dummy.next
-
-        
+        left=self.sortList(head)
+        right=self.sortList(slow)
+        return mergeList(left,right)
