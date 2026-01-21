1650a8aac69a2b9a821b280d9a1eeb0a57d9e44e
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-20 17:21:26 -0800

    Time: 3 ms (86.98%), Space: 20.5 MB (23.06%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -2,20 +2,20 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         schedule=defaultdict(list)
         indegree=[0]*numCourses
-        for cur,pre in prerequisites:
+        for pre,cur in prerequisites:
             schedule[pre].append(cur)
             indegree[cur]+=1
         q=deque()
         for i in range(numCourses):
-            if indegree[i] == 0:
+            if indegree[i]==0:
                 q.append(i)
         count=0
         while q:
-            pre=q.popleft()
+            prev= q.popleft()
             count+=1
-            for cur in schedule[pre]:
+            for cur in schedule[prev]:
                 indegree[cur]-=1
                 if indegree[cur]==0:
                     q.append(cur)
-
+                    
         return count==numCourses
