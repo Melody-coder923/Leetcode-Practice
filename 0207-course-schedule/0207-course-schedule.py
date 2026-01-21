d485ebed9712776b1feacdb75010cd5f6066fec8
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-20 23:09:57 -0800

    Time: 10 ms (15.19%), Space: 20.5 MB (23.06%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,21 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Input  0  to numsCourses-1
+        #  prerequisites [[cur,prev]]
+        # Output: finish all courses  count==numsCourse
+        
+        indegree=[0]*numCourses 
         schedule=defaultdict(list)
-        indegree=[0]*numCourses
-        for pre,cur in prerequisites:
-            schedule[pre].append(cur)
+        # 1. indegree to know how many prev for each course
+        # 2. schedule: if one course finish , how many course could reduce one indegree-1
+        for cur,prev in prerequisites:
+            schedule[prev].append(cur)
             indegree[cur]+=1
-        q=deque()
+        
+        q=deque()# prev finish could process
+        # indegree[i]==0
         for i in range(numCourses):
             if indegree[i]==0:
                 q.append(i)
+        
         count=0
         while q:
-            prev= q.popleft()
+            prev=q.popleft()
             count+=1
-            for cur in schedule[prev]:
-                indegree[cur]-=1
-                if indegree[cur]==0:
-                    q.append(cur)
-                    
+            # schedule map  prev-> [course, course,courese]
+            for course in schedule[prev]:
+                indegree[course]-=1
+                if indegree[course]==0:
+                    q.append(course)
+
         return count==numCourses
+
