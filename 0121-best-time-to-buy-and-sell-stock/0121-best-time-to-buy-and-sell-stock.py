2a1e36315262b04cd7647202ffa7b7277c234102
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-24 21:35:35 -0800

    Time: 223 ms (5.32%), Space: 33.2 MB (5.32%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #dp[i]天数[0]不持有[1]持有
-        n=len(prices)
-        if n == 0:
+        # buy and sell one time and can't be at the same day
+        # output: max profit
+        # status: [i] days [j] 0 without 1 hold
+        # buy -prices[i] or sell +prices[i]
+        n=len(prices) 
+        #edge case
+        if n<=1:
             return 0
-        dp = [[0]*2 for _ in range(n)]
+
+        dp= [[0,0] for _ in range(n)]
+
         for i in range(n):
             if i==0:
-                dp[i][0]= 0
-                dp[i][1]= -prices[i]
+                dp[i][0]=0
+                dp[i][1]=-prices[0]
                 continue
-            dp[i][0]=max(dp[i-1][0],dp[i-1][1]+prices[i])
-            dp[i][1]=max(dp[i-1][1],- prices[i])
+            
+            dp[i][0]=max(dp[i-1][0], dp[i-1][1]+prices[i])
+            dp[i][1]=max(dp[i-1][1], -prices[i])
+
         return dp[n-1][0]
