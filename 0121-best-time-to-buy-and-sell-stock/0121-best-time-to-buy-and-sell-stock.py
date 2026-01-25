29d223f7e46f09d8eb03e1a8357a5f68ad32223f
Yan Zhao <zhao.y4@northeastern.edu>
2026-01-24 18:50:49 -0800

    Time: 254 ms (5.32%), Space: 33.1 MB (5.32%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #n 为天数,0 和 1 代表是否持有股票
+        #dp[i]天数[0]不持有[1]持有
         n=len(prices)
+        if n == 0:
+            return 0
         dp = [[0]*2 for _ in range(n)]
         for i in range(n):
-            if i - 1 == -1:
-                dp[i][0] = 0
-                dp[i][1] = -prices[i]
+            if i==0:
+                dp[i][0]= 0
+                dp[i][1]= -prices[i]
                 continue
-            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
-            dp[i][1] = max(dp[i-1][1], -prices[i])
-        return dp[n - 1][0]
+            dp[i][0]=max(dp[i-1][0],dp[i-1][1]+prices[i])
+            dp[i][1]=max(dp[i-1][1],- prices[i])
+        return dp[n-1][0]
