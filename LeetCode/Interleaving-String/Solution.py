f0eb0161d35302d746cbd21a0a340760478b07a0
Yan Zhao <zhao.y4@northeastern.edu>
2026-02-08 22:28:36 -0800

    Add solution for Interleaving String

--- LeetCode/Interleaving-String/Solution.py
+++ LeetCode/Interleaving-String/Solution.py
@@ -1,18 +1,14 @@
 1class Solution:
 2    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
 3        m,n=len(s1),len(s2)
-4        # Pruning
-5        if m+n!=len(s3):
-6            return False
-7        @lru_cache(None)
-8        def dfs(i,j):
-9            if i==0 and j==0:
-10                return True
-11            k=i+j-1
-12            if i>0 and s1[i-1]==s3[k] and dfs(i-1,j):
-13                return True
-14            if j>0 and s2[j-1]==s3[k] and dfs(i,j-1):
-15                return True
-16            return False
-17        return dfs(m,n)
-18
+4        if m+n!=len(s3):
+5            return False
+6        dp= [[False]*(n+1) for _ in range(m+1)]
+7        dp[0][0]=True
+8        for i in range(m+1):
+9            for j in range(n+1):
+10                if i>0 and s1[i-1]==s3[i+j-1]:
+11                    dp[i][j]=dp[i][j] or dp[i-1][j]
+12                if j>0 and s2[j-1]==s3[i+j-1]:
+13                    dp[i][j] = dp[i][j] or dp[i][j-1]
+14        return dp[m][n]
