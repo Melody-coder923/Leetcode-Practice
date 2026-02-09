448b4b63330e30f23e7111b6e6f1a62a3d7eedce
Yan Zhao <zhao.y4@northeastern.edu>
2026-02-08 22:27:35 -0800

    Add solution for Edit Distance

--- LeetCode/Edit-Distance/Solution.py
+++ LeetCode/Edit-Distance/Solution.py
@@ -1,25 +1,27 @@
 1class Solution:
 2    def minDistance(self, word1: str, word2: str) -> int:
-3        m,n=len(word1),len(word2)
-4        #edge case
-5        if not word1:
+3        #Edge case:
+4        m,n= len(word1),len(word2)
+5        if m==0:
 6            return n
-7        if not word2:
+7        if n==0:
 8            return m
-9        @lru_cache(None)
-10        def dfs(i,j):
-11            if i < 0:
-12                return j + 1  
-13            if j < 0:
-14                return i + 1  
-15
-16            if word1[i] == word2[j]:
-17                return dfs(i - 1, j - 1)
-18
-19            # 三种操作都要 +1
-20            replace_cost = dfs(i - 1, j - 1) + 1
-21            delete_cost  = dfs(i - 1, j) + 1
-22            insert_cost  = dfs(i, j - 1) + 1
-23            return min(replace_cost, delete_cost, insert_cost)
-24
-25        return dfs(m - 1, n - 1)
+9
+10        @lru_cache(None)
+11        def dfs(i,j):
+12        #base case:
+13            if j<0:
+14                return i+1
+15            if i<0:
+16                return j+1
+17            
+18            if word1[i]==word2[j]:
+19                return dfs(i-1,j-1)
+20
+21            # word1[i]!=word2[j]
+22            delete=dfs(i-1,j)+1
+23            insert=dfs(i,j-1)+1
+24            replace=dfs(i-1,j-1)+1
+25            return min(delete,insert,replace)
+26
+27        return dfs(m-1,n-1)
