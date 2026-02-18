3a281cf9cd166d8dac22cdb9b7a719a1fe63a856
Yan Zhao <zhao.y4@northeastern.edu>
2026-02-17 22:31:28 -0800

    Add solution for Longest Common Prefix

--- LeetCode/Longest-Common-Prefix/Solution.py
+++ LeetCode/Longest-Common-Prefix/Solution.py
@@ -1,15 +1,13 @@
-1class Solution:
-2    def longestCommonPrefix(self, strs: List[str]) -> str:
-3        # edge case
-4        if not strs:
-5            return ""
-6        n=len(strs)
-7        if n==1:
-8            return strs[0]
-9        # suppose the first one is prefix to short it
-10        for i in range(len(strs[0])):
-11            c = strs[0][i]
-12            for s in strs:
-13                if i == len(s) or s[i] != c:
-14                    return strs[0][:i]
-15        return strs[0]
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        prefix=[]
+        num = len(strs)
+        for x in zip(*strs):
+            if len(set(x)) == 1:
+                prefix.append(x[0])
+            else:
+                break
+        return "".join(prefix) ```
