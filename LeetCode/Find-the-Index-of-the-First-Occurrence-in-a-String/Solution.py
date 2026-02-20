b132a1c2d44beaf01661a8b9d7b21b98c2b6feaa
Yan Zhao <zhao.y4@northeastern.edu>
2026-02-20 11:29:25 -0800

    Add solution for Find the Index of the First Occurrence in a String

--- LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.py
+++ LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.py
@@ -1,11 +1,11 @@
 1class Solution:
 2    def strStr(self, haystack: str, needle: str) -> int:
-3        if needle == "":
+3        if not needle:
 4            return 0
-5        n=len(needle)
-6        f=needle[0]
-7        for i, char in enumerate(haystack):
-8            if char==f:
-9                if haystack[i:i+n]==needle:
-10                    return i
+5        for i in range(len(haystack)-len(needle)+1):
+6            for j in range(len(needle)):
+7                if haystack[i+j]!=needle[j]:
+8                    break
+9            else:
+10                return i
 11        return -1
