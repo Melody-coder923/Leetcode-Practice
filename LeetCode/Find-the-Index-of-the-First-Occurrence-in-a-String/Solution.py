3abb5da0a8bcba753b9ca30ee660c219bbdf1277
Yan Zhao <zhao.y4@northeastern.edu>
2026-02-21 22:48:38 -0800

    Add solution for Find the Index of the First Occurrence in a String

--- LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.py
+++ LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.py
@@ -1,11 +1,32 @@
 1class Solution:
 2    def strStr(self, haystack: str, needle: str) -> int:
-3        if not needle:
-4            return 0
-5        for i in range(len(haystack)-len(needle)+1):
-6            for j in range(len(needle)):
-7                if haystack[i+j]!=needle[j]:
-8                    break
-9            else:
-10                return i
-11        return -1
+3        n=len(haystack)
+4        m=len(needle)
+5        if m==0:
+6            return 0
+7        next_arr = self.buildnext_arr(needle)
+8        j=0
+9        for i in range(n):
+10            while j > 0 and haystack[i] != needle[j]:
+11                j = next_arr[j - 1]
+12            if haystack[i] == needle[j]:
+13                j += 1
+14            if j==m:
+15                return i-m+1
+16        return -1
+17    def buildnext_arr(self, pattern:str) ->List[int]:
+18        m=len(pattern)
+19        next_arr=[0] * m
+20        j=0
+21        for i in range(1,m):
+22            while j > 0 and pattern[i] != pattern[j]:
+23                j = next_arr[j - 1]
+24            if pattern[i] == pattern[j]:
+25                j += 1
+26            next_arr[i] = j
+27        return next_arr
+28    
+29
+30                    
+31
+32
