ff61e8ce7846e245a6e147d2e90902b5401fa943
Yan Zhao <zhao.y4@northeastern.edu>
2026-02-21 22:57:13 -0800

    Add solution for Find the Index of the First Occurrence in a String

--- LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.py
+++ LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.py
@@ -1,32 +1,32 @@
 1class Solution:
 2    def strStr(self, haystack: str, needle: str) -> int:
-3        n=len(haystack)
-4        m=len(needle)
-5        if m==0:
-6            return 0
-7        next_arr = self.buildnext_arr(needle)
-8        j=0
-9        for i in range(n):
-10            while j > 0 and haystack[i] != needle[j]:
-11                j = next_arr[j - 1]
-12            if haystack[i] == needle[j]:
-13                j += 1
-14            if j==m:
-15                return i-m+1
-16        return -1
-17    def buildnext_arr(self, pattern:str) ->List[int]:
-18        m=len(pattern)
-19        next_arr=[0] * m
+3        def buildnext(s):
+4            n=len(s)
+5            nxt=[0]*n
+6            j=0
+7            for i in range(1,n):
+8                while j>0 and s[i]!=s[j]:
+9                    j=nxt[j-1]  
+10                if s[i]==s[j]:   
+11                    j+=1
+12                nxt[i]=j
+13            return nxt
+14        n,m=len(needle),len(haystack)
+15        if n==0:
+16            return 0
+17        if n>m:
+18            return -1
+19        nxt=buildnext(needle)
 20        j=0
-21        for i in range(1,m):
-22            while j > 0 and pattern[i] != pattern[j]:
-23                j = next_arr[j - 1]
-24            if pattern[i] == pattern[j]:
+21        for i in range(m):
+22            while j>0 and haystack[i] != needle[j]:
+23                j=nxt[j-1]
+24            if haystack[i] == needle[j]:
 25                j += 1
-26            next_arr[i] = j
-27        return next_arr
-28    
-29
-30                    
+26            if j==n:
+27                return i-j+1
+28        return -1
+29                
+30            
 31
 32
