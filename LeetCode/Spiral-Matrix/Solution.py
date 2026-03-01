5a9a7e07deda7dc1a70f7d3f7a8762c5faae91e6
Yan Zhao <zhao.y4@northeastern.edu>
2026-02-28 21:39:18 -0800

    Add solution for Spiral Matrix

--- LeetCode/Spiral-Matrix/Solution.py
+++ LeetCode/Spiral-Matrix/Solution.py
@@ -1,29 +1,32 @@
 1class Solution:
 2    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-3        if not matrix or not matrix[0]:
-4            return []
-5
-6        left,right=0,len(matrix[0])-1
-7        top,bottom=0,len(matrix)-1
-8        res=[]
-9        while left<=right and top<=bottom:
-10            for i in range(left,right+1):
-11                res.append(matrix[top][i])
-12            top+=1
-13
-14            for i in range(top,bottom+1):
-15                res.append(matrix[i][right])
-16            right-=1
-17
-18            if top<=bottom:
-19                for i in range(right,left-1,-1):
-20                    res.append(matrix[bottom][i])
-21                bottom-=1
-22            
-23            if left<=right: 
-24                for i in range(bottom, top-1,-1):
-25                    res.append(matrix[i][left])
-26                left+=1
-27
-28        return res           
-29
+3        m,n=len(matrix),len(matrix[0])
+4        if not matrix or not matrix[0]:
+5            return []
+6
+7        res=[]
+8        l,r=0,n-1
+9        top,bottom=0,m-1
+10        while l<=r and top<=bottom:
+11            #l->r
+12            for i in range(l,r+1):
+13                res.append(matrix[top][i])
+14            top+=1
+15            #top->bottom
+16            for i in range(top,bottom+1):
+17                res.append(matrix[i][r])
+18            r-=1
+19            if top<=bottom:
+20            #r->l
+21                for i in range(r,l-1,-1):
+22                    res.append(matrix[bottom][i])
+23                bottom-=1
+24            if l<=r:
+25            #bottom->top
+26                for i in range(bottom,top-1,-1):
+27                    res.append(matrix[i][l])
+28                l+=1
+29        return res
+30      
+31
+32
