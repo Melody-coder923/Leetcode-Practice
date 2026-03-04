2ace68598ab55ba6e67b8da4e8dc6b0f7ac758e3
Yan Zhao <zhao.y4@northeastern.edu>
2026-03-03 20:27:39 -0800

    Add solution for LRU Cache

--- LeetCode/LRU-Cache/Solution.py
+++ LeetCode/LRU-Cache/Solution.py
@@ -1,66 +1,83 @@
-1class Node:
-2    def __init__(self,key,value):
-3        self.key=key
-4        self.val=value
-5        self.prev=None
-6        self.next=None
-7
-8class LRUCache:
-9    def __init__(self, capacity: int):
-10        self.capacity=capacity
-11        self.size=0
-12        self.head=Node(0,0)
-13        self.tail=Node(0,0)
-14        self.head.next=self.tail
-15        self.tail.prev=self.head
-16        self.map={} # key-> node
-17    
-18    # (不常用)head-1-3-4-5-2-tail(常用)
-19    def detach(self,node): #前后断开
-20        prev_node=node.prev
-21        next_node=node.next
-22        prev_node.next=next_node
-23        next_node.prev=prev_node
-24
-25    def move_to_tail(self,node): 
-26        node.next=self.tail
-27        node.prev=self.tail.prev
-28        self.tail.prev.next=node
-29        self.tail.prev=node
-30    
-31    def add(self,node,is_new=False):
-32        self.move_to_tail(node)
-33        if is_new:
-34            self.size+=1
-35
-36    def delete(self,node): #空间满需要删除
-37        self.detach(node)
-38        self.size-=1
-39        
-40    def get(self, key: int) -> int:
-41        if key in self.map:
-42            node=self.map[key]
-43            self.detach(node)
-44            self.move_to_tail(node)
-45            return node.val
-46        return -1
-47    def put(self, key: int, value: int) -> None:
-48        if key in self.map:
-49            node=self.map[key]
-50            node.val=value
-51            self.detach(node)
-52            self.move_to_tail(node)
-53        else:
-54            new_node=Node(key,value)
-55            if self.capacity==self.size:
-56                temp=self.head.next
-57                del self.map[temp.key]
-58                self.delete(temp)
-59            self.map[key] = new_node  
-60            self.add(new_node, is_new=True)
-61
-62                            
-63# Your LRUCache object will be instantiated and called as such:
-64# obj = LRUCache(capacity)
-65# param_1 = obj.get(key)
-66# obj.put(key,value)
+class DListNode {
+public:
+    int key, val;
+    DListNode *prev, *next;
+
+    DListNode(int key = 0, int val = 0, DListNode* prev = nullptr, DListNode* next = nullptr)
+    : key(key), val(val), prev(prev), next(next) {}
+};
+
+class DLinkedList {
+public:
+    int size;
+    DListNode *head, *tail;
+
+    DLinkedList() : size(0) {
+        head = new DListNode(-1, -1);
+        head->next = tail = new DListNode(-1, -1, head);
+    }
+
+    void append(DListNode* node) {
+        tail->prev->next = node;
+        node->prev = tail->prev;
+        node->next = tail;
+        tail->prev = node;
+        size++;
+    }
+
+    void erase(DListNode* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+        size--;
+    }
+
+    ~DLinkedList() {
+        DListNode *curr = head, *cnext;
+        while (curr) {
+            cnext = curr->next;
+            delete curr;
+            curr = cnext;
+        }
+    }
+};
+
+class LRUCache {
+private:
+    int capacity;
+    unordered_map<int, DListNode*> keyNode;
+    DLinkedList list;
+
+public:
+    LRUCache(int capacity) : capacity(capacity) {}
+    
+    int get(int key) {
+        if (keyNode.find(key) == keyNode.end()) {
+            return -1;
+        }
+        DListNode* node = keyNode[key];
+        list.erase(node);
+        list.append(node);
+        return node->val;
+    }
+    
+    void put(int key, int value) {
+        if (keyNode.find(key) != keyNode.end()) {
+            list.erase(keyNode[key]);
+        } else if (list.size == capacity) {
+            DListNode* lru = list.head->next;
+            list.erase(lru);
+            keyNode.erase(lru->key);
+            delete lru;
+        }
+        DListNode* node = new DListNode(key, value);
+        list.append(node);
+        keyNode[key] = node;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
