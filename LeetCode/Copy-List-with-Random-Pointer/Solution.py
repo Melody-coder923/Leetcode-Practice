d5304e9c07e25619d0d9060489d594d79ef98ff2
Yan Zhao <zhao.y4@northeastern.edu>
2026-03-08 13:39:43 -0700

    Add solution for Copy List with Random Pointer

--- LeetCode/Copy-List-with-Random-Pointer/Solution.py
+++ LeetCode/Copy-List-with-Random-Pointer/Solution.py
@@ -9,33 +9,21 @@
 9
 10class Solution:
 11    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-12        # Output: Construct a new linkedlist, val, next, random -> newlinedlist
-13        # [val, random_index]
-14        #Eduge CASE
-15        if not head:
-16            return None
-17        #Step1 Build New node copy origial linkedlist
-18        # map : old node -> new node  NewNode
-19        map={}
-20        cur=head
-21        while cur:
-22            map[cur]=Node(cur.val)
-23            cur=cur.next
-24        
-25        #Step2: build new linkedlist relationship according to old one  Build
-26        cur=head
-27        while cur:
-28            map[cur].next=map.get(cur.next)
-29            map[cur].random=map.get(cur.random)
-30            cur=cur.next
-31        return map[head]
-32
-33
-34
-35
-36
-37
-38
-39        
-40
-41        
+12        if not head:
+13            return None
+14
+15        node_map = {}
+16        cur = head
+17        while cur:
+18            node_map[cur] = Node(cur.val)
+19            cur = cur.next
+20
+21        cur=head
+22        while cur:
+23            if cur.next:
+24                node_map[cur].next = node_map[cur.next]
+25            if cur.random:
+26                node_map[cur].random = node_map[cur.random]
+27            cur = cur.next
+28
+29        return node_map[head]
