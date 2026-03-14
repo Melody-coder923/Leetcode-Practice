63ac5e90c546a03e50cf11eecf84820ff4b8c3a9
Yan Zhao <zhao.y4@northeastern.edu>
2026-03-14 14:59:12 -0700

    Add solution for Construct Binary Tree from Preorder and Inorder Traversal

--- LeetCode/Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/Solution.py
+++ LeetCode/Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/Solution.py
@@ -6,28 +6,14 @@
 6#         self.right = right
 7class Solution:
 8    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-9        #preorder: 根 左 右
-10        #inorder: 左 根 右
-11        # preorder-> root 分界
-12        #root-> inorder-> left and right 
-13        val_idx= {val:i for i,val in enumerate(inorder)}
-14        self.pre_idx=0
-15        def helper(left,right):
-16            if left>right:
-17                return None
-18            val = preorder[self.pre_idx]
-19            self.pre_idx+=1
-20            
-21            root=TreeNode(val)
-22            idx=val_idx[val]
-23
-24            root.left = helper(left, idx - 1)
-25            root.right = helper(idx + 1, right)
-26            return root
-27
-28        return helper(0,len(inorder)-1)
-29
-30
-31
-32
-33
+9        if not preorder or not inorder:
+10            return 
+11        #preoder  root-left -right   -> get root +  build root 
+12        root_val=preorder.pop(0)
+13        root= TreeNode(root_val)
+14        idx= inorder.index(root_val)
+15
+16        root.left = self.buildTree(preorder,inorder[:idx])
+17        root.right= self.buildTree(preorder,inorder[idx+1:])
+18        
+19        return root
