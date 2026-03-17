af1c36cff150d84c19916a71555757ab8d56d57a
Yan Zhao <zhao.y4@northeastern.edu>
2026-03-17 01:41:59 -0700

    Add solution for Word Break II

--- LeetCode/Word-Break-II/Solution.py
+++ LeetCode/Word-Break-II/Solution.py
@@ -1,30 +1,22 @@
 1class Solution:
 2    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-3        #139题升级版, 只要题目要“列出所有方案", 99% 会走 DFS / 回溯
-4        #dfs(i): 返回 s[i:] 可以构成的所有合法句子List[str] +memo
-5        #从字符串下标 i 开始，到字符串结尾，能组成哪些合法的句子？
-6        wordSet = set(wordDict)
-7        memo = {}
-8        def dfs(i):
+3        wordDict=set(wordDict)
+4        memo={}
+5
+6        def dfs(i):
+7            if i==len(s):
+8                return [""]
 9            if i in memo:
 10                return memo[i]
-11            #我已经刚好走到字符串末尾,这是“一条成功路径"
-12            if i==len(s):
-13                return [""]
-14
-15            res = []
-16            for j in range(i + 1, len(s) + 1):
-17                word = s[i:j]
-18                if word in wordSet:
-19                    subs = dfs(j)
-20                    for sub in subs:
-21                        if sub == "":
-22                            res.append(word)
-23                        else:
-24                            res.append(word + " " + sub)
-25                        #隐式回溯:如果凑不上, 选择会被忽略,没有任何东西加入res
-26            memo[i] = res
-27            return res
-28        return dfs(0)
-29        
-30
+11            res=[]
+12            for j in range(i+1,len(s)+1):
+13                prefix=s[i:j]
+14                if prefix in wordDict:
+15                    tmp=dfs(j)
+16                    for word in tmp:
+17                        res.append((prefix+" "+word).strip())
+18            memo[i]=res
+19            return res
+20        return dfs(0)
+21                        
+22                
