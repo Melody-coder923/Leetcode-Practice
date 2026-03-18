9ff10bdd043e908f3c8cddb6b3b90d23c4b7372e
Yan Zhao <zhao.y4@northeastern.edu>
2026-03-18 01:37:01 -0700

    Add solution for Meeting Rooms III

--- LeetCode/Meeting-Rooms-III/Solution.py
+++ LeetCode/Meeting-Rooms-III/Solution.py
@@ -1,35 +1,35 @@
 1import heapq
 2class Solution:
 3    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-4        #按会议开始时间遍历每个会议,释放空闲房间
-5        #分配房间:有空闲房间,无空闲房间
-6        freerooms=[i for i in range(n)] #room_id
-7        heapq.heapify(freerooms)
-8        userooms=[] #(end_time, room_id)
-9        room_count= [0]*n
-10        meetings.sort(key=lambda x: x[0])
-11        for start,end in meetings:
-12            while userooms and userooms[0][0] <= start:
-13                _, room_id = heapq.heappop(userooms)
-14                heapq.heappush(freerooms, room_id)
-15            duration= end-start
-16            if freerooms:
-17                room_id = heapq.heappop(freerooms)
-18                actual_end = start + duration
-19            else:
-20                prev_end, room_id = heapq.heappop(userooms)
-21                actual_end = prev_end + duration  # 延迟会议结束时间
-22            
-23            heapq.heappush(userooms, (actual_end, room_id))
-24            room_count[room_id] += 1
-25        maxcount=0
-26        for i, count in enumerate(room_count):
-27            if count>maxcount:
-28                maxcount=count
-29                res=i
-30        return res
+4        freq=[0] *n
+5        available=list(range(n))
+6        heapq.heapify(available)
+7        occupy=[]
+8        
+9        meetings.sort()
+10        for start,end in meetings:
+11            # 先释放所有已经结束的房间
+12            while occupy and occupy[0][0] <= start:
+13                _, room_id= heapq.heappop(occupy)
+14                heapq.heappush(available, room_id)
+15            
+16            duration= end-start
+17            if available: # 有空闲房间，直接安排
+18                room_id=heapq.heappop(available)
+19                actual_end =end
+20            else: # 没有空闲房间，等最早结束的那个
+21                prev_end, room_id = heapq.heappop(occupy)
+22                actual_end = prev_end + duration  # 延迟会议结束时间
+23            
+24            heapq.heappush(occupy, (actual_end, room_id))
+25            freq[room_id] += 1
+26        maxcount=max(freq)
+27        for i, count in enumerate(freq):
+28            if count == maxcount:
+29                return i
+30        
 31
 32
-33
-34        
+33                    
+34                
 35
