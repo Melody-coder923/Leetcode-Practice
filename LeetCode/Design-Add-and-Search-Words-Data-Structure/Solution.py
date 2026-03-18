08f6ac7dbb27321f089f7df579a15d557f3f94d4
Yan Zhao <zhao.y4@northeastern.edu>
2026-03-18 02:23:07 -0700

    Add solution for Design Add and Search Words Data Structure

--- LeetCode/Design-Add-and-Search-Words-Data-Structure/Solution.py
+++ LeetCode/Design-Add-and-Search-Words-Data-Structure/Solution.py
@@ -1,43 +1,40 @@
-1class TrieNode():
+1class TrieNode:
 2    def __init__(self):
-3        self.children={} # char->node
+3        self.children={}
 4        self.is_end=False
-5# 方便查找-> 存进去 -> container -> tries (node)
-6class WordDictionary:
+5class WordDictionary:
+6
 7    def __init__(self):
 8        self.root= TrieNode()
 9
 10    def addWord(self, word: str) -> None:
-11        cur=self.root #b a d
+11        cur=self.root
 12        for c in word:
 13            if c not in cur.children:
-14                cur.children[c]=TrieNode()
+14                cur.children[c] = TrieNode()
 15            cur=cur.children[c]
 16        cur.is_end=True
-17        
+17
 18    def search(self, word: str) -> bool:
 19        n=len(word)
-20        def helper(j,node):
-21            for i in range(j,n):
-22                char=word[i]
-23                if char==".":  
-24                    for child in node.children.values():
-25                        if helper(i+1,child):
-26                            return True
-27                    return False
-28                else:
-29                    if char not in node.children:
-30                        return False
-31                    node=node.children[char]
-32            return node.is_end
-33                    
-34        return helper(0,self.root)
+20        def helper(i,node):
+21            if i==n:
+22                return node.is_end
+23            c=word[i]
+24            if c==".":
+25                for child in node.children.values():
+26                    if  helper(i+1,child):
+27                        return True
+28                return False
+29            else:
+30                if c not in node.children:
+31                    return False
+32                else:
+33                    return helper(i + 1, node.children[c])
+34        return helper(0, self.root)
 35
-36        
-37        
-38
-39
-40# Your WordDictionary object will be instantiated and called as such:
-41# obj = WordDictionary()
-42# obj.addWord(word)
-43# param_2 = obj.search(word)
+36
+37# Your WordDictionary object will be instantiated and called as such:
+38# obj = WordDictionary()
+39# obj.addWord(word)
+40# param_2 = obj.search(word)
