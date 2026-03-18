21341a1cc80ef303738523a860fa64f0f4699654
Yan Zhao <zhao.y4@northeastern.edu>
2026-03-18 11:26:28 -0700

    Add solution for Climbing Stairs

--- LeetCode/Climbing-Stairs/Solution.py
+++ LeetCode/Climbing-Stairs/Solution.py
@@ -1,8 +1,13 @@
 1class Solution:
 2    def climbStairs(self, n: int) -> int:
-3        if n<=2:
-4            return n
-5        a,b=1,2
-6        for _ in range(3, n + 1):
-7            a, b = b, a + b
-8        return b
+3        memo = {}
+4        def dfs(rest):
+5            if rest < 0:
+6                return 0
+7            if rest == 0:
+8                return 1
+9            if rest in memo:
+10                return memo[rest]
+11            memo[rest] = dfs(rest - 1) + dfs(rest - 2)
+12            return memo[rest]
+13        return dfs(n)
