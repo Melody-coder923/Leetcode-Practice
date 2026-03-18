f921f8cf2824756a5fd488bb52fe25fc3f36ca39
Yan Zhao <zhao.y4@northeastern.edu>
2026-03-18 11:26:58 -0700

    Add solution for Climbing Stairs

--- LeetCode/Climbing-Stairs/Solution.py
+++ LeetCode/Climbing-Stairs/Solution.py
@@ -2,10 +2,8 @@
 2    def climbStairs(self, n: int) -> int:
 3        if n <= 1:
 4            return 1
-5        dp = [0] * (n + 1)
-6        dp[0]=1
-7        dp[1]=1
-8        for i in range(2, n + 1):
-9            dp[i] = dp[i - 1] + dp[i - 2]
-10        return dp[n]
-11       
+5        prev, curr = 1, 1
+6        for i in range(2, n + 1):
+7            prev, curr = curr, prev + curr
+8        return curr
+9       
