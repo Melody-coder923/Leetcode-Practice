35369baaabb3964a3ffd38620bcd7e192cded367
Yan Zhao <zhao.y4@northeastern.edu>
2026-03-31 21:05:03 -0700

    Add solution for Greatest Common Divisor of Strings

--- LeetCode/Greatest-Common-Divisor-of-Strings/Solution.py
+++ LeetCode/Greatest-Common-Divisor-of-Strings/Solution.py
@@ -1,6 +1,10 @@
 1class Solution:
 2    def gcdOfStrings(self, str1: str, str2: str) -> str:
 3        if str1+str2!=str2+str1:
-4            return ""
-5        k = math.gcd(len(str1), len(str2))
-6        return str1[:k]
+4            return""
+5        def gcd(a,b):
+6            while b:
+7                a,b=b,a%b
+8            return a
+9        gcd_length=gcd(len(str1),len(str2))
+10        return str1[:gcd_length]
