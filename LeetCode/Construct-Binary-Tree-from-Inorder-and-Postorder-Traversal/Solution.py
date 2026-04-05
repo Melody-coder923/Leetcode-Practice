8a7e9f9e0f0bc7d8f16432e8a19107f98f8880fd
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-04 22:30:02 -0700

    Add solution for Construct Binary Tree from Inorder and Postorder Traversal

--- LeetCode/Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/Solution.py
+++ LeetCode/Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/Solution.py
@@ -6,20 +6,30 @@
 6#         self.right = right
 7class Solution:
 8    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-9        #inorder: left,root,right
-10        #               idx
-11        #postorder: left,right,root
-12        #root ->root_val= postorder.pop()
-13        # build TreeNode
-14        # idx=inorder.index(root_val)
-15        #root.left=inorder[:idx]
-16        #root.right=inorder[idx+1:]
-17        if not inorder or not postorder:
-18            return
-19        root_val= postorder.pop()
-20        root=TreeNode(root_val)
-21        idx= inorder.index(root_val)
-22        root.right=self.buildTree(inorder[idx+1:],postorder)
-23        root.left=self.buildTree(inorder[:idx],postorder)
-24
-25        return root
+9
+10        # 哈希表存储中序遍历的值到索引的映射
+11        inorder_map = {val: idx for idx, val in enumerate(inorder)}
+12        
+13        def helper(in_left, in_right, post_left, post_right):
+14            # 递归终止条件
+15            if in_left > in_right:
+16                return None
+17            
+18            # 后序遍历的最后一个元素是根节点
+19            root_val = postorder[post_right]
+20            root = TreeNode(root_val)
+21            
+22            # 在中序遍历中找到根节点的位置
+23            root_idx = inorder_map[root_val]
+24            
+25            # 计算左子树的节点数量
+26            left_size = root_idx - in_left
+27            
+28            # 递归构建左子树和右子树
+29            root.left = helper(in_left, root_idx - 1, post_left, post_left + left_size - 1)
+30            root.right = helper(root_idx + 1, in_right, post_left + left_size, post_right - 1)
+31            
+32            return root
+33        
+34        return helper(0, len(inorder) - 1, 0, len(postorder) - 1)
+35
