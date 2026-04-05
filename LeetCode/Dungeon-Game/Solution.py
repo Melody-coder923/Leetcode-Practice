076d3c9271198ea6538b97a3b88164646c993023
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-05 15:31:40 -0700

    Add solution for Dungeon Game

--- LeetCode/Dungeon-Game/Solution.py
+++ LeetCode/Dungeon-Game/Solution.py
@@ -1,18 +1,19 @@
 1class Solution:
 2    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
-3        m,n=len(dungeon),len(dungeon[0])
-4        dp= [[0]*n for _ in range(m)]
-5        dp[m-1][n-1] = max(1, 1 - dungeon[m-1][n-1])
-6        # 最后一列
-7        for i in range(m-2, -1, -1):
-8            dp[i][n-1] = max(1, dp[i+1][n-1] - dungeon[i][n-1])
-9        # 最后一行
-10        for j in range(n-2, -1, -1):
-11            dp[m-1][j] = max(1, dp[m-1][j+1] - dungeon[m-1][j])
-12        
-13        # 其他位置
-14        for i in range(m-2, -1, -1):
-15            for j in range(n-2, -1, -1):
-16                need = min(dp[i+1][j], dp[i][j+1])
-17                dp[i][j] = max(1, need - dungeon[i][j])
-18        return dp[0][0]
+3        # dp[i][j] = 从 (i,j) 出发，到终点能活下去，所需的最少血量
+4        m,n=len(dungeon),len(dungeon[0])
+5        dp= [[0]*n for _ in range(m)]
+6        dp[m-1][n-1] = max(1, 1 - dungeon[m-1][n-1])
+7        # 最后一列
+8        for i in range(m-2, -1, -1):
+9            dp[i][n-1] = max(1, dp[i+1][n-1] - dungeon[i][n-1])
+10        # 最后一行
+11        for j in range(n-2, -1, -1):
+12            dp[m-1][j] = max(1, dp[m-1][j+1] - dungeon[m-1][j])
+13        
+14        # 其他位置
+15        for i in range(m-2, -1, -1):
+16            for j in range(n-2, -1, -1):
+17                need = min(dp[i+1][j], dp[i][j+1])
+18                dp[i][j] = max(1, need - dungeon[i][j])
+19        return dp[0][0]
