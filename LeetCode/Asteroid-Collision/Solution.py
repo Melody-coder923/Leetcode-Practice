914724daf0cbc96dc51a9888dace188ba6854210
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-12 18:58:43 -0700

    Add solution for Asteroid Collision

--- LeetCode/Asteroid-Collision/Solution.py
+++ LeetCode/Asteroid-Collision/Solution.py
@@ -1,17 +1,17 @@
 1class Solution:
 2    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
 3        stack=[]
-4
-5        for s in asteroids:
-6            if s>0:
-7                stack.append(s)
-8            else:
-9                while stack and stack[-1] > 0 and abs(s) > stack[-1]:
-10                    stack.pop()
-11                if stack and stack[-1] > 0 and abs(s) == stack[-1]:
-12                    stack.pop()
-13                elif not stack or stack[-1] < 0:
-14                    stack.append(s)
-15
-16        return stack
-17    
+4        for num in asteroids:
+5            if num<0:
+6                while stack and stack[-1]>0 and stack[-1]<abs(num):
+7                    stack.pop()
+8                if stack and stack[-1]==abs(num):
+9                    stack.pop()
+10                    continue 
+11                if stack and stack[-1]<0:
+12                    stack.append(num)
+13                if not stack:
+14                    stack.append(num)
+15            if num>0:    
+16                stack.append(num)
+17        return stack 
