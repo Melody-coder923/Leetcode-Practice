e74ea3180bea64e3bf23c297651057423e55d66c
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-13 22:02:57 -0700

    Add solution for Odd Even Linked List

--- LeetCode/Odd-Even-Linked-List/Solution.py
+++ LeetCode/Odd-Even-Linked-List/Solution.py
@@ -4,16 +4,29 @@
 4#         self.val = val
 5#         self.next = next
 6class Solution:
-7    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+7    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:  
 8        if not head or not head.next:
-9            return head
+9            return head 
 10        odd=head
 11        even=head.next
-12        even_head=even
+12        connect=even
 13        while even and even.next:
 14            odd.next=even.next
 15            odd=odd.next
-16            even.next=odd.next
-17            even=even.next
-18        odd.next=even_head
-19        return head
+16
+17            even.next=odd.next
+18            even=even.next
+19        
+20        odd.next=connect
+21        return head
+22
+23
+24        # 1     2     3 4 5
+25        # odd.  even
+26        # 1->3 -> 5
+27        #        odd
+28        #connect
+29        # 2->4-> None
+30        #        even
+31
+32
