b9c7c607a139e8c8743f0c6f56e988c5f38588da
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-16 12:51:38 -0700

    Add solution for Reorder Routes to Make All Paths Lead to the City Zero

--- LeetCode/Reorder-Routes-to-Make-All-Paths-Lead-to-the-City-Zero/Solution.py
+++ LeetCode/Reorder-Routes-to-Make-All-Paths-Lead-to-the-City-Zero/Solution.py
@@ -1,17 +1,22 @@
 1class Solution:
 2    def minReorder(self, n: int, connections: List[List[int]]) -> int:
-3        from collections import defaultdict
-4        graph = defaultdict(list)
-5        for a, b in connections:
-6            graph[a].append((b, 1))
-7            graph[b].append((a, 0))
-8        visited = set()
-9        def dfs(node):
-10            visited.add(node)
-11            changes = 0
-12            for neighbor, needs_change in graph[node]:
-13                if neighbor not in visited:
-14                    changes += needs_change
-15                    changes += dfs(neighbor)
+3        graph=defaultdict(list)
+4        for a,b in connections:
+5            graph[a].append((b,1))
+6            graph[b].append((a,0))
+7        visited=set()
+8        def dfs(node):
+9            visited.add(node)
+10            changes=0
+11            visited.add(node)
+12            for nei, need_change in graph[node]:
+13                if nei not in visited:
+14                    changes+=need_change
+15                    changes+=dfs(nei)
 16            return changes
 17        return dfs(0)
+18        
+19
+20
+21
+22
