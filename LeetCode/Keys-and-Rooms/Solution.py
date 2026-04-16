02ab92c0b3234009dec7c32b8604c1ec951bded2
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-16 09:23:01 -0700

    Add solution for Keys and Rooms

--- LeetCode/Keys-and-Rooms/Solution.py
+++ LeetCode/Keys-and-Rooms/Solution.py
@@ -1,11 +1,15 @@
 1class Solution:
 2    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-3        visited=set()
-4        def dfs(key,visited):
-5            if key in visited:
-6                return 
-7            visited.add(key)
-8            for key in rooms[key]:
-9                dfs(key,visited)
-10        dfs(0,visited)
-11        return len(visited)==len(rooms)
+3        n=len(rooms) # how many rooms
+4        visited=set()
+5        #前后有依赖
+6
+7        def dfs(id, visited):
+8            if id in visited:
+9                return
+10            visited.add(id)
+11            for r in rooms[id]:
+12                dfs(r,visited)
+13            return visited
+14        dfs(0,visited)
+15        return len(visited)==len(rooms)
