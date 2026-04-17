2cccefc0c34b320254d67df5ce2fd656f2b2cd14
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-16 21:50:41 -0700

    Add solution for Reorder Routes to Make All Paths Lead to the City Zero

--- LeetCode/Reorder-Routes-to-Make-All-Paths-Lead-to-the-City-Zero/Solution.py
+++ LeetCode/Reorder-Routes-to-Make-All-Paths-Lead-to-the-City-Zero/Solution.py
@@ -1,22 +1,30 @@
 1class Solution:
 2    def minReorder(self, n: int, connections: List[List[int]]) -> int:
-3        graph=defaultdict(list)
-4        for a,b in connections:
-5            graph[a].append((b,1))
-6            graph[b].append((a,0))
-7        visited=set()
-8        def dfs(node):
-9            visited.add(node)
-10            changes=0
-11            visited.add(node)
-12            for nei, need_change in graph[node]:
-13                if nei not in visited:
-14                    changes+=need_change
-15                    changes+=dfs(nei)
-16            return changes
-17        return dfs(0)
-18        
-19
-20
-21
-22
+3        #input: connections a->b , n: how many cities, no cycle
+4        #output: how many edges should be changed in order to ensure all cities could get to zero   
+5        # first: to know the directions +1, 0
+6        #change: -> 
+7        # graph dict key a : value b
+8
+9        graph=defaultdict(list)
+10        for a,b in connections:
+11            graph[a].append((b, 1))
+12            graph[b].append((a,0))
+13        visited=set()
+14        changes=0
+15        def dfs(node,visited): #return changes
+16            nonlocal changes
+17            visited.add(node)
+18            for nei,dis in graph[node]:
+19                if nei not in visited:
+20                    changes+=dis
+21                    dfs(nei,visited)
+22        dfs(0,visited)
+23        return changes
+24
+25        # 0 
+26        #visited {0}
+27        # nei in graph[0]
+28        # 0 -> nei [ddd]
+29
+30
