f7ac4fb8dac761f5dbaec42d679003888deaae85
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-18 21:43:19 -0700

    Add solution for Smallest Number in Infinite Set

--- LeetCode/Smallest-Number-in-Infinite-Set/Solution.py
+++ LeetCode/Smallest-Number-in-Infinite-Set/Solution.py
@@ -1,16 +1,30 @@
-class SmallestInfiniteSet:
-    def __init__(self):
-        self.arr = []
-        heapify(self.arr)
-        self.ceiling = 1
-
-    def popSmallest(self) -> int:
-        if len(self.arr) == 0:
-            self.ceiling += 1
-            return self.ceiling - 1
-        else:
-            return heappop(self.arr)
-
-    def addBack(self, num: int) -> None:
-        if num < self.ceiling:
-            heappush(self.arr, num)
+1import heapq
+2class SmallestInfiniteSet:
+3
+4    def __init__(self):
+5        self.current_num =1
+6        self.min_heap = []
+7        self.heap_elements_set = set()
+8
+9    def popSmallest(self) -> int:
+10        if self.min_heap:
+11            smallest = heapq.heappop(self.min_heap)
+12            self.heap_elements_set.remove(smallest)
+13            return smallest
+14        else:
+15            smallest = self.current_num
+16            self.current_num +=1
+17            return smallest
+18
+19
+20    def addBack(self, num: int) -> None:
+21        if num < self.current_num:
+22            if num not in self.heap_elements_set:
+23                heapq.heappush(self.min_heap, num)
+24                self.heap_elements_set.add(num)   
+25
+26
+27# Your SmallestInfiniteSet object will be instantiated and called as such:
+28# obj = SmallestInfiniteSet()
+29# param_1 = obj.popSmallest()
+30# obj.addBack(num)
