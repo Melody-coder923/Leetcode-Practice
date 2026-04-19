46dad7eb423516be875eed6721ffaf7d95bcbfc4
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-18 21:04:49 -0700

    Add solution for Maximum Subsequence Score

--- LeetCode/Maximum-Subsequence-Score/Solution.py
+++ LeetCode/Maximum-Subsequence-Score/Solution.py
@@ -1,28 +1,17 @@
-1import heapq
-2from typing import List
-3class Solution:
-4    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
-5        pairs = sorted(zip(nums2, nums1), key=lambda x: x[0], reverse=True)
-6        min_heap=[]
-7        curr_sum=0
-8        max_score=0
-9        for min_val, val in pairs:
-10            heapq.heappush(min_heap, val)
-11            curr_sum += val
-12            if len(min_heap) > k:
-13                removed = heapq.heappop(min_heap)
-14                curr_sum -= removed
-15            if len(min_heap) == k:
-16                score = curr_sum * min_val
-17                max_score = max(max_score, score)
-18            
-19        return max_score
-20                
-21                
-22            
-23            
-24            
-25
-26            
-27
-28
+1class Solution:
+2    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+3        # 从大到小排 min_val
+4        pairs = sorted(zip(nums2, nums1), key=lambda x: x[0], reverse=True)
+5        min_heap=[]
+6        cur_sum=0
+7        max_score=0
+8        for min_val, num in pairs:
+9            heapq.heappush(min_heap,num)
+10            cur_sum+=num
+11            if len(min_heap) > k:
+12                removed = heapq.heappop(min_heap)
+13                cur_sum -= removed
+14            
+15            if len(min_heap)==k:
+16                max_score=max(max_score, cur_sum*min_val)
+17        return max_score
