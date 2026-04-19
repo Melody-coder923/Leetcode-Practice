818cbd94d0b6d9df2466c13a1fc2a41b0ceeea24
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-18 22:04:00 -0700

    Add solution for Nearest Exit from Entrance in Maze

--- LeetCode/Nearest-Exit-from-Entrance-in-Maze/Solution.py
+++ LeetCode/Nearest-Exit-from-Entrance-in-Maze/Solution.py
@@ -1,9 +1,18 @@
-0 (start)	|1			|3					|7				|15
-----------------------------------------------------------------
-2			|4,5		|8,9,11				|16,17,19,23	|
-----------------------------------------------------------------
-6			|10,12,13	|18,20,21,24,25,27	|				|
-----------------------------------------------------------------
-14			|22,26,28,29|					|				|
-----------------------------------------------------------------
-30			|			|					|				|
+1class Solution:
+2    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+3        m, n = len(maze), len(maze[0])
+4        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+5        q = deque([(entrance[0], entrance[1], 0)])  # row, col, steps
+6        maze[entrance[0]][entrance[1]] = "+" 
+7        
+8        while q:
+9            x, y, steps = q.popleft()
+10            # 只要当前点在边界，并且不是入口，就是出口
+11            if (x, y) != (entrance[0], entrance[1]) and (x == 0 or x == m - 1 or y == 0 or y == n - 1):
+12                return steps
+13            for dx, dy in directions:
+14                nx, ny = x + dx, y + dy
+15                if 0 <= nx < m and 0 <= ny < n and maze[nx][ny] == ".":
+16                    maze[nx][ny] = "+"
+17                    q.append((nx, ny, steps + 1))
+18        return -1
