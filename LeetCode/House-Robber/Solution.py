ad71988fd7ddc59c4bb8aa7ae2cef094b341d42e
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-24 15:18:14 -0700

    Add solution for House Robber

--- LeetCode/House-Robber/Solution.py
+++ LeetCode/House-Robber/Solution.py
@@ -8,10 +8,8 @@
 8        dp=[0]*n
 9        dp[0]=nums[0]
 10        dp[1] = max(nums[0], nums[1])
-11        
+11
 12        for i in range(2,n):
 13            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
-14        return dp[n-1]
+14        return dp[n - 1]
 15
-16
-17
