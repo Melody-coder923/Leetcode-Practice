243aaf8b4cb1f56ac8656d406df86cc5b7e24995
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-24 14:53:11 -0700

    Add solution for Climbing Stairs

--- LeetCode/Climbing-Stairs/Solution.py
+++ LeetCode/Climbing-Stairs/Solution.py
@@ -1,18 +1,15 @@
 1class Solution:
 2    def climbStairs(self, n: int) -> int:
-3        #edge case
-4        if n<=2:
-5            return n
-6        # 定义：1维/2维，什么是dp[i]
-7        #dp[i]: 到i位置有多少方法 
-8        dp= [0] * (n+1) 
-9        # base case 
-10        a=1
-11        b=2
-12        # update the dp matrix
-13        for i in range(3,n+1):
-14            cur = a + b
-15            a = b
-16            b = cur
-17        return b
-18      
+3        if n<=2:
+4            return n
+5            
+6        @lru_cache(None)
+7        def dfs(n):
+8            #base case
+9            if n<=2:
+10                return n
+11
+12            # break down problems 
+13            return dfs(n-1)+dfs(n-2)
+14
+15        return dfs(n)
