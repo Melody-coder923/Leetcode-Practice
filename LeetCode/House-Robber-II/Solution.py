8d365667a7ecee18787308501256a6255b4954d2
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-29 14:01:47 -0700

    Add solution for House Robber II

--- LeetCode/House-Robber-II/Solution.py
+++ LeetCode/House-Robber-II/Solution.py
@@ -1,28 +1,22 @@
 1class Solution:
 2    def rob(self, nums: List[int]) -> int:
-3        n = len(nums)
-4        if n == 1:
-5            return nums[0]
-6        def helper(arr):
-7            n = len(arr)
-8
-9            if n == 0:
-10                return 0
-11            if n == 1:
-12                return arr[0]
-13            if n == 2:
-14                return max(arr[0], arr[1])
-15
-16            prev_nei = arr[0]
-17            nei = max(arr[0], arr[1])
-18
-19            for i in range(2, n):
-20                curr = max(prev_nei + arr[i], nei)
-21                prev_nei, nei = nei, curr
-22
-23            return curr
-24
-25        return max(
-26            helper(nums[:-1]),
-27            helper(nums[1:])
-28        )
+3        n=len(nums)
+4        #edge case
+5        if n==1:
+6            return nums[0]
+7        if n==2:
+8            return max(nums[0],nums[1])
+9        
+10        def helper(nums):
+11            n=len(nums)
+12            dp=[0]*n
+13
+14            dp[0]=nums[0]
+15            dp[1]=max(nums[1],nums[0])
+16
+17            for i in range(2,n):
+18                dp[i]=max(dp[i-1],dp[i-2]+nums[i])
+19            
+20            return dp[n-1]
+21        
+22        return max(helper(nums[1:]),helper(nums[:-1]))
