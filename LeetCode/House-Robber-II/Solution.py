875a2af0d5ce0e7db81a88244a876580cda006fe
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-29 14:01:51 -0700

    Add solution for House Robber II

--- LeetCode/House-Robber-II/Solution.py
+++ LeetCode/House-Robber-II/Solution.py
@@ -2,21 +2,20 @@
 2    def rob(self, nums: List[int]) -> int:
 3        n=len(nums)
 4        #edge case
-5        if n==1:
+5        if len(nums)==1:
 6            return nums[0]
-7        if n==2:
-8            return max(nums[0],nums[1])
-9        
-10        def helper(nums):
-11            n=len(nums)
-12            dp=[0]*n
-13
-14            dp[0]=nums[0]
-15            dp[1]=max(nums[1],nums[0])
-16
-17            for i in range(2,n):
-18                dp[i]=max(dp[i-1],dp[i-2]+nums[i])
-19            
-20            return dp[n-1]
-21        
-22        return max(helper(nums[1:]),helper(nums[:-1]))
+7        
+8        @lru_cache(None)
+9        def dfs(i,start):
+10            if i<start:
+11                return 0
+12            
+13            return max(
+14                nums[i] + dfs(i - 2, start),
+15                dfs(i - 1, start)
+16            )
+17
+18        return max(
+19            dfs(n - 2, 0),  # 只看 0 到 n-2
+20            dfs(n - 1, 1)   # 只看 1 到 n-1
+21        )
