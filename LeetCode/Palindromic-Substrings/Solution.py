67cb5a59224ee90bd61429726e8577e48d6c669d
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-01 14:11:12 -0700

    Add solution for Palindromic Substrings

--- LeetCode/Palindromic-Substrings/Solution.py
+++ LeetCode/Palindromic-Substrings/Solution.py
@@ -1,24 +1,21 @@
 1class Solution:
 2    def countSubstrings(self, s: str) -> int:
-3        n = len(s)
-4        count = 0
-5
-6        # 遍历字符串中的每一个字符，将其作为中心
+3        n=len(s)
+4        if n==1:
+5            return 1
+6        count=0
 7        for i in range(n):
-8            # --- 情况 1: 回文串长度为奇数 ---
-9            # 中心是单个字符 s[i]
-10            l, r = i, i
-11            while l >= 0 and r < n and s[l] == s[r]:
-12                count += 1  # 发现一个回文串
-13                l -= 1      # 向左扩展
-14                r += 1      # 向右扩展
-15
-16            # --- 情况 2: 回文串长度为偶数 ---
-17            # 中心是两个字符 s[i] 和 s[i+1]
-18            l, r = i, i + 1
-19            while l >= 0 and r < n and s[l] == s[r]:
-20                count += 1  # 发现一个回文串
-21                l -= 1      # 向左扩展
-22                r += 1      # 向右扩展
-23        
-24        return count
+8            l,r=i,i
+9            while l>=0 and r<n and s[l]==s[r]:
+10                count+=1
+11                l-=1
+12                r+=1
+13            
+14            l,r=i,i+1
+15            while l>=0 and r<n and s[l]==s[r]:
+16                count+=1
+17                l-=1
+18                r+=1
+19        
+20        return count
+21
