0a22b4f3d284ae6b321df96349e54b9a567f2ed3
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-01 15:15:03 -0700

    Add solution for Decode Ways

--- LeetCode/Decode-Ways/Solution.py
+++ LeetCode/Decode-Ways/Solution.py
@@ -3,14 +3,18 @@
 3        n=len(s)
 4        @lru_cache(None)
 5        def dfs(i):
-6            if i<0:
-7                return 1
+6            if i == 0:
+7                return 1 if s[0] != "0" else 0
 8            res=0
 9            if s[i]!="0":
 10                res+=dfs(i-1)
 11            
-12            if i-1>=0 and 10<= int(s[i-1:i+1]) <= 26:
-13                res+=dfs(i-2)
-14            return res
-15        return dfs(n-1)
-16            
+12            if i - 1 >= 0 and 10 <= int(s[i-1:i+1]) <= 26:
+13                if i == 1:
+14                    res += 1
+15                else:
+16                    res += dfs(i - 2)
+17
+18            return res
+19        return dfs(n-1)
+20            
