a9d28a788e7e86c2fa69a173c133769d57f3a8d1
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-30 20:24:49 -0700

    Add solution for Word Search

--- LeetCode/Word-Search/Solution.py
+++ LeetCode/Word-Search/Solution.py
@@ -1,25 +1,28 @@
 1class Solution:
 2    def exist(self, board: List[List[str]], word: str) -> bool:
-3        m,n=len(board),len(board[0])
-4        directions=[(-1,0),(0,1),(1,0),(0,-1)]
-5        def dfs(i,j,idx):
-6            if word[idx]!=board[i][j]:
-7                return False
-8            if idx==len(word)-1:
-9                return True
-10            temp=board[i][j]
-11            board[i][j]=1
-12            for dx,dy in directions:
-13                nx,ny=dx+i,dy+j
-14                if 0<=nx<m and 0<=ny<n and board[nx][ny]!=1:
-15                    if dfs(nx,ny,idx+1):
-16                        return True
-17            board[i][j]=temp
-18            return False
-19        idx=0
-20        for i in range(m):
-21            for j in range(n):
-22                if board[i][j]==word[idx]:
-23                    if dfs(i,j,idx):
-24                        return True
-25        return False
+3        directions=[(0,1),(0,-1),(1,0),(-1,0)]
+4        m,n= len(board),len(board[0])
+5        
+6
+7        def dfs(i,j,k):
+8            if i<0 or j<0 or i>=m or j>=n or board[i][j]==1 or board[i][j]!=word[k]:
+9                return False
+10            
+11            if k==len(word)-1:
+12                return True
+13
+14            temp=board[i][j]
+15            board[i][j]=1
+16            for dx,dy in directions:
+17                if dfs(i+dx,j+dy,k+1):
+18                    return True
+19            board[i][j]=temp
+20            return False
+21
+22        k=0
+23        for i in range(m):
+24            for j in range(n):
+25                if board[i][j]==word[k]:
+26                    if dfs(i,j,k):
+27                        return True
+28        return False
