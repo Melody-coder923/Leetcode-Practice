0341b8c45c371264580f29f0b96e9e74c79a34d8
Yan Zhao <zhao.y4@northeastern.edu>
2026-04-30 20:24:27 -0700

    Add solution for Word Search

--- LeetCode/Word-Search/Solution.py
+++ LeetCode/Word-Search/Solution.py
@@ -1,28 +1,28 @@
 1class Solution:
 2    def exist(self, board: List[List[str]], word: str) -> bool:
-3        if not board or not board[0] or not word:
-4            return False
-5        m, n = len(board), len(board[0])
-6        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-7
-8        def dfs(x, y, index):
-9            if index == len(word):
-10                return True
-11            if (x < 0 or x >= len(board) or y < 0 or y >= len(board[0]) or
-12        board[x][y] != word[index] or board[x][y] == '#'):
-13                return False
-14            temp = board[x][y]
-15            board[x][y] = '#'
-16            directions = [(-1,0), (1,0), (0,-1), (0,1)]
-17            for dx, dy in directions:
-18                if dfs(x + dx, y + dy, index + 1):
-19                    return True
-20            board[x][y] = temp
-21            return False
-22
-23        for i in range(len(board)):
-24            for j in range(len(board[0])):
-25                if board[i][j] == word[0]:  # 找到首字母
-26                    if dfs(i, j, 0):  # 从这里开始DFS
-27                        return True
-28        return False
+3        m,n=len(board),len(board[0])
+4        if len(word)> m*n:
+5            return False
+6        
+7        directions=[(1,0),(-1,0),(0,1),(0,-1)]
+8        def dfs(x,y,index):
+9            if x<0 or x>=m or y<0 or y>=n or board[x][y]==1 or board[x][y]!=word[index]:
+10                return False
+11            if index==len(word)-1:
+12                return True
+13            temp=board[x][y]
+14            board[x][y]=1
+15            for dx,dy in directions:
+16                nx,ny=x+dx, y+dy
+17                if dfs(nx, ny, index + 1):
+18                    return True
+19            board[x][y]=temp
+20        
+21        for i in range(m):
+22            for j in range(n):
+23                if board[i][j]==word[0]:
+24                    if dfs(i,j,0):
+25                        return True
+26        return False
+27
+28
