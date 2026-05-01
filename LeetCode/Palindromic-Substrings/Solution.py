efad0d31ad147071b50da99c82057f8d82603eb4
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-01 14:38:09 -0700

    Add solution for Palindromic Substrings

--- LeetCode/Palindromic-Substrings/Solution.py
+++ LeetCode/Palindromic-Substrings/Solution.py
@@ -1,12 +1,16 @@
 1class Solution:
 2    def countSubstrings(self, s: str) -> int:
 3        n = len(s)
-4        dp = [[False] * n for _ in range(n)]
-5        count = 0
-6        for i in range(n - 1, -1, -1):
-7            for j in range(i, n):
-8                if s[i] == s[j] and (j - i < 3 or dp[i + 1][j - 1]):
-9                    dp[i][j] = True
-10                    count += 1
-11
-12        return count
+4        count=0
+5        def expand(l,r):
+6            nonlocal count
+7            while l>=0 and r<n and s[l]==s[r]:
+8                l -= 1
+9                r += 1
+10                count+=1
+11        
+12        for i in range(n):
+13            expand(i,i)
+14            expand(i,i+1)
+15        
+16        return count
