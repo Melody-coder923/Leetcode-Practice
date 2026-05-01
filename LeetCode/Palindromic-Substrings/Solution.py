7deef81f53ec3154d2bc5937d2b8a6ad984fa9f9
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-01 14:23:31 -0700

    Add solution for Palindromic Substrings

--- LeetCode/Palindromic-Substrings/Solution.py
+++ LeetCode/Palindromic-Substrings/Solution.py
@@ -4,18 +4,17 @@
 4        if n==1:
 5            return 1
 6        count=0
-7        for i in range(n):
-8            l,r=i,i
-9            while l>=0 and r<n and s[l]==s[r]:
-10                count+=1
-11                l-=1
-12                r+=1
-13            
-14            l,r=i,i+1
-15            while l>=0 and r<n and s[l]==s[r]:
-16                count+=1
-17                l-=1
-18                r+=1
-19        
-20        return count
-21
+7        @lru_cache(None)
+8        def dfs(l,r):
+9            if l>=r:
+10                return True
+11            return dfs(l+1, r-1) and s[l] == s[r]
+12        
+13        for i in range(n):
+14            for j in range(i,n):
+15                if dfs(i,j):
+16                    count+=1
+17        
+18        return count
+19                
+20
