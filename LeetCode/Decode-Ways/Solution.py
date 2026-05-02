cd92553ea19bb1547996af5fcc9331a5ba7ddca2
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-02 11:44:52 -0700

    Add solution for Decode Ways

--- LeetCode/Decode-Ways/Solution.py
+++ LeetCode/Decode-Ways/Solution.py
@@ -1,16 +1,20 @@
 1class Solution:
 2    def numDecodings(self, s: str) -> int:
-3        n=len(s)
-4        @lru_cache(None)
-5        def dfs(i):  #dfs(i) = s[0:i+1] 这一段前缀的解码方式数量
-6            if i<0:
-7                return 1
-8            res=0
-9            if s[i]!="0":
-10                res+=dfs(i-1) 
-11            if i-1>=0 and 10<= int(s[i-1:i+1]) <=26:
-12                res+=dfs(i-2)
-13            return res
-14        
-15        return dfs(n-1)
-16
+3        n = len(s)
+4        if s[0]=="0":
+5            return 0
+6        dp = [0] * (n + 1)  #dp[i] = s[0:i] 这一段前 i 个字符，有多少种解码方式，s[0:i] 不包含下标 i。
+7        dp[0] = 1 # 为了让dp[2] += dp[0]成立，所以必须为1
+8        dp[1] = 0 if s[0] == '0' else 1
+9
+10        for i in range(2, n + 1):
+11            if s[i-1] != '0':
+12                dp[i] += dp[i-1]
+13            two_digit = int(s[i-2:i])
+14            if 10 <= two_digit <= 26:
+15                dp[i] += dp[i-2]
+16        return dp[n]
+17"""
+18s   0 1.   n-2 n-1 
+19dp                  n
+20"""
