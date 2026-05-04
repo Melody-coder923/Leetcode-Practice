7cf958c2b4d087adc1ecdd04221bd915c0d40683
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-04 12:05:12 -0700

    Add solution for Coin Change

--- LeetCode/Coin-Change/Solution.py
+++ LeetCode/Coin-Change/Solution.py
@@ -1,11 +1,19 @@
 1class Solution:
 2    def coinChange(self, coins: List[int], amount: int) -> int:
-3        """dp[i] 表示凑出金额 i 所需的最少硬币数"""
-4        dp = [float('inf')] * (amount + 1)
-5        dp[0] = 0
-6
-7        for coin in coins:
-8            for i in range(coin, amount + 1):
-9                dp[i] = min(dp[i], dp[i - coin] + 1)
-10
-11        return dp[amount] if dp[amount] != float('inf') else -1
+3        #memo
+4        count=0
+5        @lru_cache(None)
+6        #def dfs
+7        def dfs(remain):
+8            #base case
+9            if remain==0:
+10                return 0
+11            if remain<0:
+12                return float("inf")
+13            res=float("inf")
+14            # break down
+15            for coin in coins:
+16                res=min(res,dfs(remain-coin)+1)
+17            return res
+18        res= dfs(amount) 
+19        return res if res!=float("inf") else -1
