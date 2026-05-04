2cf977f6ce1be1c334acb2fef43e67f65c4dd224
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-04 13:28:21 -0700

    Add solution for Coin Change

--- LeetCode/Coin-Change/Solution.py
+++ LeetCode/Coin-Change/Solution.py
@@ -4,27 +4,26 @@
 4
 5        @lru_cache(None)
 6        def dfs(target):
-7            nonlocal res
-8            if target==0:
-9                return 0
-10            if target<0:
-11                return float("inf")
-12            res= float("inf")
-13            #break down
-14            for coin in coins:
-15                res=min(res,dfs(target-coin)+1)
-16
-17            return res
-18        res=dfs(amount)
-19        return res if res!=float("inf") else -1
-20
-21    """
-22            amounts
-23              0  1. 2  3  4  5 6 7 8 9 10 11
-24    coins 0   0. e  e. e  e. e e e e e e   e
-25          1   0  
-26          2   0
-27          5   0
-28
-29    """
-30
+7            if target==0:
+8                return 0
+9            if target<0:
+10                return float("inf")
+11            res= float("inf")
+12            #break down
+13            for coin in coins:
+14                res=min(res,dfs(target-coin)+1)
+15
+16            return res
+17        res=dfs(amount)
+18        return res if res!=float("inf") else -1
+19
+20    """
+21            amounts
+22              0  1. 2  3  4  5 6 7 8 9 10 11
+23    coins 0   0. e  e. e  e. e e e e e e   e
+24          1   0  
+25          2   0
+26          5   0
+27
+28    """
+29
