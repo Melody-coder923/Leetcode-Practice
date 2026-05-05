61c4880867d2699df7bc3474a9fa5048a9f7823c
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-04 17:13:36 -0700

    Add solution for Word Break

--- LeetCode/Word-Break/Solution.py
+++ LeetCode/Word-Break/Solution.py
@@ -1,12 +1,16 @@
 1class Solution:
 2    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-3        wordDict=set(wordDict)
-4        n=len(s)
-5        dp=[False]*(n+1)
-6        dp[0]=True
-7        for i in range(n+1):
-8            for j in range(i):
-9                if dp[j] and s[j:i] in wordDict:
-10                    dp[i]=True
-11                    break
-12        return dp[n]         
+3        n=len(s)
+4        wordDict=set(wordDict)
+5        @lru_cache(None)
+6        def dfs(i):
+7            if i==0:
+8                return True
+9            
+10            for j in range(i):
+11                if s[j:i] in wordDict and dfs(j):
+12                    return True
+13            return False
+14        
+15        return dfs(n)
+16
