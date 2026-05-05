bc2bfe7835f556a94680c82daf33eb9c22fc1fd1
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-04 17:01:38 -0700

    Add solution for Word Break

--- LeetCode/Word-Break/Solution.py
+++ LeetCode/Word-Break/Solution.py
@@ -1,17 +1,12 @@
 1class Solution:
 2    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-3       # 前缀 DP / 字符串切分 DP（Prefix DP）
-4       #关键点只有一个：切位置, dp[i]：s 的前 i 个字符，能不能被成功拆分
-5        wordSet = set(wordDict)
-6        n = len(s)
-7        dp = [False] * (n + 1)
-8        
-9        # 空字符串 一定可以被拆分
-10        dp[0] = True
-11
-12        for i in range(1, n + 1):
-13            for j in range(i):
-14                if dp[j] and s[j:i] in wordSet:
-15                    dp[i] = True
-16                    break
-17        return dp[n]
+3        wordDict=set(wordDict)
+4        n=len(s)
+5        dp=[False]*(n+1)
+6        dp[0]=True
+7        for i in range(n+1):
+8            for j in range(i):
+9                if dp[j] and s[j:i] in wordDict:
+10                    dp[i]=True
+11                    break
+12        return dp[n]         
