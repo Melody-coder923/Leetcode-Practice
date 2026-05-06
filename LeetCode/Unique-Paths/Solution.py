d4648e935ab0c0629c338316f1d0fbdcaf223ea6
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-05 18:03:59 -0700

    Add solution for Unique Paths

--- LeetCode/Unique-Paths/Solution.py
+++ LeetCode/Unique-Paths/Solution.py
@@ -1,14 +1,11 @@
 1class Solution:
 2    def uniquePaths(self, m: int, n: int) -> int:
-3        dp=[[0]*n for _ in range(m)] 
-4        for j in range(n):
-5            dp[0][j]=1
-6        for i in range(m):
-7            dp[i][0]=1
-8        
-9        for i in range(1,m):
-10            for j in range(1,n):
-11                dp[i][j]=dp[i-1][j]+dp[i][j-1]
-12        
-13        return dp[m-1][n-1]       
-14    
+3        @lru_cache(None)
+4        def dfs(i,j):     
+5            if i==0 or j==0:
+6                return 1
+7            
+8            return dfs(i-1,j)+dfs(i,j-1)
+9
+10        return dfs(m-1,n-1)
+11    
