ff7ec228d16ef0acc1674b3a9889940fc36561c3
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-05 18:32:08 -0700

    Add solution for Decode Ways

--- LeetCode/Decode-Ways/Solution.py
+++ LeetCode/Decode-Ways/Solution.py
@@ -1,14 +1,18 @@
 1class Solution:
 2    def numDecodings(self, s: str) -> int:
-3        n=len(s)
-4        if s[0]=="0":
-5            return 0
-6        dp=[0]*(n+1)
-7        dp[0]=1
-8        dp[1]=0 if s[0]=="0" else 1
-9        for i in range(2,n+1):
-10            if s[i-1]!="0":
-11                dp[i]+=dp[i-1]
-12            if 10<= int(s[i-2:i])<=26:
-13                dp[i]+=dp[i-2]
-14        return dp[n]
+3        n = len(s)
+4        prev2=1
+5        prev1=0 if s[0]=="0" else 1
+6
+7        for i in range(2,n+1):
+8            curr=0
+9            if s[i-1]!="0":
+10                curr+=prev1
+11
+12            twodigit=int(s[i-2:i])
+13            if 10<= twodigit<=26:
+14                curr+=prev2
+15            
+16            prev2,prev1=prev1,curr
+17
+18        return prev1
