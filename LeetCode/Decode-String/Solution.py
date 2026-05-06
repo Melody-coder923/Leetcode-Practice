6534da44212c964e5b46bd446240dd4cf84bf267
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-05 17:54:06 -0700

    Add solution for Decode String

--- LeetCode/Decode-String/Solution.py
+++ LeetCode/Decode-String/Solution.py
@@ -1,21 +1,31 @@
 1class Solution:
 2    def decodeString(self, s: str) -> str:
-3        num_stack=[]
-4        str_stack=[]
-5        cur_num=0
-6        cur_str = ""  
-7        for ch in s:
-8            if ch.isdigit():
-9                cur_num = cur_num * 10 + int(ch)
-10            elif ch == '[':
-11                num_stack.append(cur_num)        
-12                str_stack.append(cur_str)        
-13                cur_num = 0
-14                cur_str = ""
-15            elif ch == ']':
-16                repeat_times = num_stack.pop()
-17                last_str = str_stack.pop()
-18                cur_str = last_str + repeat_times*cur_str
-19            else:
-20                cur_str += ch
-21        return cur_str
+3        def dfs(i):
+4            res=""
+5            num=0
+6            while i<len(s):
+7                ch=s[i]
+8                if ch.isdigit():
+9                    num=num*10+ int(ch)
+10                    i+=1
+11                #遇到 [ 递归进去
+12                elif ch=="[":
+13                    inner,i=dfs(i+1)
+14                    res+=num*inner
+15                    num=0
+16                # 遇到 ] → 当前层结束返回
+17                elif ch=="]":
+18                    return res,i+1
+19                else:
+20                    res+=ch
+21                    i+=1
+22            return res, i 
+23            
+24        ans, _ = dfs(0)
+25        return ans
+26                
+27
+28
+29                
+30
+31
