f2fb72a7f2689fb134f028c66cf807e02aa9737d
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-09 16:24:36 -0700

    Add solution for Longest Common Subsequence

--- LeetCode/Longest-Common-Subsequence/Solution.py
+++ LeetCode/Longest-Common-Subsequence/Solution.py
@@ -1,15 +1,15 @@
-define function LCS(text1, text2):
-    # If either string is empty there, can be no common subsequence.
-    if length of text1 or text2 is 0:
-        return 0
-
-    letter1 = the first letter in text1
-    firstOccurence = first position of letter1 in text2
-
-    # The case where the line *is not* part of the optimal solution
-    case1 = LCS(text1.substring(1), text2)
-
-    # The case where the line *is* part of the optimal solution
-    case2 = 1 + LCS(text1.substring(1), text2.substring(firstOccurence + 1))
-   
-    return maximum of case1 and case2
+1class Solution:
+2    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+3        m,n=len(text1),len(text2)
+4        @lru_cache(None)
+5        def dfs(i,j):
+6            if i==0 or j==0:
+7                return 0
+8            if text1[i-1]==text2[j-1]:
+9                return dfs(i-1,j-1)+1
+10            
+11            else:
+12                return max(dfs(i-1,j),dfs(i,j-1))
+13        
+14        return dfs(m,n)
+15        
