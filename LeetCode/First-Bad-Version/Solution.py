0c3bb45c047e64e0e1e3501c63c0c235fbdfd1fd
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-11 14:01:31 -0700

    Add solution for First Bad Version

--- LeetCode/First-Bad-Version/Solution.py
+++ LeetCode/First-Bad-Version/Solution.py
@@ -3,12 +3,13 @@
 3
 4class Solution:
 5    def firstBadVersion(self, n: int) -> int:
-6        l,r=1,n
+6        l,r=0,n+1
 7        while l<r:
 8            mid=l+(r-l)//2
 9            if isBadVersion(mid):
 10                r=mid
 11            else:
 12                l=mid+1
-13        
-14        return l
+13        return l
+14
+15
