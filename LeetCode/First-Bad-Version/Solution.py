74de2a2b20864925f27eacbbc0ebf123783bba3f
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-11 14:02:34 -0700

    Add solution for First Bad Version

--- LeetCode/First-Bad-Version/Solution.py
+++ LeetCode/First-Bad-Version/Solution.py
@@ -3,12 +3,12 @@
 3
 4class Solution:
 5    def firstBadVersion(self, n: int) -> int:
-6        l,r=1,n        #找好的最后一个      
+6        l,r=1,n        #最后一个坏版本     
 7        while l<=r:
 8            mid=(l+r)//2
 9            if isBadVersion(mid)==True:
-10                r=mid-1  #mid是坏的,好的在左边
+10                r=mid-1  #mid是坏的,第一个坏的在左边或者就是mid
 11            elif isBadVersion(mid)==False:
-12                l=mid+1   #mid是好的,好的在右边
+12                l=mid+1   #mid是好的,坏在mid右边
 13            
 14        return l 
