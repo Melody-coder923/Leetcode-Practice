b512eb90efb394bedb3d69e88e631eeb0db03864
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-11 09:43:43 -0700

    Add solution for Maximum Product Subarray

--- LeetCode/Maximum-Product-Subarray/Solution.py
+++ LeetCode/Maximum-Product-Subarray/Solution.py
@@ -1,20 +1,12 @@
 1class Solution:
 2    def maxProduct(self, nums: List[int]) -> int:
-3        left = 1
-4        right = 1
-5        n = len(nums)
-6        res = max(nums)
-7
-8        for i in range(n):
-9            left *= nums[i]
-10            right *= nums[n - 1 - i]
-11
-12            res = max(res, left, right)
-13
-14            if left == 0:
-15                left = 1
-16            if right == 0:
-17                right = 1
-18
-19        return res
-20
+3        cur_max=nums[0]
+4        cur_min=nums[0]
+5        res=nums[0]
+6
+7        for i in range(1,len(nums)):
+8            temp=cur_max
+9            cur_max=max(cur_max*nums[i], cur_min*nums[i], nums[i])
+10            cur_min=min(temp * nums[i], cur_min*nums[i], nums[i])
+11            res=max(cur_max,res)
+12        return res
