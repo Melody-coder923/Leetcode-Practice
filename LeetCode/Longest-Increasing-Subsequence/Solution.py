23526fd83ff6f538db4971a6a1f0631ecf18d864
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-12 10:03:22 -0700

    Add solution for Longest Increasing Subsequence

--- LeetCode/Longest-Increasing-Subsequence/Solution.py
+++ LeetCode/Longest-Increasing-Subsequence/Solution.py
@@ -1,10 +1,10 @@
 1class Solution:
 2    def lengthOfLIS(self, nums: List[int]) -> int:
-3        d=[]
-4        for x in nums:  
-5            i = bisect_left(d, x) 
-6            if i == len(d): 
-7                d.append(x) 
-8            else:  
-9                d[i] = x 
-10        return len(d)  
+3        d = []  # 用来维护一个递增序列
+4        for x in nums:  # 遍历 nums 里的每个数
+5            i = bisect_left(d, x)  # 在 d 中找第一个 >= x 的位置索引
+6            if i == len(d):  # 如果没找到（x 比所有数都大）
+7                d.append(x)  # 直接加到末尾
+8            else:  # 否则
+9                d[i] = x  # 替换这个位置的数为 x
+10        return len(d)  # d 的长度就是 LIS 长度
