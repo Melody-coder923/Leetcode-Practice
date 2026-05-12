e86e3b07be6719beedbb62683683da3ebbf88b29
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-12 10:24:36 -0700

    Add solution for Decode Ways

--- LeetCode/Decode-Ways/Solution.py
+++ LeetCode/Decode-Ways/Solution.py
@@ -1,22 +1,16 @@
 1class Solution:
 2    def numDecodings(self, s: str) -> int:
-3        n = len(s)
-4
-5        @lru_cache(None)
-6        def dfs(i):  # 表示 s[:i] 的解码方式
-7            if i == 0:
-8                return 1
-9
-10            res = 0
+3        n=len(s)
+4        @lru_cache(None)
+5        def dfs(i):
+6            if i<0:
+7                return 1
+8            res=0
+9            if s[i]!="0":
+10                res+=dfs(i-1)
 11
-12            # 单个字符（看 s[i-1]）
-13            if s[i-1] != "0":
-14                res += dfs(i-1)
-15
-16            # 两个字符（看 s[i-2:i]）
-17            if i >= 2 and 10 <= int(s[i-2:i]) <= 26:
-18                res += dfs(i-2)
-19
-20            return res
-21
-22        return dfs(n)
+12            if i-1>=0 and 10<=int(s[i-1:i+1])<=26:
+13                res+=dfs(i-2)
+14            return res
+15        
+16        return dfs(n-1)
