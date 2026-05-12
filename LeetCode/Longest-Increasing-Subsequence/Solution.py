cdec2afa428b5bd12832b99ac22ae3e51a06abe9
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-12 09:50:33 -0700

    Add solution for Longest Increasing Subsequence

--- LeetCode/Longest-Increasing-Subsequence/Solution.py
+++ LeetCode/Longest-Increasing-Subsequence/Solution.py
@@ -1,13 +1,9 @@
-public int lengthOfLIS(int[] nums) {
-    int N = nums.length, dp[] = new int[N], res = 1;
-    Arrays.fill(dp, 1);
-    for (int i = 1; i < N; i++) {
-        for (int j = 0; j < i; j++) {
-            if (nums[i] > nums[j]) {
-                dp[i] = Math.max(dp[i], dp[j] + 1);
-            }
-        }
-        res = Math.max(res, dp[i]);
-    }
-    return res;
-}
+1class Solution:
+2    def lengthOfLIS(self, nums: List[int]) -> int:
+3        n=len(nums)
+4        dp=[1]*n
+5        for i in range(1,n):
+6            for j in range(i):
+7                if nums[i]>nums[j]:
+8                    dp[i]=max(dp[j]+1,dp[i])
+9        return max(dp)
