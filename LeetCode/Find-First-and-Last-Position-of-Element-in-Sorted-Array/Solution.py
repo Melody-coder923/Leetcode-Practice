98e3514a79a815995e5bbd8c80afcf2130c55845
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-14 09:21:36 -0700

    Add solution for Find First and Last Position of Element in Sorted Array

--- LeetCode/Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.py
+++ LeetCode/Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.py
@@ -1,12 +1,48 @@
-[]
-42
-[7,8,8,8,8,8,8,8,8,8,8,9]
-7
-[7,8,8,8,8,8,8,8,8,8,8,9]
-8
-[7,8,8,8,8,8,8,8,8,8,8,9]
-10
-[1,2,2,2,2,3,4,5,5,5,5,6,7,8,9,10,11,12,12,12,12,12,13]
-2
-[-999985131,-999953607,-999953607,-999915742,-999883817,-999849817,-999822901,-999815377,-999810801,-68594,-49967,20394,114012,999969829,999973689,999975494]
--999953607
+var searchRange = function(nums, target) {
+    if (nums.length === 0) return [-1, -1];
+    
+    function findLeftMostIndex() {
+        let left = 0;
+        let right = nums.length - 1;
+        let leftMostIndex = -1;
+        while (left <= right) {
+            const mid = Math.trunc((left + right) / 2);
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;    
+            } else {
+                leftMostIndex = mid;
+                right = mid - 1;     
+            }
+        }
+        return leftMostIndex;
+    }
+    
+    function findRightMostIndex() {
+        let left = 0;
+        let right = nums.length - 1;
+        let rightMostIndex = -1;
+        
+        while (left <= right) {
+            const mid = Math.trunc((left + right) / 2);
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                rightMostIndex = mid;
+                left = mid + 1;
+            }
+        }
+        return rightMostIndex;
+    }
+        
+    const left = findLeftMostIndex();
+    
+    if (left === -1) return [-1, -1];
+    
+    const right = findRightMostIndex();
+    
+    return [left, right];
+};
