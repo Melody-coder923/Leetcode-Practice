97313b963f77eb331df4c548e0b6a752c9e2e894
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-18 10:40:50 -0700

    Add solution for Word Break

--- LeetCode/Word-Break/Solution.py
+++ LeetCode/Word-Break/Solution.py
@@ -4,8 +4,9 @@
 4        dp=[False]*(n+1)
 5        dp[0]=True
 6        wordDict=set(wordDict)
-7        for i in range(1,n+1):
-8            for j in range(i):
-9                if s[j:i] in wordDict and dp[j]:
-10                    dp[i]=True
-11        return dp[n]
+7
+8        for i in range(1,n+1):
+9            for j in range(i): 
+10                if s[j:i] in wordDict and dp[j]:
+11                    dp[i]=True
+12        return dp[n]
