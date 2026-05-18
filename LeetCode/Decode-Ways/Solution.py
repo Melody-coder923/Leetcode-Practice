492c7c5f202b2cf44f3b2f252ed2bab22d6f89b6
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-18 09:58:39 -0700

    Add solution for Decode Ways

--- LeetCode/Decode-Ways/Solution.py
+++ LeetCode/Decode-Ways/Solution.py
@@ -4,16 +4,12 @@
 4        if s[0]=="0":
 5            return 0
 6
-7        @lru_cache(None)
-8        def dfs(i):
-9            if i<0:
-10                return 1
-11            res=0
-12            if s[i]!="0":
-13                res+=dfs(i-1)
-14            if i-1>=0 and 10<=int(s[i-1:i+1])<=26:
-15                res+=dfs(i-2)
-16            return res
-17        
-18        return dfs(n-1)
-19        
+7        dp=[0]*(n+1)
+8        dp[0]=1
+9        for i in range(1,n+1): #i-1是当下坐标
+10            if s[i-1]!="0":
+11                dp[i]+=dp[i-1]
+12            if i-2>=0 and 10<=int(s[i-2:i])<=26:
+13                dp[i]+=dp[i-2]
+14        
+15        return dp[n]
