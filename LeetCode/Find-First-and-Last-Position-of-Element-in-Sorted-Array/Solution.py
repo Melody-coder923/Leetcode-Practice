7d1b18abf77b6bc000bc4a579ff10c1314cbcb6d
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-18 10:52:53 -0700

    Add solution for Find First and Last Position of Element in Sorted Array

--- LeetCode/Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.py
+++ LeetCode/Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.py
@@ -1,48 +1,29 @@
-var searchRange = function(nums, target) {
-    if (nums.length === 0) return [-1, -1];
-    
-    function findLeftMostIndex() {
-        let left = 0;
-        let right = nums.length - 1;
-        let leftMostIndex = -1;
-        while (left <= right) {
-            const mid = Math.trunc((left + right) / 2);
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;    
-            } else {
-                leftMostIndex = mid;
-                right = mid - 1;     
-            }
-        }
-        return leftMostIndex;
-    }
-    
-    function findRightMostIndex() {
-        let left = 0;
-        let right = nums.length - 1;
-        let rightMostIndex = -1;
-        
-        while (left <= right) {
-            const mid = Math.trunc((left + right) / 2);
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                rightMostIndex = mid;
-                left = mid + 1;
-            }
-        }
-        return rightMostIndex;
-    }
-        
-    const left = findLeftMostIndex();
-    
-    if (left === -1) return [-1, -1];
-    
-    const right = findRightMostIndex();
-    
-    return [left, right];
-};
+1class Solution:
+2    def searchRange(self, nums: List[int], target: int) -> List[int]:
+3        def findleft(l,r):
+4            while l<=r:
+5                mid=(l+r)//2
+6                if nums[mid]>=target:
+7                    r=mid-1
+8                else:
+9                    l=mid+1
+10            return l
+11        
+12        def findright(l,r):
+13            while l<=r:
+14                mid=(l+r)//2
+15                if nums[mid]<=target:
+16                    l=mid+1
+17                else:
+18                    r=mid-1
+19            return r
+20
+21        n=len(nums)
+22        if n == 0:
+23            return [-1, -1]
+24
+25        left=findleft(0,n-1)
+26        right = findright(0, n-1)
+27        if left <= right and nums[left] == target:
+28            return [left, right]
+29        return [-1,-1]
