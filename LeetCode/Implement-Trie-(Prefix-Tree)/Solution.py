f8e0261447cb71f29215cb9b95ee98503a2e42f2
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-21 22:39:45 -0700

    Add solution for Implement Trie (Prefix Tree)

--- LeetCode/Implement-Trie-(Prefix-Tree)/Solution.py
+++ LeetCode/Implement-Trie-(Prefix-Tree)/Solution.py
@@ -2,37 +2,36 @@
 2    def __init__(self):
 3        self.children={}
 4        self.is_end= False
-5       
+5
 6class Trie:
 7    def __init__(self):
 8        self.root= TrieNode()
-9
-10    def insert(self, word: str) -> None:
-11        cur=self.root
-12        for char in word:
-13            if char not in cur.children:
-14                cur.children[char]=TrieNode()
-15            cur=cur.children[char]
-16        cur.is_end=True
-17
-18    def search(self, word: str) -> bool:
-19        cur=self.root
-20        for char in word:
-21            if char not in cur.children:
-22                return False
-23            cur=cur.children[char]
-24        return cur.is_end
-25     
-26    def startsWith(self, prefix: str) -> bool:
-27        cur=self.root
-28        for char in prefix:
-29            if char not in cur.children:
-30                return False
-31            cur=cur.children[char]
-32        return True
-33        
-34# Your Trie object will be instantiated and called as such:
-35# obj = Trie()
-36# obj.insert(word)
-37# param_2 = obj.search(word)
-38# param_3 = obj.startsWith(prefix)
+9    def insert(self, word: str) -> None:
+10        cur =self.root
+11        for c in word:
+12            if c not in cur.children:
+13                cur.children[c]=TrieNode()
+14            cur=cur.children[c]
+15        cur.is_end=True
+16        
+17    def search(self, word: str) -> bool:
+18        cur=self.root
+19        for char in word:
+20            if char not in cur.children:
+21                return False
+22            cur=cur.children[char]
+23        return cur.is_end
+24
+25    def startsWith(self, prefix: str) -> bool:
+26        cur=self.root
+27        for char in prefix:
+28            if char not in cur.children:
+29                return False
+30            cur=cur.children[char]
+31        return True
+32
+33# Your Trie object will be instantiated and called as such:
+34# obj = Trie()
+35# obj.insert(word)
+36# param_2 = obj.search(word)
+37# param_3 = obj.startsWith(prefix)
