1032f1837610de14c18afca60b855067266e056d
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-27 21:07:45 -0700

    Add solution for Separate the Digits in an Array

--- LeetCode/Separate-the-Digits-in-an-Array/Solution.py
+++ LeetCode/Separate-the-Digits-in-an-Array/Solution.py
@@ -2,7 +2,12 @@
 2    def separateDigits(self, nums: List[int]) -> List[int]:
 3        res = []
 4        for num in nums:
-5            for digit in str(num):
-6                res.append(int(digit))
-7
-8        return res
+5            temp = []
+6
+7            while num > 0:
+8                temp.append(num % 10)
+9                num //= 10
+10
+11            res.extend(temp[::-1])
+12
+13        return res
