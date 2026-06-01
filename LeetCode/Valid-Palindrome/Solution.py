f09f9c70a6a6ec0f9906c90bb1b96d51ac1e3be0
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-01 13:29:01 -0700

    Add solution for Valid Palindrome

--- LeetCode/Valid-Palindrome/Solution.py
+++ LeetCode/Valid-Palindrome/Solution.py
@@ -1,18 +1,17 @@
 1class Solution:
 2    def isPalindrome(self, s: str) -> bool:
-3        if len(s)<=1:
-4            return True
-5        s=s.lower()
-6        left,right=0,len(s)-1
-7        while left<right:
-8            if not s[left].isalnum():
-9                left+=1
-10            elif not s[right].isalnum():
-11                right-=1
-12            elif s[left]!=s[right]:
+3        s = s.lower()
+4        l,r=0,len(s)-1
+5        while l<r:
+6            if not s[l].isalnum():
+7                l+=1
+8                continue
+9            if not s[r].isalnum():
+10                r-=1
+11                continue
+12            if s[l]!=s[r]:
 13                return False
-14            else:
-15                left+=1
-16                right-=1
-17        return True
-18            
+14            l += 1
+15            r -= 1
+16        return True
+17            
