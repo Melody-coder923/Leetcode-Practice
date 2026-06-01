b49975f95b1e9dae37a3c54c4a42c133b293ce9e
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-31 22:48:31 -0700

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.py
+++ LeetCode/Valid-Parentheses/Solution.py
@@ -1,19 +1,21 @@
 1class Solution:
 2    def isValid(self, s: str) -> bool:
-3        stack=[]
-4        map={
-5            ')':'(',
-6            '}':'{',
-7            ']':'['
-8        }
-9        for c in s:
-10            if c in map:
-11                if stack:
-12                    if map[c]!=stack.pop():
-13                        return False
-14                else:
-15                    return False
-16            else:
-17                stack.append(c)
-18        return not stack
-19                
+3        map= { 
+4            ")":"(",
+5            "]":"[",
+6            "}":"{"
+7        }
+8
+9        stack=[]
+10        for char in s:
+11            if char not in map:
+12                stack.append(char) 
+13            else:
+14                if stack:
+15                    if map[char]!= stack.pop():
+16                        return False
+17                elif not stack:
+18                    return False
+19        
+20        return not stack
+21
