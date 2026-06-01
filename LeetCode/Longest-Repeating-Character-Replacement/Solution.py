cd94cf2e7d952968f2907baed1b0153e6464ada6
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-01 00:47:38 -0700

    Add solution for Longest Repeating Character Replacement

--- LeetCode/Longest-Repeating-Character-Replacement/Solution.py
+++ LeetCode/Longest-Repeating-Character-Replacement/Solution.py
@@ -1,17 +1,22 @@
 1class Solution:
 2    def characterReplacement(self, s: str, k: int) -> int:
-3        from collections import defaultdict
-4        start=0
-5        maxcount=0
-6        maxlength=0
-7        count=defaultdict(int)
-8        for end in range(len(s)):
-9            count[s[end]]+=1
-10            maxcount = max(maxcount, count[s[end]])
-11
-12            while (end - start + 1) - maxcount > k:
-13                count[s[start]] -= 1 
-14                start+=1
-15            maxlength = max(maxlength, end - start + 1)
-16        
-17        return maxlength
+3        #sliding window最后可以有k个不一样的，否则挪动l
+4        # 不一样的= sliding window长度 - 最多的一样的字母数量
+5        l = 0
+6        count = defaultdict(int)
+7        maxletter = 0
+8        res = 0
+9
+10        for r in range(len(s)):
+11            count[s[r]] += 1
+12            maxletter = max(maxletter, count[s[r]])
+13
+14            while (r - l + 1) - maxletter > k:
+15                count[s[l]] -= 1
+16                l += 1
+17
+18            res = max(res, r - l + 1)
+19
+20        return res
+21
+22
