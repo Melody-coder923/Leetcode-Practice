5e24d2c457bcf94d9025303259eb922302a097f1
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-01 14:22:21 -0700

    Add solution for Jump Game

--- LeetCode/Jump-Game/Solution.py
+++ LeetCode/Jump-Game/Solution.py
@@ -1,13 +1,12 @@
 1class Solution:
 2    def canJump(self, nums: List[int]) -> bool:
-3        n=len(nums)
-4        farest=0
-5        for i in range(n):
-6            if i > farest:
-7                return False
-8            if nums[i]+i>=n-1:
-9                return True
-10            farest=max(farest,nums[i]+i)
-11        return False
+3        farest=0
+4        for i,num in enumerate(nums):
+5            if farest<i:
+6                return False
+7            if i+num>=len(nums)-1:
+8                return True
+9            farest=max(farest,i+num)
+10            
+11        
 12
-13
