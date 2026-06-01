5cadc450d33fd74f0b2d0ddccbb9b48b26a80ee9
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-01 11:59:46 -0700

    Add solution for Meeting Rooms II

--- LeetCode/Meeting-Rooms-II/Solution.py
+++ LeetCode/Meeting-Rooms-II/Solution.py
@@ -1,17 +1,20 @@
 1class Solution:
 2    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-3        if len(intervals)==1:
-4            return 1
-5        events=[]
-6        count=0
-7        for start,end in intervals:
-8            events.append((start,1))
-9            events.append((end,-1))
-10        events.sort(key=lambda x: (x[0], x[1]))
-11        current_rooms = 0
-12        max_rooms=0
-13        for time, event in events:
-14            current_rooms += event
-15            max_rooms = max(max_rooms, current_rooms)
-16        return max_rooms
-17
+3        # 按开始时间排序
+4        intervals.sort(key=lambda x: x[0])
+5        
+6        # 最小堆存储会议结束时间
+7        heap = []
+8        
+9        for interval in intervals:
+10            start, end = interval
+11            
+12            # 如果当前会议开始时间>=堆顶结束时间，可以复用会议室
+13            if heap and start >= heap[0]:
+14                heapq.heappop(heap)
+15            
+16            # 将当前会议结束时间加入堆
+17            heapq.heappush(heap, end)
+18        
+19        return len(heap)
+20
