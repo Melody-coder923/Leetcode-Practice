232abd33aa9b532470d6e83c2ada72b4eb347cf4
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-01 13:05:15 -0700

    Add solution for Meeting Rooms

--- LeetCode/Meeting-Rooms/Solution.py
+++ LeetCode/Meeting-Rooms/Solution.py
@@ -1,1 +1,13 @@
-return merge(intervals).length == intervals.length;    
+1class Solution:
+2    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+3        if not intervals:
+4            return True
+5        intervals.sort()
+6        prev=intervals[0][1]
+7        for start,end in intervals[1:]:
+8            if prev>start:
+9                return False
+10            prev=max(prev,end)
+11        
+12        return True
+13
