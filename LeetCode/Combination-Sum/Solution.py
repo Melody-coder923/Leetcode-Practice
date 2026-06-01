7885df1f2f6b5d399d62433ec10de446604ddfe1
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-01 00:00:35 -0700

    Add solution for Combination Sum

---
+++ LeetCode/Combination-Sum/Solution.py
@@ -0,0 +1,16 @@
+1class Solution:
+2    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+3        candidates.sort()
+4        res = []
+5        def backtrack(start,path,remain):
+6            if remain==0:
+7                res.append(path[:])
+8                return 
+9            for i in range(start,len(candidates)):
+10                if candidates[i] > remain:
+11                    break
+12                path.append(candidates[i])
+13                backtrack(i,path,remain-candidates[i])
+14                path.pop()
+15        backtrack(0,[],target)
+16        return res
