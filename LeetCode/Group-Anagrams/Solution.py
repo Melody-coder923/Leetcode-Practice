0649ef54131293fac479257de56988a7597d0b8e
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-01 16:30:46 -0700

    Add solution for Group Anagrams

--- LeetCode/Group-Anagrams/Solution.py
+++ LeetCode/Group-Anagrams/Solution.py
@@ -1,50 +1,7 @@
-primes = {'a': 2, 
-                  'b': 3, 
-                  'c': 5, 
-                  'd': 7, 
-                  'e': 11, 
-                  'f': 13,
-                  'g': 17,
-                  'h': 19,
-                  'i': 23,
-                  'j': 29,
-                  'k': 31,
-                  'l': 37,
-                  'm': 41,
-                  'n': 43,
-                  'o': 47,
-                  'p': 53,
-                  'q': 59,
-                  'r': 61,
-                  's': 67, 
-                  't': 71,
-                  'u': 73,
-                  'v': 79,
-                  'w': 83,
-                  'x': 89,
-                  'y': 97,
-                  'z': 101
-                 }
-        
-        
-        subLists = {}
-        
-        for string in strs:
-            product = 1
-            
-            for character in string:
-                product = primes[character] * product
-            
-            if product in subLists.keys():
-                listA = subLists[product]
-                listA.append(string)
-                subLists[product] = listA
-            else:
-                subLists[product] = [string]
-                
-        listToReturn = []
-        
-        for value in subLists.keys():
-            listToReturn.append(subLists[value])
-            
-        return listToReturn
+1class Solution:
+2    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+3        map=defaultdict(list)
+4        for word in strs:
+5            check=tuple(sorted(word))
+6            map[check].append(word)
+7        return list(map.values())
