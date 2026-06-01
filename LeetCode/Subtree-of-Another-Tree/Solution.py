6ff07927dd9e632d8412f6a0bf800f6e19a6ddfd
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-01 16:20:56 -0700

    Add solution for Subtree of Another Tree

--- LeetCode/Subtree-of-Another-Tree/Solution.py
+++ LeetCode/Subtree-of-Another-Tree/Solution.py
@@ -6,13 +6,14 @@
 6#         self.right = right
 7class Solution:
 8    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-9        def isSame(p,q):
+9        def dfs(p,q):
 10            if not p and not q:
 11                return True
 12            if not p or not q or p.val!=q.val:
 13                return False
-14            return p.val==q.val and isSame(p.left,q.left) and isSame(p.right,q.right)
-15        if not root:
-16            return False
-17        
-18        return isSame(root,subRoot) or self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot)
+14            return p.val==q.val and dfs(p.left,q.left) and dfs(p.right,q.right)
+15        
+16        if not root:
+17            return False
+18        
+19        return dfs(root,subRoot) or self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot)
