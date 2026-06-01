ec81ce4cddcf87b85666a1785f67ef10f927b5ae
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-01 00:54:37 -0700

    Add solution for Rotate Image

--- LeetCode/Rotate-Image/Solution.py
+++ LeetCode/Rotate-Image/Solution.py
@@ -1,12 +1,14 @@
 1class Solution:
 2    def rotate(self, matrix: List[List[int]]) -> None:
-3        n = len(matrix)
-4        # Step 1: 转置矩阵
-5        for i in range(n):
-6            for j in range(i + 1, n):
-7                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-8
-9        # Step 2: 每一行反转
-10        for row in matrix:
-11            row.reverse()
-12
+3        """
+4        Do not return anything, modify matrix in-place instead.
+5        """
+6        n=len(matrix)
+7        for i in range(n):
+8            for j in range(i+1, n):
+9                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+10        
+11        for row in matrix:
+12            row.reverse()
+13        return matrix
+14
