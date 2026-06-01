1a4843460cb6e87f2fa06fbe853390ccacb03839
Yan Zhao <zhao.y4@northeastern.edu>
2026-05-31 21:51:10 -0700

    Add solution for Reorder List

--- LeetCode/Reorder-List/Solution.py
+++ LeetCode/Reorder-List/Solution.py
@@ -8,39 +8,42 @@
 8        """
 9        Do not return anything, modify head in-place instead.
 10        """
-11        def reverse(node):
-12            pre=None
-13            cur=node
-14            while cur:
-15                temp=cur.next
-16                cur.next=pre
-17                pre=cur
-18                cur=temp
-19            return pre
-20
-21        fast=slow=head
-22        while fast and fast.next:
-23            fast=fast.next.next
-24            slow=slow.next
-25        p1=head
-26        p2=halfhead= reverse(slow.next)
-27        slow.next = None
+11        if not head:
+12            return head
+13
+14        def reverse(node):
+15            prev=None
+16            curr=node
+17            while curr:
+18                nxt=curr.next
+19                curr.next=prev
+20                prev=curr
+21                curr=nxt
+22            return prev
+23        
+24        p1=fast=slow=head
+25        while fast and fast.next:
+26            fast=fast.next.next
+27            slow=slow.next
 28        
-29        while p1 and p2:
-30            p1_next = p1.next
-31            p2_next = p2.next
-32
-33            p1.next = p2
-34            if not p1_next:
-35                break  # l1 完了，直接接上 l2 后续
-36            p2.next = p1_next
-37
-38            p1 = p1_next
-39            p2 = p2_next
+29        p2=reverse(slow.next)
+30        slow.next = None
+31
+32        while p1 and p2:
+33            p1_nxt=p1.next
+34            p2_nxt=p2.next
+35
+36            p1.next = p2
+37            if not p1_nxt:
+38                break
+39            p2.next=p1_nxt
 40
-41        return head
-42
-43
-44
-45        
-46       
+41            p1 = p1_nxt
+42            p2 = p2_nxt
+43            
+44        return head
+45
+46
+47        
+48
+49
