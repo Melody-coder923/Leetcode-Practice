8d3ce258e9df1127957926ea52b14a3e0c365a48
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-01 11:59:35 -0700

    Add solution for Meeting Rooms II

--- LeetCode/Meeting-Rooms-II/Solution.py
+++ LeetCode/Meeting-Rooms-II/Solution.py
@@ -1,15 +1,17 @@
 1class Solution:
 2    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-3        lst=[]
-4        for start,end in intervals:
-5            lst.append((start,1))
-6            lst.append((end,-1))
-7        
-8        lst.sort()
-9        max_rooms=0
-10        res=0
-11        for time,room in lst:
-12            res+=room
-13            max_rooms = max(max_rooms, res)
-14        return max_rooms
-15            
+3        if len(intervals)==1:
+4            return 1
+5        events=[]
+6        count=0
+7        for start,end in intervals:
+8            events.append((start,1))
+9            events.append((end,-1))
+10        events.sort(key=lambda x: (x[0], x[1]))
+11        current_rooms = 0
+12        max_rooms=0
+13        for time, event in events:
+14            current_rooms += event
+15            max_rooms = max(max_rooms, current_rooms)
+16        return max_rooms
+17
