8605368a51e11987d029d5952c2b28de3f22433a
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-02 10:21:56 -0700

    Add solution for Redundant Connection

--- LeetCode/Redundant-Connection/Solution.py
+++ LeetCode/Redundant-Connection/Solution.py
@@ -1,26 +1,23 @@
 1class Solution:
 2    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-3        # tree: n nodes and n-1=edges
-4        # no indepent node
-5        # connect but without cycle 
-6
-7        n=len(edges)
-8        parents = list(range(n + 1))
-9
-10        def find(x):
-11            if parents[x] != x:
-12                parents[x] = find(parents[x])  
-13            return parents[x]
-14        
-15        def union(x,y):
-16            root_x= find(x)
-17            root_y= find(y)
-18            if root_x==root_y:
-19                return False
-20            parents[root_y]=root_x
-21            return True
+3        n=len(edges)
+4        parents=list(range(n+1))
+5
+6        def find(x):
+7            if parents[x]!=x:
+8                parents[x]=find(parents[x])
+9            return parents[x]
+10        def union(x,y):
+11            root_x=find(x)
+12            root_y=find(y)
+13            if root_x!=root_y:
+14                parents[root_y]=root_x
+15                return True     
+16            return False
+17        
+18
+19        for u,v in edges:
+20            if not union(u,v):
+21                return [u, v]
 22        
-23        for u,v in edges:
-24            if not union(u,v):
-25                return [u,v]
-26    
+23
