ae70018b7178c0aed1ddf93b58e62c46e0603324
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-03 18:14:49 -0700

    Add solution for Flatten Binary Tree to Linked List

--- LeetCode/Flatten-Binary-Tree-to-Linked-List/Solution.py
+++ LeetCode/Flatten-Binary-Tree-to-Linked-List/Solution.py
@@ -8,20 +8,15 @@
 8    def flatten(self, root: Optional[TreeNode]) -> None:
 9        """
 10        Do not return anything, modify root in-place instead.
-11        """       
-12        cur = root
+11        """
+12        cur=root
 13        while cur:
 14            if cur.left:
-15                # 找到左子树的最右节点
-16                rightmost = cur.left
-17                while rightmost.right:
-18                    rightmost = rightmost.right
-19
-20                # 把当前右子树挂到左子树最右节点后面
-21                rightmost.right = cur.right
-22                # 左子树移到右边，左边置空
-23                cur.right = cur.left
-24                cur.left = None
-25
-26            cur = cur.right
-27   
+15                rightmost=cur.left
+16                while rightmost.right:
+17                    rightmost=rightmost.right
+18                rightmost.right=cur.right
+19                cur.right=cur.left
+20                cur.left=None
+21            
+22            cur=cur.right
