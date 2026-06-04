43bebbd1263b7957a1661661abb5fbd4a8bdc4d9
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-04 10:41:30 -0700

    Add solution for Find First and Last Position of Element in Sorted Array

--- LeetCode/Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.py
+++ LeetCode/Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.py
@@ -1,28 +1,29 @@
 1class Solution:
 2    def searchRange(self, nums: List[int], target: int) -> List[int]:
-3        if not nums:
-4            return [-1, -1]
-5        l,r=0,len(nums)-1
-6        res=[]
-7        while l<r:
-8            mid= l+(r-l)//2
-9            if nums[mid]<target:
-10                l=mid+1
-11            else:
-12                r=mid
-13        
-14        if nums[l] != target:
-15            return [-1, -1]
-16        
-17        left = l
-18
-19        l, r = 0, len(nums) - 1
-20        while l<r:
-21            mid = l + (r - l + 1) // 2
-22            if nums[mid]<=target:
-23                l = mid
-24            else:
-25                r = mid-1
-26        right = l
-27        return [left, right]
-28
+3        def findleft(l,r):
+4            while l<=r:
+5                mid=(l+r)//2
+6                if nums[mid]>=target:
+7                    r=mid-1
+8                else:
+9                    l=mid+1
+10            return l
+11        
+12        def findright(l,r):
+13            while l<=r:
+14                mid=(l+r)//2
+15                if nums[mid]<=target:
+16                    l=mid+1
+17                else:
+18                    r=mid-1
+19            return r
+20
+21        n=len(nums)
+22        if n == 0:
+23            return [-1, -1]
+24
+25        left=findleft(0,n-1)
+26        right = findright(0, n-1)
+27        if left <= right and nums[left] == target:
+28            return [left, right]
+29        return [-1,-1]
