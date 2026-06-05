2ad9be7753820e684be790a6df50b14afe880b3d
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-05 16:34:55 -0700

    Add solution for Find Leaves of Binary Tree

--- LeetCode/Find-Leaves-of-Binary-Tree/Solution.py
+++ LeetCode/Find-Leaves-of-Binary-Tree/Solution.py
@@ -6,19 +6,31 @@
 6#         self.right = right
 7class Solution:
 8    def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
-9        if not root:
-10            return root
-11        res=[]
-12        #每个节点要知道自己高度
-13        def dfs(node):
-14            if not node:
-15                return -1
-16            left=dfs(node.left)
-17            right=dfs(node.right)
-18            height=max(left,right)+1
-19            if height == len(res):
-20                res.append([])
-21            res[height].append(node.val)
-22            return height
-23        dfs(root)
-24        return res
+9        # 搜集叶子节点
+10        def collect(node):
+11            if not node: 
+12                return 
+13            if not node.left and not node.right:
+14                path.append(node.val)
+15                return 
+16            collect(node.left)
+17            collect(node.right)
+18        #删除叶子节点
+19        def remove(node):
+20            if not node:
+21                return None
+22            if not node.left and not node.right:
+23                return None
+24            node.left = remove(node.left) 
+25            node.right = remove(node.right)
+26            return node
+27        
+28        if not root:
+29            return []
+30        res=[]
+31        while root:
+32            path = []
+33            collect(root)
+34            res.append(path)
+35            root = remove(root)           
+36        return res
