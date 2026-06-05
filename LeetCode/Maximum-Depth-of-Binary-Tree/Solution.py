48475fdd0efb309b3e40c8f896de5f5424e1fcde
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-05 11:02:28 -0700

    Add solution for Maximum Depth of Binary Tree

--- LeetCode/Maximum-Depth-of-Binary-Tree/Solution.py
+++ LeetCode/Maximum-Depth-of-Binary-Tree/Solution.py
@@ -6,18 +6,13 @@
 6#         self.right = right
 7class Solution:
 8    def maxDepth(self, root: Optional[TreeNode]) -> int:
-9        if not root:
-10            return 0
-11        q=deque([root])
-12        level=0
-13        while q:
-14            level+=1
-15            size=len(q)
-16            for _ in range(size):
-17                node=q.popleft()
-18                if node.left:
-19                    q.append(node.left)
-20                if node.right:
-21                    q.append(node.right)
-22        return level
-23
+9        max_depth=0
+10        def dfs(node):
+11            nonlocal max_depth
+12            if not node:
+13                return 0
+14            left=dfs(node.left)
+15            right=dfs(node.right)
+16            max_depth=max(max_depth,left,right)
+17            return max(left,right)+1
+18        return dfs(root)
