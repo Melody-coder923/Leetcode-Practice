2a7ced7f6b51655532bb95c602f607743d9c11b9
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-05 11:40:51 -0700

    Add solution for Balanced Binary Tree

--- LeetCode/Balanced-Binary-Tree/Solution.py
+++ LeetCode/Balanced-Binary-Tree/Solution.py
@@ -6,14 +6,15 @@
 6#         self.right = right
 7class Solution:
 8    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-9        if not root:
-10            return True
-11        def help(node):
+9        balanced=True
+10        def help(node):
+11            nonlocal balanced
 12            if not node:
 13                return 0
 14            left=help(node.left)
 15            right=help(node.right)
-16            if abs(left-right)>1 or left==-1 or right==-1:
-17                return -1
+16            if abs(left-right)>1:
+17                balanced=False
 18            return max(left,right)+1
-19        return help(root) != -1
+19        help(root)
+20        return balanced
