f7eec2f4520639ed7b046ae604be8f75992dac46
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-12 15:40:58 -0700

    Add solution for Best Time to Buy and Sell Stock with Cooldown

--- LeetCode/Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/Solution.py
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/Solution.py
@@ -1,19 +1,18 @@
 1class Solution:
 2    def maxProfit(self, prices: List[int]) -> int:
-3        n=len(prices)
-4        dp=[[0]*2 for _ in range(n)]
-5
-6        dp[0][0] = 0
-7        dp[0][1] = -prices[0]
-8        for i in range(1,n):
-9            if i==1:
-10                #不持有
-11                dp[i][0]=max(dp[i-1][0],dp[i-1][1]+prices[i])
-12                #持有
-13                dp[i][1] = max(dp[i-1][1], -prices[i])
-14            #不持有 cool down one day
-15            dp[i][0]=max(dp[i-1][0],dp[i-1][1]+prices[i])
-16            #持有
-17            dp[i][1]=max(dp[i-1][1],dp[i-2][0]-prices[i])
-18
-19        return dp[n-1][0]
+3        n = len(prices)
+4        if n == 1:
+5            return 0
+6        dp = [[0] * 2 for _ in range(n)]
+7
+8        dp[0][0] = 0
+9        dp[0][1] = -prices[0]
+10
+11        dp[1][0] = max(dp[0][0], dp[0][1] + prices[1])
+12        dp[1][1] = max(dp[0][1], -prices[1])
+13
+14        for i in range(2, n):
+15            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+16            dp[i][1] = max(dp[i-1][1], dp[i-2][0] - prices[i])
+17
+18        return dp[n-1][0]
