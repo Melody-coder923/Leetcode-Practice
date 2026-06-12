9a4276e7c95cf132f679409bc17a776469f8a82f
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-12 09:31:34 -0700

    Add solution for Implement Trie (Prefix Tree)

--- LeetCode/Implement-Trie-(Prefix-Tree)/Solution.py
+++ LeetCode/Implement-Trie-(Prefix-Tree)/Solution.py
@@ -1,34 +1,34 @@
 1class TrieNode:
 2    def __init__(self):
 3        self.children={}
-4        self.is_end= False
-5
-6class Trie:
-7    def __init__(self):
-8        self.root= TrieNode()
-9    def insert(self, word: str) -> None:
-10        cur =self.root
-11        for c in word:
-12            if c not in cur.children:
-13                cur.children[c]=TrieNode()
-14            cur=cur.children[c]
-15        cur.is_end=True
-16        
-17    def search(self, word: str) -> bool:
-18        cur=self.root
-19        for char in word:
-20            if char not in cur.children:
-21                return False
-22            cur=cur.children[char]
-23        return cur.is_end
-24
-25    def startsWith(self, prefix: str) -> bool:
-26        cur=self.root
-27        for char in prefix:
-28            if char not in cur.children:
-29                return False
-30            cur=cur.children[char]
-31        return True
+4        self.end=False
+5class Trie:
+6    def __init__(self):
+7        self.root=TrieNode()
+8    def insert(self, word: str) -> None:
+9        cur=self.root
+10        for c in word:
+11            if c not in cur.children:
+12                cur.children[c]=TrieNode()
+13            cur=cur.children[c]
+14        cur.end=True
+15
+16    def search(self, word: str) -> bool:
+17        cur=self.root
+18        for c in word:
+19            if c not in cur.children:
+20                return False
+21            cur=cur.children[c]
+22        return cur.end
+23
+24    def startsWith(self, prefix: str) -> bool:
+25        cur=self.root
+26        for c in prefix:
+27            if c not in cur.children:
+28                return False
+29            cur=cur.children[c]
+30        return True
+31
 32
 33# Your Trie object will be instantiated and called as such:
 34# obj = Trie()
