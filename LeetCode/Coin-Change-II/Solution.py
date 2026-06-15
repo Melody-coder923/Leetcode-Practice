3dda9a12f6b7193ce0d556043857fd033035e0e9
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-15 09:05:18 -0700

    Add solution for Coin Change II

--- LeetCode/Coin-Change-II/Solution.py
+++ LeetCode/Coin-Change-II/Solution.py
@@ -1,11 +1,16 @@
 1from typing import List
 2class Solution:
 3    def change(self, amount: int, coins: List[int]) -> int:
-4        dp = [0] * (amount + 1)
-5        dp[0] = 1
-6         # 关键：外层遍历硬币
-7        for coin in coins:
-8            # 内层遍历金额
-9            for i in range(coin, amount + 1):
-10                dp[i] += dp[i - coin]
-11        return dp[amount]
+4        m=len(coins)
+5        dp= [ [0]* (amount+1) for _ in range(m+1) ]
+6        for i in range(m+1):
+7            dp[i][0]=1
+8        for j in range(1,amount+1):
+9            dp[0][j]=0
+10        for i in range(1,m+1):
+11            for j in range(amount+1):
+12                dp[i][j]=dp[i-1][j]
+13                if j>=coins[i-1]:
+14                    dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]]
+15        return dp[m][amount]
+16
