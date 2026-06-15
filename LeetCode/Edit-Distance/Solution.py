eb013ee0aa97992b99610f0037e1d453a2a674e2
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-15 13:05:27 -0700

    Add solution for Edit Distance

--- LeetCode/Edit-Distance/Solution.py
+++ LeetCode/Edit-Distance/Solution.py
@@ -1,27 +1,15 @@
 1class Solution:
 2    def minDistance(self, word1: str, word2: str) -> int:
-3        #Edge case:
-4        m,n= len(word1),len(word2)
-5        if m==0:
-6            return n
-7        if n==0:
-8            return m
-9
-10        @lru_cache(None)
-11        def dfs(i,j):
-12        #base case:
-13            if j<0:
-14                return i+1
-15            if i<0:
-16                return j+1
-17            
-18            if word1[i]==word2[j]:
-19                return dfs(i-1,j-1)
-20
-21            # word1[i]!=word2[j]
-22            delete=dfs(i-1,j)+1
-23            insert=dfs(i,j-1)+1
-24            replace=dfs(i-1,j-1)+1
-25            return min(delete,insert,replace)
-26
-27        return dfs(m-1,n-1)
+3        m, n = len(word1), len(word2)
+4        dp = [[0] * (n + 1) for _ in range(m + 1)]
+5        for i in range(m + 1):
+6            dp[i][0] = i
+7        for j in range(n + 1):
+8            dp[0][j] = j
+9        for i in range(1, m + 1):
+10            for j in range(1, n + 1):
+11                if word1[i - 1] == word2[j - 1]:
+12                    dp[i][j] = dp[i - 1][j - 1]
+13                else:
+14                    dp[i][j] = 1 + min(dp[i - 1][j],dp[i][j - 1],dp[i - 1][j - 1])
+15        return dp[m][n]
