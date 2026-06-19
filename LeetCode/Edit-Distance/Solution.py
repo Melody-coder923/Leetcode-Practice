94e71c1885470c92e7f3bdcb53d5ba87e76c93ae
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-19 04:47:51 -0700

    Add solution for Edit Distance

--- LeetCode/Edit-Distance/Solution.py
+++ LeetCode/Edit-Distance/Solution.py
@@ -1,11 +1,11 @@
 1class Solution:
 2    def minDistance(self, word1: str, word2: str) -> int:
 3        m,n=len(word1),len(word2)
-4        dp = [[0] * (n + 1) for _ in range(m + 1)]
-5        for j in range(n+1):
-6            dp[0][j]= j
-7        for i in range(m+1):
-8            dp[i][0]=i
+4        dp=[[0]*(n+1) for _ in range(m+1)]
+5        for i in range(m+1):
+6            dp[i][0]=i
+7        for j in range(n+1):
+8            dp[0][j]=j
 9
 10        for i in range(1,m+1):
 11            for j in range(1,n+1):
@@ -13,11 +13,9 @@
 13                    dp[i][j]=dp[i-1][j-1]
 14                else:
 15                    dp[i][j]=min(
-16                        #删除
-17                        dp[i-1][j],
-18                        #插入
-19                        dp[i][j-1],
-20                        #替换
-21                        dp[i-1][j-1]
-22                    )+1
-23        return dp[m][n]
+16                        dp[i][j-1], #插入
+17                        dp[i-1][j-1], # 替换
+18                        dp[i-1][j] # 删除
+19                    )+1
+20        return dp[m][n]
+21
