b3c244294928a9450e828013ac9490b9f2073e73
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-23 15:38:34 -0700

    Add solution for Interleaving String

--- LeetCode/Interleaving-String/Solution.py
+++ LeetCode/Interleaving-String/Solution.py
@@ -1,20 +1,28 @@
 1class Solution:
 2    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-3        # dp i 和 j 前i个s1 和前j个s2 是否可以组成s3
-4        m,n=len(s1),len(s2)
-5        #Edge Case
-6        if m+n!=len(s3):
-7            return False
-8        # dp initialzation
-9        dp= [[False]*(n+1) for _ in range(m+1)]
-10        for i in range(m+1):
-11            for j in range(n+1):
-12                if i==0 and j==0:
-13                    dp[i][j]=True
-14                else:
-15                    if i>0 and s1[i-1]==s3[i+j-1] and dp[i-1][j]:
-16                        dp[i][j]= True
-17                    if j>0 and s2[j-1]==s3[i+j-1] and dp[i][j-1]:
-18                        dp[i][j]= True
-19                
-20        return dp[m][n]
+3        m,n,t=len(s1),len(s2),len(s3)
+4        if m+n!=t:
+5            return False
+6        
+7        dp= [[False]*(n+1) for _ in range(m+1)]
+8     
+9        for i in range(m+1):
+10            for j in range(n+1):
+11                if i==0 and j==0:
+12                    dp[i][j]=True
+13                else:
+14                    if i>0 and s1[i-1]==s3[i+j-1] and dp[i-1][j]: 
+15                        dp[i][j]=True
+16                    if j>0 and s2[j-1]==s3[i+j-1] and dp[i][j-1]:
+17                        dp[i][j]=True
+18        return dp[m][n]    
+19
+20"""
+21      0 d b b c a  n
+22    0 T 
+23    a
+24    a
+25    b
+26    c
+27    c
+28"""
