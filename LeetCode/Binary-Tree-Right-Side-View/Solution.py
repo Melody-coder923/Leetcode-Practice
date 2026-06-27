cff5a8146e1999537f519ef076a8898e19797349
Yan Zhao <zhao.y4@northeastern.edu>
2026-06-26 18:45:31 -0700

    Add solution for Binary Tree Right Side View

--- LeetCode/Binary-Tree-Right-Side-View/Solution.py
+++ LeetCode/Binary-Tree-Right-Side-View/Solution.py
@@ -1,21 +1,20 @@
-1# Definition for a binary tree node.
-2# class TreeNode:
-3#     def __init__(self, val=0, left=None, right=None):
-4#         self.val = val
-5#         self.left = left
-6#         self.right = right
-7class Solution:
-8    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-9        if not root:
-10            return []
-11        res=[]
-12        def dfs(node,depth):
-13            if not node:
-14                return 
-15            if len(res)==depth:
-16                res.append(node.val)
-17            dfs(node.right,depth+1)
-18            dfs(node.left,depth+1)
-19            return res
-20        return dfs(root,0)
-21        
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new LinkedList<>();
+        if(root==null)   return ans;
+        Deque<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i<size; i++){
+                TreeNode node = q.poll();
+                if(i==size-1){
+                    ans.add(node.val);
+                }
+                if(node.left!=null) q.add(node.left);
+                if(node.right!=null)    q.add(node.right);
+            }
+        }
+        return ans;
+    }
+}
