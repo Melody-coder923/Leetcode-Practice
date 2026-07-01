f926629067b5f47ec338b168cf7bdf0252a9f16e
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-01 16:58:57 -0700

    Add solution for Redundant Connection

--- LeetCode/Redundant-Connection/Solution.py
+++ LeetCode/Redundant-Connection/Solution.py
@@ -1,25 +1,20 @@
 1class Solution:
 2    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-3        graph=defaultdict(list)
-4        def dfs(cur, target, visited):
-5            if cur == target:
-6                return True
-7            visited.add(cur)
-8            for nei in graph[cur]:
-9                if nei not in visited:
-10                    if dfs(nei, target, visited):
-11                        return True
-12            return False
-13
-14        for u, v in edges:
-15            # 如果 u 已经能走到 v，再加 [u,v] 就形成环
-16            if u in graph and v in graph:
-17                if dfs(u, v, set()):
-18                    return [u, v]
-19
-20            graph[u].append(v)
-21            graph[v].append(u)
-22
-23        return []
-24       
-25        
+3        parent=[i for i in range(len(edges)+1)]
+4
+5        def find(x):
+6            if parent[x] != x:
+7                parent[x]=find(parent[x])
+8            return parent[x]
+9
+10        def union(u,v):
+11            root_u=find(u)
+12            root_v=find(v)
+13            if root_u==root_v:
+14                return False
+15            parent[root_v]=root_u
+16            return True
+17            
+18        for u,v in edges:
+19            if not union(u,v):
+20                return [u,v]
