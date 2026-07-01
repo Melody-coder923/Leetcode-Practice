9672b66e476d905b732066e400ab3bb7b8cab2c8
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-01 11:38:13 -0700

    Add solution for All Nodes Distance K in Binary Tree

--- LeetCode/All-Nodes-Distance-K-in-Binary-Tree/Solution.py
+++ LeetCode/All-Nodes-Distance-K-in-Binary-Tree/Solution.py
@@ -4,12 +4,12 @@
 4#         self.val = x
 5#         self.left = None
 6#         self.right = None
-7from collections import deque
+7
 8class Solution:
 9    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-10        
-11        #build parent relations
-12        n_to_parent={}
+10        #build parent relations
+11        n_to_parent = {}
+12
 13        def findparent(node):
 14            if not node:
 15                return
@@ -20,18 +20,20 @@
 20                n_to_parent[node.right]=node
 21                findparent(node.right)
 22        findparent(root)
-23        #bfs
-24        q=deque([(target,0)])
-25        visited={target}
-26        res=[]
-27        while q:
-28            for _ in range(len(q)):
-29                node,dis=q.popleft()
-30                if dis==k:
-31                    res.append(node.val)
-32                    continue
-33                for nei in (node.left,node.right, n_to_parent.get(node)):
-34                    if nei and nei not in visited:
-35                        q.append((nei,dis+1))
-36                        visited.add(nei)
-37        return res
+23
+24        # bfs
+25        q=deque([(target,0)])
+26        visited={target}
+27        res=[]
+28        while q:
+29            size=len(q)
+30            for _ in range(size):
+31                node,dis=q.popleft()
+32                if dis==k:
+33                    res.append(node.val)
+34                    continue
+35                for nxt in (node.left,node.right,n_to_parent.get(node)):
+36                    if nxt and nxt not in visited:
+37                        q.append((nxt,dis+1))
+38                        visited.add(nxt)
+39        return res
