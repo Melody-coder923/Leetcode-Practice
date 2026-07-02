ab8b166b5ebe5bd25f6af6129d340c63bac3152a
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-02 16:17:47 -0700

    Add solution for Word Search

--- LeetCode/Word-Search/Solution.py
+++ LeetCode/Word-Search/Solution.py
@@ -1,27 +1,28 @@
 1class Solution:
 2    def exist(self, board: List[List[str]], word: str) -> bool:
-3        directions=[(0,1),(0,-1),(1,0),(-1,0)]
-4        def backtrack(i,j,idx):
-5            if i<0 or j<0 or i>=m or j>=n or board[i][j]==1:
-6                return False
-7            if board[i][j]!=word[idx]:
-8                return False
-9            if idx==len(word)-1:
-10                return True
-11            temp=board[i][j]
-12            board[i][j]=1
-13            for dx,dy in directions:
-14                nx,ny=i+dx,j+dy
-15                if backtrack(nx,ny,idx+1):
-16                    return True
-17            board[i][j]=temp
-18            return False
-19
-20        m,n=len(board),len(board[0])
-21        idx=0
-22        for i in range(m):
-23            for j in range(n):
-24                if board[i][j]==word[idx]:
-25                    if backtrack(i,j,idx):
-26                        return True
-27        return False
+3        m,n=len(board),len(board[0])
+4        directions = [(0,1), (0,-1), (1,0), (-1,0)]
+5        def dfs(i,j,idx):
+6            if i<0 or j<0 or i>=m or j>=n or board[i][j]!=word[idx]:
+7                return False
+8            if board[i][j]=="#":
+9                return False
+10            if idx==len(word)-1:
+11                return True
+12            
+13            temp=board[i][j]
+14            board[i][j]="#"
+15            for di,dj in directions:
+16                ni,nj=i+di,j+dj
+17                if dfs(ni,nj,idx+1):
+18                    board[i][j] =temp
+19                    return True
+20            board[i][j]=temp
+21            return False
+22
+23        for i in range(m):
+24            for j in range(n):
+25                if board[i][j]==word[0]:
+26                    if dfs(i,j,0):
+27                        return True
+28        return False
