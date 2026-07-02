2156262a89cf3d21bb68d497dfaf99f501394a72
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-02 16:07:07 -0700

    Add solution for Max Area of Island

--- LeetCode/Max-Area-of-Island/Solution.py
+++ LeetCode/Max-Area-of-Island/Solution.py
@@ -1,19 +1,20 @@
 1class Solution:
 2    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-3        m = len(grid)
-4        n = len(grid[0])
-5        maxarea = 0
-6
-7        def dfs(x, y):
-8            if x < 0 or y < 0 or x >= m or y >= n or grid[x][y] != 1:
-9                return 0
-10            grid[x][y] = 0
-11            return 1 + dfs(x + 1, y) + dfs(x - 1, y) + dfs(x, y + 1) + dfs(x, y - 1)
-12
-13        for i in range(m):
-14            for j in range(n):
-15                if grid[i][j] == 1:
-16                    area = dfs(i, j)  # ✅ 获取当前岛屿面积
-17                    maxarea = max(maxarea, area)
-18
-19        return maxarea
+3        m,n=len(grid),len(grid[0])
+4        directions=[(0, 1), (0, -1), (1, 0), (-1, 0)]
+5        def dfs(x,y):
+6            if x<0 or y<0 or x>=m or y>=n or grid[x][y]==0:
+7                return 0
+8            area=1
+9            grid[x][y]=0
+10            for dx,dy in directions:
+11                nx,ny= x+dx,y+dy
+12                area+=dfs(nx,ny)
+13            return area
+14        res=0
+15        for i in range(m):
+16            for j in range(n):
+17                if grid[i][j]==1:
+18                    res=max(res,dfs(i,j))
+19
+20        return res
