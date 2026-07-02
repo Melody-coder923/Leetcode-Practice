5273741048ed56e572c147fede466cf5bba90520
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-01 17:35:15 -0700

    Add solution for Number of Connected Components in an Undirected Graph

--- LeetCode/Number-of-Connected-Components-in-an-Undirected-Graph/Solution.py
+++ LeetCode/Number-of-Connected-Components-in-an-Undirected-Graph/Solution.py
@@ -1,29 +1,20 @@
-1from typing import List
-2from collections import defaultdict
-3
-4class Solution:
-5    def countComponents(self, n: int, edges: List[List[int]]) -> int:
-6        # Step 1: 建图
-7        graph = defaultdict(list)
-8        for a, b in edges:
-9            graph[a].append(b)
-10            graph[b].append(a)  # 无向图：双向边
-11
-12        visited = set()
-13        count = 0
-14
-15        # Step 2: 对每个节点尝试 DFS
-16        def dfs(node):
-17            if node in visited:
-18                return
-19            visited.add(node)
-20            for neighbor in graph[node]:
-21                dfs(neighbor)
-22
-23        for i in range(n):
-24            if i not in visited:
-25                dfs(i)
-26                count += 1  # 每次新的 DFS 表示一个连通分量
-27
-28        return count
-29
+1class Solution:
+2    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+3        graph=defaultdict(list)
+4        for a,b in edges:
+5            graph[a].append(b)
+6            graph[b].append(a)
+7        visited = [False]*n
+8        count=0
+9        
+10        def dfs(node):
+11            visited[node] = True
+12            for neighbor in graph[node]:
+13                if not visited[neighbor]:
+14                    dfs(neighbor)
+15                    
+16        for i in range(n):
+17            if visited[i]==False:
+18                count+=1
+19                dfs(i)
+20        return count
