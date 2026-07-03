d8e16d69dc4ba1f22c7624a13fe1b85a5d44c77f
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-02 18:13:49 -0700

    Add solution for Letter Combinations of a Phone Number

--- LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.py
+++ LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.py
@@ -1,26 +1,24 @@
 1class Solution:
 2    def letterCombinations(self, digits: str) -> List[str]:
-3        #Step1: 做字典
-4        d_to_l={ "2":"abc",
-5        "3": "def",
-6        "4": "ghi",
-7        "5": "jkl",
-8        "6": "mno",
-9        "7": "pqrs",
-10        "8": "tuv",
-11        "9": "wxyz"}
-12        res=[]
-13        def backtrack(path,idx):
-14            if idx==len(digits):
-15                res.append("".join(path[:]))
-16                return
-17            
-18            for letter in d_to_l[digits[idx]]:
-19                path.append(letter)
-20                backtrack(path,idx+1)
-21                path.pop()
-22            return
-23
-24        backtrack([],0)
-25        return res
-26
+3        if not digits:
+4            return []
+5        #Step1: 做字典
+6        map={ "2":"abc",
+7        "3": "def",
+8        "4": "ghi",
+9        "5": "jkl",
+10        "6": "mno",
+11        "7": "pqrs",
+12        "8": "tuv",
+13        "9": "wxyz"}
+14        res=[]
+15        def backtrack(idx,path):
+16            if idx==len(digits):
+17                res.append("".join(path[:]))
+18                return 
+19            for letter in map[digits[idx]]:
+20                path.append(letter)
+21                backtrack(idx+1,path)
+22                path.pop()
+23        backtrack(0,[])
+24        return res
