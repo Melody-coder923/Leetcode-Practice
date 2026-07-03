2f0d7a6b0e49666832a801b27d3f22562a01f629
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-03 15:46:32 -0700

    Add solution for Regular Expression Matching

--- LeetCode/Regular-Expression-Matching/Solution.py
+++ LeetCode/Regular-Expression-Matching/Solution.py
@@ -1,5 +1,21 @@
-class Solution {
-    public boolean isMatch(String s, String p) {
-        return s.matches(p);
-    }
-}
+1class Solution:
+2    def isMatch(self, s: str, p: str) -> bool:
+3        m,n=len(s),len(p)
+4        dp=[[False]*(n+1) for _ in range(m+1)]
+5        dp[0][0]=True
+6        for j in range(2,n+1):
+7            if p[j-1]=="*":
+8                dp[0][j]=dp[0][j-2]
+9        
+10        for i in range(1,m+1):
+11            for j in range(1,n+1):
+12                if p[j-1]!="*":
+13                    if p[j-1]==s[i-1] or p[j-1]==".":
+14                        dp[i][j]=dp[i-1][j-1]
+15                else:
+16                    zeromatch= dp[i][j-2]
+17                    multimatch=(s[i-1]==p[j-2] or p[j-2]==".")
+18                    if multimatch:
+19                        multimatch=dp[i-1][j]
+20                    dp[i][j]=zeromatch or multimatch
+21        return dp[m][n]
