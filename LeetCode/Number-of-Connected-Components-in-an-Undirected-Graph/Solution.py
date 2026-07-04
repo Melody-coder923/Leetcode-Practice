5206a03a80dd9d0e9102895013801e02154cd4a6
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-04 12:41:43 -0700

    Add solution for Number of Connected Components in an Undirected Graph

--- LeetCode/Number-of-Connected-Components-in-an-Undirected-Graph/Solution.py
+++ LeetCode/Number-of-Connected-Components-in-an-Undirected-Graph/Solution.py
@@ -1,20 +1,22 @@
 1class Solution:
 2    def countComponents(self, n: int, edges: List[List[int]]) -> int:
 3        graph=defaultdict(list)
-4        for a,b in edges:
-5            graph[a].append(b)
-6            graph[b].append(a)
-7        visited = [False]*n
-8        count=0
-9        
-10        def dfs(node):
-11            visited[node] = True
-12            for neighbor in graph[node]:
-13                if not visited[neighbor]:
-14                    dfs(neighbor)
-15                    
-16        for i in range(n):
-17            if visited[i]==False:
-18                count+=1
-19                dfs(i)
-20        return count
+4
+5        for u,v in edges:
+6            graph[u].append(v)
+7            graph[v].append(u)
+8        visited=set()
+9        def dfs(node):
+10            visited.add(node)
+11            for nei in graph[node]:
+12                if nei not in visited:
+13                    dfs(nei)
+14
+15        
+16        count=0
+17        for node in range(n):
+18            if node not in visited:
+19                dfs(node)
+20                count+=1
+21        return count
+22
