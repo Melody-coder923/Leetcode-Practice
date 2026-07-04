263d16d317f7d5b772a5e068dde394196a9b2e2e
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-04 12:34:24 -0700

    Add solution for Graph Valid Tree

--- LeetCode/Graph-Valid-Tree/Solution.py
+++ LeetCode/Graph-Valid-Tree/Solution.py
@@ -2,30 +2,26 @@
 2    def validTree(self, n: int, edges: List[List[int]]) -> bool:
 3        if len(edges)!=n-1:
 4            return False
-5        parent=[i for i in range(n)]
-6        rank=[0]*n
-7        def find(x):
-8            if parent[x]!=x:
-9                parent[x]=find(parent[x])
-10            return parent[x]
-11        
-12        def union(x,y):
-13            root_x= find(x)
-14            root_y= find(y)
-15
-16            if root_x==root_y:
-17                return False
-18            if rank[x]>rank[y]:
-19                parent[root_y] = root_x
-20            elif rank[x]<rank[y]:
-21                parent[root_x] = root_y
-22            else:
-23                parent[root_y] = root_x
-24                rank[root_x]+=1
-25            return True
-26        
-27        for a, b in edges:
-28            if not union(a,b):
-29                return False
-30        return True    
-31        
+5        graph=defaultdict(list)
+6        for u,v in edges:
+7            graph[u].append(v)
+8            graph[v].append(u)
+9        #如果有环就不是树
+10        visited=set()
+11        def dfs(node, parent):
+12            visited.add(node)
+13            for nei in graph[node]:
+14                if nei == parent:
+15                    continue
+16                if nei in visited:
+17                    return False
+18                if not dfs(nei, node):
+19                    return False
+20            return True
+21
+22        if not dfs(0, -1):
+23            return False
+24        return len(visited) == n
+25
+26
+27       
