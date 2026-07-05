5cbc8510d034b9a1b3a79cd750878a5270b4ae00
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-05 16:16:38 -0700

    Add solution for Gas Station

--- LeetCode/Gas-Station/Solution.py
+++ LeetCode/Gas-Station/Solution.py
@@ -1,14 +1,13 @@
 1class Solution:
 2    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-3        n=len(gas)
-4        cur=total=0
+3        n = len(gas)
+4        total, curr = 0, 0
 5        start=0
-6
-7        for i in range(n):
-8            cur= cur+gas[i]-cost[i]
-9            total=total+gas[i]-cost[i]
-10
-11            if cur<0:
-12                cur=0
-13                start=i+1
-14        return start if total>=0 else -1
+6        for i in range(n):
+7            total += gas[i] - cost[i]
+8            curr += gas[i] - cost[i]
+9            if curr < 0:
+10                # 当前 start 到 i 无法作为起点，跳过
+11                start = i + 1
+12                curr = 0
+13        return start if total >= 0 else -1
