b384bf126aca574ddf3686b17bdf914bbff0bcd8
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-04 18:23:30 -0700

    Add solution for Burst Balloons

--- LeetCode/Burst-Balloons/Solution.py
+++ LeetCode/Burst-Balloons/Solution.py
@@ -1,26 +1,13 @@
 1class Solution:
 2    def maxCoins(self, nums: List[int]) -> int:
-3        nums = [1] + nums + [1]
-4        n = len(nums)
-5
-6        @lru_cache(None)
-7        def dfs(left, right):
-8            # left 和 right 中间没有气球
-9            if right - left <= 1:
-10                return 0
-11
-12            res = 0
-13
-14            # 枚举最后戳爆的气球 k
-15            for k in range(left + 1, right):
-16                coin = (
-17                    dfs(left, k)
-18                    + nums[left] * nums[k] * nums[right]
-19                    + dfs(k, right)
-20                )
-21                res = max(res, coin)
-22
-23            return res
-24
-25        return dfs(0, n - 1)
-26
+3        nums=[1]+nums+[1]
+4        n=len(nums)
+5        dp= [[0]*n for _ in range(n)]
+6
+7        for length in range(2,n):
+8            for left in range(0,n-length):
+9                right=left+length
+10                for k in range(left+1,right):
+11                    coin=dp[left][k] + nums[left] * nums[k] * nums[right] + dp[k][right]
+12                    dp[left][right]=max(coin,dp[left][right])
+13        return dp[0][n-1]
