1d5d3c1998bcb083d3381057f16db3b38e7b693c
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-06 09:57:52 -0700

    Add solution for Edit Distance

--- LeetCode/Edit-Distance/Solution.py
+++ LeetCode/Edit-Distance/Solution.py
@@ -1,34 +1,5 @@
-1class Solution:
-2    def minDistance(self, word1: str, word2: str) -> int:
-3        m,n=len(word1),len(word2)
-4        dp=[[0]*(n+1) for _ in range(m+1)]
-5        for i in range(m+1):
-6            dp[i][0]=i
-7        for j in range(n+1):
-8            dp[0][j]=j
-9        
-10        for i in range(1,m+1):
-11            for j in range(1,n+1):
-12                if word1[i-1]==word2[j-1]:
-13                    dp[i][j]=dp[i-1][j-1]
-14                else:
-15                    dp[i][j]=min(
-16                    #insert
-17                    dp[i][j-1],
-18                    #delete
-19                    dp[i-1][j-1],
-20                    #replace
-21                    dp[i-1][j]
-22                    )+1
-23        return dp[m][n]
-24"""
-25插入看左边，删除看上面，替换看左上角
-26
-27       0 r o s
-28    0  0 1 2 3
-29    h. 1 1 2 3
-30    o. 2 2 1 2
-31    r. 3 2 2 2
-32    s  4 3 3 2
-33    e. 5 4 4 3
-34"""
+Edit distance to transform "word1" to "word2" =
+
+Minimum (Number of operations after deleting e from "word1", 
+Number of operations after inserting c in "word1",
+Number operations after replacing e with c in "word1") + 1
