ed63ed6cbff743ef7d727de2979d3103f100a46d
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-06 15:39:10 -0700

    Add solution for Rotting Oranges

--- LeetCode/Rotting-Oranges/Solution.py
+++ LeetCode/Rotting-Oranges/Solution.py
@@ -3,26 +3,26 @@
 3        m,n=len(grid),len(grid[0])
 4        fresh=0
 5        q=deque()
-6        for i in range(m):
-7            for j in range(n):
-8                if grid[i][j]==1:
-9                    fresh+=1
-10                elif grid[i][j]==2:
-11                    q.append((i,j))
-12        
-13        if fresh==0:
-14            return 0
-15        directions=[[0,1],[0,-1],[1,0],[-1,0]]
-16        time=0
-17        while q and fresh>0:
-18            for _ in range(len(q)):
-19                x, y = q.popleft()
+6        time=0
+7        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+8        for i in range(m):
+9            for j in range(n):
+10                if grid[i][j]==1:
+11                    fresh+=1
+12                elif grid[i][j]==2:
+13                    q.append((i,j,time))
+14        if fresh == 0:
+15            return 0
+16        while q:
+17            size=len(q)
+18            for _ in range(size):
+19                x,y,time=q.popleft()
 20                for dx,dy in directions:
-21                    nx,ny=x+dx,y+dy
+21                    nx, ny = x + dx, y + dy
 22                    if 0<=nx<m and 0<=ny<n and grid[nx][ny]==1:
-23                        fresh-=1
-24                        grid[nx][ny]=2
-25                        q.append((nx,ny))         
-26            time+=1
-27        return time if fresh == 0 else -1
-28
+23                        grid[nx][ny] = 2
+24                        q.append((nx,ny,time+1))
+25                        fresh-=1
+26                        if fresh == 0:
+27                            return time + 1
+28        return -1
