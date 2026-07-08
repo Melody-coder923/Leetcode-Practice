e3a86c9ec3a2a855439235f927d989bc9e91fe05
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-07 17:42:04 -0700

    Add solution for Word Ladder

--- LeetCode/Word-Ladder/Solution.py
+++ LeetCode/Word-Ladder/Solution.py
@@ -1,27 +1,30 @@
 1class Solution:
 2    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-3        wordList=set(wordList)
-4        if endWord not in wordList:
+3        wordSet = set(wordList)
+4        if endWord not in wordSet:
 5            return 0
-6        #build dict
-7        pattern=defaultdict(list)
-8        for word in wordList:
-9            for i in range(len(word)):
-10                w=word[:i]+"*"+word[i+1:]
-11                pattern[w].append(word)
-12        #bfs
-13        q = deque([(beginWord,1)])
-14        visited=set()
-15        visited.add(beginWord)
-16        while q:
-17            for _ in range(len(q)):
-18                word,level=q.popleft()
-19                if word==endWord:
-20                    return level
-21                for i in range(len(word)):
-22                    w=word[:i]+"*"+word[i+1:]
-23                    for p in pattern[w]:
-24                        if p not in visited:
-25                            q.append((p,level+1))
-26                            visited.add(p)
-27        return 0
+6
+7        graph=defaultdict(list)
+8        n = len(beginWord)
+9        for word in wordList:
+10            for i in range(n):
+11                p=word[:i]+"*"+word[i+1:]
+12                graph[p].append(word)
+13
+14        q = deque([(beginWord, 1)])
+15        visited = set([beginWord])
+16        
+17        while q:
+18            size=len(q)
+19            for _ in range(size):
+20                word,level=q.popleft()
+21                if word==endWord:
+22                    return level
+23              
+24                for i in range(n):
+25                    pattern = word[:i] + "*" + word[i + 1:]
+26                    for nei in graph[pattern]:
+27                        if nei not in visited:
+28                            visited.add(nei)
+29                            q.append((nei, level + 1))
+30        return 0
