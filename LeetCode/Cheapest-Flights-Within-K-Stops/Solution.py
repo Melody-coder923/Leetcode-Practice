133285d1ae2caf6e2ae2134694cbf1e038838434
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-08 15:54:31 -0700

    Add solution for Cheapest Flights Within K Stops

--- LeetCode/Cheapest-Flights-Within-K-Stops/Solution.py
+++ LeetCode/Cheapest-Flights-Within-K-Stops/Solution.py
@@ -1,12 +1,12 @@
 1class Solution:
-2    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int: 
-3        dist = [float("inf")] * n
-4        dist[src] = 0
-5        for i in range(k + 1):
-6            tmp = dist[:]
-7            for u, v, w in flights:
-8                if dist[u] != float("inf") and dist[u] + w < tmp[v]:  # ✅ 用 tmp[v] 判断和更新
-9                    tmp[v] = dist[u] + w
-10            dist=tmp
-11        return -1 if dist[dst] == float("inf") else dist[dst]
-12            
+2    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+3        check= [float("inf")]*n
+4        check[src]=0
+5
+6        for i in range(k+1):
+7            temp=check[:]
+8            for start,end,w in flights:
+9                if check[start] != float("inf") and check[start]+w<temp[end]:
+10                    temp[end] = check[start] + w
+11            check=temp
+12        return -1 if check[dst] == float("inf") else check[dst]
