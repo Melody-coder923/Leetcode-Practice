41eaeca3627e374e2881586b35edea6f4db11e14
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-13 19:17:59 -0700

    Add solution for Maximum Product Subarray

--- LeetCode/Maximum-Product-Subarray/Solution.py
+++ LeetCode/Maximum-Product-Subarray/Solution.py
@@ -1,11 +1,6 @@
-1class Solution:
-2    def maxProduct(self, nums: List[int]) -> int:
-3        maxcur=mincur=res=nums[0]
-4
-5        for num in nums[1:]:
-6            temp=maxcur
-7            maxcur=max(num,maxcur*num,mincur*num)
-8            mincur=min(num,temp*num,mincur*num)
-9
-10            res=max(res,maxcur)
-11        return res
+for i in [0...nums-1]: 
+    for j in [i..nums-1]: 
+        accumulator = 1
+        for k in [i..j]:
+            accumulator = accumulator * nums[k]
+        result = max(result, accumulator)
