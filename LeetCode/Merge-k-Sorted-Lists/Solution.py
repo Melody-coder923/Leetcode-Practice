3cb5479a0dc1158b099df7ceb3cceeb5b5bedbfd
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-13 19:14:46 -0700

    Add solution for Merge k Sorted Lists

--- LeetCode/Merge-k-Sorted-Lists/Solution.py
+++ LeetCode/Merge-k-Sorted-Lists/Solution.py
@@ -1,23 +1,22 @@
-1# Definition for singly-linked list.
-2# class ListNode:
-3#     def __init__(self, val=0, next=None):
-4#         self.val = val
-5#         self.next = next
-6class Solution:
-7    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-8        heap=[]
-9        for idx, node in enumerate(lists):
-10            if node:
-11                heapq.heappush(heap, (node.val, idx, node))
-12        
-13        newlist=ListNode(0)
-14        cur=newlist
-15        while heap:
-16            val,idx,node=heapq.heappop(heap)
-17            cur.next = node
-18            cur = cur.next
-19            if node.next:
-20                nxt=node.next
-21                heapq.heappush(heap,(nxt.val,idx,nxt))
-22        
-23        return newlist.next
+public ListNode mergeKLists(ListNode[] lists) {
+    List<Integer> l = new ArrayList<Integer>();
+   
+    for (ListNode ln : lists) {
+        while (ln != null) {
+            l.add(ln.val);
+            ln = ln.next;
+        }
+    }
+   
+    Collections.sort(l);
+ 
+    ListNode head = new ListNode(0);
+    ListNode h = head;
+    for (int i : l) {
+        ListNode t = new ListNode(i);
+        h.next = t;
+        h = h.next;
+    }
+    h.next = null;
+    return head.next;
+}
