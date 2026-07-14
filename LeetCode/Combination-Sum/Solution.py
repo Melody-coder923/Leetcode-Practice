0083f4169b9f84a008974a221bfd5a6a6dde1dd4
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-13 19:39:13 -0700

    Add solution for Combination Sum

--- LeetCode/Combination-Sum/Solution.py
+++ LeetCode/Combination-Sum/Solution.py
@@ -1,20 +1,17 @@
-1class Solution:
-2    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-3        res=[]
-4        candidates.sort()
-5
-6        def backtrack(start,path,remain):
-7            if remain==0:
-8                res.append(path[:])
-9                return 
-10            for i in range(start,len(candidates)):
-11                if candidates[i]>remain:
-12                    break
-13                path.append(candidates[i])
-14                backtrack(i,path,remain-candidates[i])
-15                path.pop()
-16
-17        backtrack(0, [], target)
-18        return res
-19
-20        
+public class Solution {
+public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    List<List<Integer>>[] dp = new List[target + 1];
+    for (int i = 0; i <= target; i++)
+        dp[i] = new ArrayList<>();
+    dp[0].add(new ArrayList<>());
+    for (int candidate: candidates) {
+        for (int j = candidate; j <= target; j++) {                
+            for (List<Integer> comb: dp[j - candidate]) {
+                List<Integer> newComb = new ArrayList(comb);
+                newComb.add(candidate);
+                dp[j].add(newComb);
+            }                    
+        }
+    }
+    return dp[target];
+}
