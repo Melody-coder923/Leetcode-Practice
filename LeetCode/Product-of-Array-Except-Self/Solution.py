ec48fb754abf340eb81007ce54b5c88ca2c5a593
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-15 20:12:40 -0700

    Add solution for Product of Array Except Self

--- LeetCode/Product-of-Array-Except-Self/Solution.py
+++ LeetCode/Product-of-Array-Except-Self/Solution.py
@@ -1,19 +1,14 @@
 1class Solution:
 2    def productExceptSelf(self, nums: List[int]) -> List[int]:
-3        p1=[]
-4        p2=[]
-5        res=[]
-6        prev=1
-7        for i in nums:
-8            p1.append(prev)
-9            prev*=i
-10        prev=1
-11        for j in nums[::-1]:
-12            p2.append(prev)
-13            prev*=j
-14        p2.reverse()
-15        for i in range(len(nums)):
-16            res.append(p1[i]*p2[i])
-17       
-18        
-19        return res
+3        result=[]
+4        if nums.count(0)>1:
+5            return [0]*len(nums)
+6        elif nums.count(0)==1:
+7            total=1
+8            for num in nums:
+9                if num !=0:
+10                    total*=num
+11            return [total if num==0 else 0 for num in nums]
+12        else:
+13            total = math.prod(nums)
+14            return [total//num for num in nums]
