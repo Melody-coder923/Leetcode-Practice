ff0d986b946369930bb0de2195702e2579e8a204
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-20 12:57:18 -0700

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.py
+++ LeetCode/Two-Sum/Solution.py
@@ -1,8 +1,8 @@
 1class Solution:
 2    def twoSum(self, nums: List[int], target: int) -> List[int]:
-3        dict={}
-4        for i,num in enumerate(nums):
-5            if target-num in dict:
-6                return [dict[target-num],i]
-7            dict[num]=i
+3        map=defaultdict(int)
+4        for idx,num in enumerate(nums):
+5            if target-num in map:
+6                return [idx,map[target-num]]
+7            map[num]=idx
 8        return -1
