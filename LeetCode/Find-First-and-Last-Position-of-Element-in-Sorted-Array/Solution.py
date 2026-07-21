c3134f92d98b89dcaa13e2bfd6a784ab1c7d2d8e
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-21 09:41:46 -0700

    Add solution for Find First and Last Position of Element in Sorted Array

--- LeetCode/Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.py
+++ LeetCode/Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.py
@@ -1,29 +1,33 @@
 1class Solution:
 2    def searchRange(self, nums: List[int], target: int) -> List[int]:
-3        def getLeft():
-4            left, right = 0, len(nums) - 1
-5            while left <= right:
-6                mid = (left + right) // 2                  
-7                if nums[mid] < target:
-8                    left = mid + 1
-9                else:
-10                    right = mid - 1
-11            return left
-12
-13        def getRight():
-14            left, right = 0, len(nums) - 1
-15            while left <= right:
-16                mid = (left + right) // 2 
-17                if nums[mid] <= target:
-18                    left = mid + 1
-19                else:
-20                    right = mid - 1
-21            return right
-22
-23        left_idx = getLeft()
-24        right_idx = getRight()
-25
-26        if left_idx <= right_idx:
-27            return [left_idx, right_idx]
-28        else:
-29            return [-1, -1]
+3        def findleft(l,r):
+4            ans=-1
+5            while l<=r:
+6                mid=(l+r)//2
+7                if nums[mid]>=target:
+8                    ans=mid
+9                    r=mid-1
+10                else:
+11                    l=mid+1
+12            return ans
+13        
+14        def findright(l,r):
+15            ans=-1
+16            while l<=r:
+17                mid=(l+r)//2
+18                if nums[mid]<=target:
+19                    ans=mid
+20                    l=mid+1
+21                else:
+22                    r=mid-1
+23            return ans
+24
+25        n=len(nums)
+26        if n == 0:
+27            return [-1, -1]
+28
+29        left = findleft(0, n - 1)
+30        if left == -1 or nums[left] != target:
+31            return [-1, -1]
+32        right = findright(0, n - 1)
+33        return [left, right]
