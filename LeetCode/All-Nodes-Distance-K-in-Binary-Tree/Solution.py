45803151354f1c50ac11b6274f6b801415206f1f
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-21 09:21:23 -0700

    Add solution for All Nodes Distance K in Binary Tree

--- LeetCode/All-Nodes-Distance-K-in-Binary-Tree/Solution.py
+++ LeetCode/All-Nodes-Distance-K-in-Binary-Tree/Solution.py
@@ -7,32 +7,29 @@
 7
 8class Solution:
 9    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-10        #build parent relations
-11        n_to_parent = {}
-12
-13        def findparent(node):
-14            if not node:
-15                return
-16            if node.left:
-17                n_to_parent[node.left]=node
-18                findparent(node.left)
-19            if node.right:
-20                n_to_parent[node.right]=node
-21                findparent(node.right)
-22        findparent(root)
-23
-24        q=deque([(target,0)])
+10        parent_map={}
+11        def find_parent(node,parent):
+12            if not node:
+13                return
+14            if node.left:
+15                parent_map[node.left]=node
+16                find_parent(node.left,node)
+17            if node.right:
+18                parent_map[node.right]=node
+19                find_parent(node.right,node)
+20        
+21        find_parent(root,None)
+22
+23        q=deque([(target,0)])
+24        visited={target}
 25        res=[]
-26        visited={target}
-27        while q:
-28            size=len(q)
-29            for _ in range(size):
-30                node,dis=q.popleft()
-31                if dis==k:
-32                    res.append(node.val)
-33                    continue
-34                for nxt in (node.left, node.right,n_to_parent.get(node)):
-35                    if nxt and nxt not in visited:
-36                        q.append((nxt,dis+1))
-37                        visited.add(nxt)
-38        return res
+26        while q:
+27            node,dis=q.popleft()
+28            if dis==k:
+29                res.append(node.val)
+30                continue
+31            for nei in [node.left,node.right,parent_map.get(node)]:
+32                if nei and nei not in visited:
+33                    q.append((nei,dis+1))
+34                    visited.add(nei)
+35        return res
