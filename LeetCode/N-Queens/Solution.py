6644b52de879e1bf11ace5022020853ed94392af
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-22 16:21:52 -0700

    Add solution for N-Queens

--- LeetCode/N-Queens/Solution.py
+++ LeetCode/N-Queens/Solution.py
@@ -1,32 +1,39 @@
 1class Solution:
 2    def solveNQueens(self, n: int) -> List[List[str]]:
-3        result = []
-4        board = ['.' * n for _ in range(n)] 
+3        res=[]
+4        board=["."*n for _ in range(n)]
 5        def backtrack(row):
-6            if row == n:
-7                result.append(board[:])
-8                return
-9            for col in range(n):
-10                if is_valid(board, row, col):
-11                    board[row] = board[row][:col] + 'Q' + board[row][col+1:]
-12                    backtrack(row + 1)
-13                    board[row] = board[row][:col] + '.' + board[row][col+1:]
-14        def is_valid(board, row, col):
-15            for i in range(row):
-16                if board[i][col] == 'Q':
-17                    return False
-18            i, j = row - 1, col - 1
-19            while i >= 0 and j >= 0:
-20                if board[i][j] == 'Q':
-21                    return False
-22                i -= 1
-23                j -= 1
-24            i, j = row - 1, col + 1
-25            while i >= 0 and j < n:
-26                if board[i][j] == 'Q':
-27                    return False
-28                i -= 1
-29                j += 1
-30            return True
-31        backtrack(0)
-32        return result
+6            if row==n:
+7                res.append(board[:])
+8                return 
+9            
+10            for col in range(n):
+11                if valid(board,row,col):
+12                    board[row]=board[row][:col]+"Q"+board[row][col+1:]
+13                    backtrack(row+1)
+14                    board[row]=board[row][:col]+"."+board[row][col+1:]
+15
+16
+17        def valid(board,row,col):
+18            for i in range(row):
+19                if board[i][col]=="Q":
+20                    return False
+21                
+22            i,j=row-1,col-1
+23            while i>=0 and j>=0:
+24                if board[i][j]=="Q":
+25                    return False
+26                i-=1
+27                j-=1
+28        
+29            i,j=row-1,col+1
+30            while i>=0 and j<n:
+31                if board[i][j]=="Q":
+32                    return False
+33                i-=1
+34                j+=1
+35            return True
+36
+37
+38        backtrack(0)
+39        return res
