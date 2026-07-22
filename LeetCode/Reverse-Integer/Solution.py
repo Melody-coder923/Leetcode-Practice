03d73a2303c04da0b32f5576b037d014f4919e87
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-22 15:43:46 -0700

    Add solution for Reverse Integer

--- LeetCode/Reverse-Integer/Solution.py
+++ LeetCode/Reverse-Integer/Solution.py
@@ -2,16 +2,15 @@
 2    def reverse(self, x: int) -> int:
 3        sign=-1 if x<0 else 1
 4        num=abs(x)
-5        mod=0
-6        res=0
-7        while num>0:
-8            mod=num%10 
-9            num=num//10
-10            res=res*10+mod 
-11        
-12        res=res*sign
-13        if res<-2**31 or res>2**31-1:
-14            return 0
-15        else:
-16            return res
-17
+5        res=0
+6        while num>0:
+7            mod=num%10 
+8            num=num//10
+9            res=res*10+mod 
+10        
+11        res=res*sign
+12        if res<-2**31 or res>2**31-1:
+13            return 0
+14        else:
+15            return res
+16
