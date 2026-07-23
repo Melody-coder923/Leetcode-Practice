4dba83971ad7ae8ab877724b4ae6d8d7fe6ad1c1
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-23 14:37:19 -0700

    Add solution for Next Permutation

--- LeetCode/Next-Permutation/Solution.py
+++ LeetCode/Next-Permutation/Solution.py
@@ -3,26 +3,28 @@
 3        """
 4        Do not return anything, modify nums in-place instead.
 5        """
-6        # scan from right to left to find the first nums[i]>=nums[i+1]
+6        #1. 从右边找第一个下降点  12354，就是3
 7        n=len(nums)
 8        if n <= 1:
 9            return
 10        i=n-2
 11        while i>=0 and nums[i]>=nums[i+1]:
 12            i-=1
-13        # i will stop at the nums[i]> nums[i+1]     1 2 3 5 4      1 2  4 3 4
-14        if i < 0:  # 全递减，如 3 2 1
+13
+14        if i < 0:
 15            nums.reverse()
 16            return
 17        
-18        # find the first bigger number from right
+18        #2. 找右侧第一个比nums[i]大的
 19        j=n-1
-20        while nums[j]<=nums[i]:
+20        while j>i and nums[j]<=nums[i]:
 21            j-=1
 22        nums[i],nums[j]=nums[j],nums[i]
-23        
-24        left,right=i+1,n-1
-25        while left<right:
-26            nums[left], nums[right] = nums[right], nums[left]
-27            left += 1
-28            right -= 1
+23
+24        #3.反转后面的数组
+25        l,r=i+1,n-1
+26        while l<r:
+27            nums[l],nums[r]=nums[r],nums[l]
+28            l+=1
+29            r-=1
+30        
