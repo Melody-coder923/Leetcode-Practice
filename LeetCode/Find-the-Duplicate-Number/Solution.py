923abda133970e10cdf03223e7e3dd19de2416ea
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-23 11:50:30 -0700

    Add solution for Find the Duplicate Number

--- LeetCode/Find-the-Duplicate-Number/Solution.py
+++ LeetCode/Find-the-Duplicate-Number/Solution.py
@@ -1,15 +1,15 @@
-1#当作链表"
-2class Solution:
-3    def findDuplicate(self, nums: List[int]) -> int:
-4        fast=slow=nums[0]
-5        while True:
+1class Solution:
+2    def findDuplicate(self, nums: List[int]) -> int:
+3        fast=slow=nums[0]
+4        while True:
+5            fast=nums[nums[fast]]
 6            slow=nums[slow]
-7            fast=nums[nums[fast]]
-8            if slow==fast:
-9                break
+7            if fast==slow:
+8                break
+9        
 10        slow=nums[0]
-11        while slow!=fast:
+11        while fast!=slow:
 12            slow=nums[slow]
 13            fast=nums[fast]
-14        return slow
-15
+14        
+15        return slow
