b9d7933407ef96fe62e715b0e1f4305c57473bd4
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-27 16:59:02 -0700

    Add solution for Binary Tree Maximum Path Sum

--- LeetCode/Binary-Tree-Maximum-Path-Sum/Solution.py
+++ LeetCode/Binary-Tree-Maximum-Path-Sum/Solution.py
@@ -6,19 +6,29 @@
 6#         self.right = right
 7class Solution:
 8    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-9        if not root:
-10            return 0
-11        res = float("-inf")
-12        def dfs(node):
-13            nonlocal res
-14            if not node:
-15                return 0
-16            left=max(0,dfs(node.left))
-17            right=max(0,dfs(node.right))
-18
-19            res= max(res,left+right+node.val)
-20
-21            return max(left,right)+node.val
-22        
-23        dfs(root)
-24        return res
+9        """
+10        input: root+binary tree
+11        output: path sum(every node only one time+ no need to pass the root)
+12
+13        孩子告诉父亲： 我单侧最大的path是多少 def -return 孩子告诉父亲
+14        父亲决策： 左右孩子加一起比较全局最大值def 内部更新
+15        """
+16        res=float("-inf")
+17
+18        def dfs(node):
+19            nonlocal res
+20            #base case
+21            if not node:
+22                return 0
+23
+24            left=max(dfs(node.left),0)
+25            right=max(dfs(node.right),0)
+26            
+27            #postorder-父亲的决策更新
+28            res=max(left+right+node.val,res)
+29
+30            return max(left,right)+node.val
+31        
+32        #调整体程序返回结果
+33        dfs(root)
+34        return res
