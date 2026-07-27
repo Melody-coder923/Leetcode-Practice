a7985b1552dac6f6e38e21786727d5175aff0329
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-27 09:05:19 -0700

    Add solution for Sqrt(x)

--- LeetCode/Sqrt(x)/Solution.py
+++ LeetCode/Sqrt(x)/Solution.py
@@ -1,15 +1,16 @@
 1class Solution:
 2    def mySqrt(self, x: int) -> int:
-3        if x==0 or x==1:
+3        if x<=1:
 4            return x
-5        
-6        l,r=0,x//2
+5
+6        l,r=1,x//2
 7        while l<=r:
-8            mid= (r+l)//2
+8            mid=(l+r)//2
 9            if mid*mid==x:
 10                return mid
-11            elif mid*mid>x:
-12                r=mid-1
-13            else:
-14                l=mid+1
-15        return r
+11
+12            elif mid*mid<x:
+13                l=mid+1
+14            else:
+15                r=mid-1
+16        return r
