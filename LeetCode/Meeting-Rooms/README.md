806cfd5441c3362732f1d1ba4ee362b2de1c746a
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-28 15:10:09 -0700

    Add solution for Meeting Rooms

--- LeetCode/Meeting-Rooms/README.md
+++ LeetCode/Meeting-Rooms/README.md
@@ -1,6 +1,10 @@
 # Meeting Rooms
 
-Can you solve this real interview question? Meeting Rooms - Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+Can you solve this real interview question? Meeting Rooms - You are given an array of meeting times intervals where intervals[i] = [starti, endi].
+
+A person can attend all meetings if no two meeting intervals overlap. Meetings ending at time t and starting at time t do not overlap.
+
+Return true if a person can attend all meetings. Otherwise, return false.
 
  
 
--- LeetCode/Meeting-Rooms/Solution.py
+++ LeetCode/Meeting-Rooms/Solution.py
@@ -1,13 +1,14 @@
 1class Solution:
 2    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-3        if not intervals:
-4            return True
-5        intervals.sort()
-6        prev=intervals[0][1]
-7        for start,end in intervals[1:]:
-8            if prev>start:
-9                return False
-10            prev=max(prev,end)
-11        
-12        return True
-13
+3        n=len(intervals)
+4        if n<=1:
+5            return True
+6        # 开始时间排序
+7        intervals.sort()
+8        #比较对象
+9        prev_end=intervals[0][1]
+10        for start,end in intervals[1:]:
+11            if start<prev_end:
+12                return False
+13            prev_end=end
+14        return True
