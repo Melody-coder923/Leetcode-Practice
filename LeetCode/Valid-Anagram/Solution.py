f3921a189f0d57020a507f743495e960d05fafa9
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-28 15:11:56 -0700

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.py
+++ LeetCode/Valid-Anagram/Solution.py
@@ -1,17 +1,19 @@
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> smap=new HashMap<>();
-        int sl=s.length();
-        int tl=t.length();
-        if(sl!=tl){return false;}
-        for(int i=0;i<sl;i++){
-            smap.put(s.charAt(i),smap.getOrDefault(s.charAt(i),0)+1);
-            smap.put(t.charAt(i),smap.getOrDefault(t.charAt(i),0)-1);
-        }
-        for(char c:smap.keySet()){
-            if(smap.get(c)!=0){return false;}
-        }
-        
-        return true;
-    }
-}
+1class Solution:
+2    def isAnagram(self, s: str, t: str) -> bool:
+3        if len(s) != len(t):
+4            return False
+5        count={}
+6        for char in s:
+7            if char in count:
+8                count[char]+=1
+9            else:
+10                count[char]=1
+11        for char in t:
+12            if char in count:
+13                count[char]-=1
+14            else:
+15                return False
+16        for value in count.values():
+17            if value!= 0:
+18                return False
+19        return True
