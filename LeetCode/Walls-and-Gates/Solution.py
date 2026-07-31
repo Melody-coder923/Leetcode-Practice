fcf78fbed107aef658830657182c9803d66ee3da
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-31 12:02:34 -0700

    Add solution for Walls and Gates

--- LeetCode/Walls-and-Gates/Solution.py
+++ LeetCode/Walls-and-Gates/Solution.py
@@ -3,22 +3,21 @@
 3        """
 4        Do not return anything, modify rooms in-place instead.
 5        """
-6        m,n=len(rooms),len(rooms[0])
-7        INF=2147483647
+6        INF=2**31-1
+7        m,n=len(rooms),len(rooms[0])
 8        q=deque()
-9        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-10        for i in range(m):
-11            for j in range(n):
-12                if rooms[i][j]== 0:
-13                    q.append((i,j,0))
-14        
-15        while q:
-16            size=len(q)
-17            for _ in range(size):
-18                x,y,level= q.popleft()
-19                for dx,dy in directions:
-20                    nx,ny=x+dx,y+dy
-21                    if 0<=nx<m and 0<=ny<n and rooms[nx][ny]==INF:
-22                        q.append((nx,ny,level+1))
-23                        rooms[nx][ny]=level+1
-24
+9        for i in range(m):
+10            for j in range(n):
+11                if rooms[i][j]==0:
+12                    q.append((i,j,0))
+13        directions=[(1,0),(-1,0),(0,1),(0,-1)]
+14        while q:
+15            size=len(q)
+16            for _ in range(size):
+17                x,y,dis=q.popleft()
+18                for dx,dy in directions:
+19                    nx,ny=x+dx,y+dy
+20                    if 0<=nx<m and 0<=ny<n and rooms[nx][ny]==INF:
+21                        q.append((nx,ny,dis+1))
+22                        rooms[nx][ny]=dis+1
+23        
