c7584d23690441e5f995c0322330e52b29027590
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-30 17:50:40 -0700

    Add solution for Sort an Array

--- LeetCode/Sort-an-Array/Solution.py
+++ LeetCode/Sort-an-Array/Solution.py
@@ -1,30 +1,32 @@
 1class Solution:
 2    def sortArray(self, nums: List[int]) -> List[int]:
-3        #归并排序 -linkedlist arr1 arr2- merge
-4
-5        def merge(l1,l2):
-6            m,n=len(l1),len(l2)
-7            i,j=0,0
-8            res=[]
-9            while i <m and j<n:
-10                if l1[i]<=l2[j]:
-11                    res.append(l1[i])
-12                    i+=1
-13                else:
-14                    res.append(l2[j])
-15                    j+=1
-16
-17            res.extend(l1[i:])
-18            res.extend(l2[j:])
-19            return res
-20        
-21        def merge_sort(arr):
-22            #base case
-23            if len(arr)<=1:
-24                return arr
-25            mid=len(arr)//2
-26            left=merge_sort(arr[:mid])
-27            right=merge_sort(arr[mid:])
-28            return merge(left,right)
-29           
-30        return merge_sort(nums)
+3        def merge(left,right):
+4            m,n=len(left),len(right)
+5            i=j=0
+6            res=[]
+7            while i<m and j<n:
+8                if left[i]<=right[j]:
+9                    res.append(left[i])
+10                    i+=1
+11                else:
+12                    res.append(right[j])
+13                    j+=1
+14
+15            res.extend(left[i:])
+16            res.extend(right[j:])
+17
+18            return res
+19        
+20        def split(arr):
+21            if len(arr) <= 1:
+22                return arr
+23            mid = len(arr) // 2
+24            left=split(arr[mid:])
+25            right=split(arr[:mid])
+26
+27            return merge(left,right)
+28
+29        return split(nums)
+30
+31
+32
