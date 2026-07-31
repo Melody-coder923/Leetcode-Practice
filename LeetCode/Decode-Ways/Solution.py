9e1c11dbbb70afc9bf8d320b9855f8abc7bad1d6
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-31 14:09:44 -0700

    Add solution for Decode Ways

--- LeetCode/Decode-Ways/Solution.py
+++ LeetCode/Decode-Ways/Solution.py
@@ -1,15 +1,26 @@
 1class Solution:
 2    def numDecodings(self, s: str) -> int:
-3        n=len(s)
-4        if s[0]=="0":
-5            return 0
-6
-7        dp=[0]*(n+1)
-8        dp[0]=1
-9        for i in range(1,n+1): #i-1是当下坐标
-10            if s[i-1]!="0":
-11                dp[i]+=dp[i-1]
-12            if i-2>=0 and 10<=int(s[i-2:i])<=26:
-13                dp[i]+=dp[i-2]
-14        
-15        return dp[n]
+3        #edge case
+4        n=len(s)
+5        if n==1 and s[0]!="0":
+6            return 1
+7        if s[0]=="0":
+8            return 0
+9        
+10        @lru_cache(None)
+11        def dfs(i):
+12            #base case
+13            if i<0:
+14                return 1
+15            res=0
+16            #single
+17            if s[i]!="0":
+18                res+=dfs(i-1)
+19            
+20            #double
+21            if i-1>=0 and 10<=int(s[i-1:i+1])<=26:
+22                res+=dfs(i-2)
+23            
+24            return res
+25        
+26        return dfs(n-1)
