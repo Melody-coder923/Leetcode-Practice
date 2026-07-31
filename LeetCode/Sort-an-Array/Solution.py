8480ed63b5bc4251c65c08dd8b1cb0fef651de1e
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-30 17:50:34 -0700

    Add solution for Sort an Array

--- LeetCode/Sort-an-Array/Solution.py
+++ LeetCode/Sort-an-Array/Solution.py
@@ -1,26 +1,30 @@
-    private void merge(int[] nums, int low, int mid, int high, int[] temp) {
-        for(int i = low; i <= mid; i++) {
-            temp[i] = nums[i];
-        }
-
-        for(int i = mid + 1; i <= high; i++) {
-            temp[i] = nums[i];
-        }
-
-        int i = low, j = mid + 1;
-        while(i <= mid && j <= high) {
-            if(temp[i] < temp[j]) {
-                nums[low++] = temp[i++];
-            } else {
-                nums[low++] = temp[j++];
-            }
-        }
-
-        while(i <= mid) {
-            nums[low++] = temp[i++];
-        }
-
-        while(j <= high) {
-            nums[low++] = temp[j++];
-        }   
-    }
+1class Solution:
+2    def sortArray(self, nums: List[int]) -> List[int]:
+3        #归并排序 -linkedlist arr1 arr2- merge
+4
+5        def merge(l1,l2):
+6            m,n=len(l1),len(l2)
+7            i,j=0,0
+8            res=[]
+9            while i <m and j<n:
+10                if l1[i]<=l2[j]:
+11                    res.append(l1[i])
+12                    i+=1
+13                else:
+14                    res.append(l2[j])
+15                    j+=1
+16
+17            res.extend(l1[i:])
+18            res.extend(l2[j:])
+19            return res
+20        
+21        def merge_sort(arr):
+22            #base case
+23            if len(arr)<=1:
+24                return arr
+25            mid=len(arr)//2
+26            left=merge_sort(arr[:mid])
+27            right=merge_sort(arr[mid:])
+28            return merge(left,right)
+29           
+30        return merge_sort(nums)
