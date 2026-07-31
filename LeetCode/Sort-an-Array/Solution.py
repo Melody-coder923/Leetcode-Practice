6541d3a128dce51b24fc919f856519f2786e761b
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-30 17:50:46 -0700

    Add solution for Sort an Array

--- LeetCode/Sort-an-Array/Solution.py
+++ LeetCode/Sort-an-Array/Solution.py
@@ -1,32 +1,21 @@
 1class Solution:
 2    def sortArray(self, nums: List[int]) -> List[int]:
-3        def merge(left,right):
-4            m,n=len(left),len(right)
-5            i=j=0
-6            res=[]
-7            while i<m and j<n:
-8                if left[i]<=right[j]:
-9                    res.append(left[i])
-10                    i+=1
-11                else:
-12                    res.append(right[j])
-13                    j+=1
-14
-15            res.extend(left[i:])
-16            res.extend(right[j:])
-17
-18            return res
-19        
-20        def split(arr):
-21            if len(arr) <= 1:
-22                return arr
-23            mid = len(arr) // 2
-24            left=split(arr[mid:])
-25            right=split(arr[:mid])
-26
-27            return merge(left,right)
-28
-29        return split(nums)
-30
-31
-32
+3        if len(nums)<=1:
+4             return nums
+5        num=len(nums)//2
+6        left=self.sortArray(nums[:num])
+7        right=self.sortArray(nums[num:])
+8        return self.merge(left,right)
+9    def merge(self,left,right):
+10        l,r=0,0
+11        result=[]
+12        while l<len(left) and r<len(right):
+13            if left[l]<right[r]:
+14                result.append(left[l])
+15                l+=1
+16            else:
+17                result.append(right[r])
+18                r+=1
+19        result+= left[l: ]
+20        result+= right[r:]
+21        return result           
