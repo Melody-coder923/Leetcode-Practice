16cdbd0ea96eb8cdaa09e63d0c17e0c5856a157f
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-31 12:59:53 -0700

    Add solution for Smallest Subsequence of Distinct Characters

--- LeetCode/Smallest-Subsequence-of-Distinct-Characters/Solution.py
+++ LeetCode/Smallest-Subsequence-of-Distinct-Characters/Solution.py
@@ -1,21 +1,23 @@
 1class Solution:
 2    def smallestSubsequence(self, s: str) -> str:
-3        map={}
-4        
-5        for idx,c in enumerate(s):
-6            map[c]=idx
-7        
-8        stack=[]
-9        seen=set()
-10        for idx,char in enumerate(s):
-11            if char in seen:
-12                continue
-13            while stack and stack[-1]>char and map[stack[-1]]>idx:
-14                removed=stack.pop()
-15                seen.remove(removed)
-16            
-17            #如果没有更小呢
-18            stack.append(char)
-19            seen.add(char)
-20        
-21        return "".join(stack)
+3        n=len(s)
+4        if n==1:
+5            return s
+6        last={}
+7        for idx,char in enumerate(s):
+8            last[char]=idx
+9
+10        stack=[]
+11        seen=set()
+12        for idx,char in enumerate(s):
+13            if char in seen:
+14                continue
+15            while stack and stack[-1]>char and last[stack[-1]]>idx:
+16                removed=stack.pop()
+17                seen.remove(removed)
+18            
+19            stack.append(char)
+20            seen.add(char)
+21        
+22        return "".join(stack)
+23            
