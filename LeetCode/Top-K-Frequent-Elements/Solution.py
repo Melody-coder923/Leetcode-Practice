9374afc8a36a150c743738354cee972e7fa22992
Yan Zhao <zhao.y4@northeastern.edu>
2026-07-31 22:43:51 -0700

    Add solution for Top K Frequent Elements

--- LeetCode/Top-K-Frequent-Elements/Solution.py
+++ LeetCode/Top-K-Frequent-Elements/Solution.py
@@ -1,18 +1,6 @@
-1from collections import Counter
-2class Solution:
-3    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-4        min_heap=[]
-5        count=Counter(nums)
-6        for value,freq in count.items():
-7                if len(min_heap)<k:
-8                    heapq.heappush(min_heap,(freq,value))
-9                elif min_heap[0][0]<freq:
-10                        heapq.heappop(min_heap)
-11                        heapq.heappush(min_heap,(freq,value))
-12        res=[]
-13        while min_heap:
-14            freq,value=heapq.heappop(min_heap)
-15            res.append(value)
-16        return res
-17        
-18       
+def topKFrequent(self, nums, k):
+    bucket = [[] for _ in range(len(nums) + 1)]
+    Count = Counter(nums).items()  
+    for num, freq in Count: bucket[freq].append(num) 
+    flat_list = [item for sublist in bucket for item in sublist]
+    return flat_list[::-1][:k]
