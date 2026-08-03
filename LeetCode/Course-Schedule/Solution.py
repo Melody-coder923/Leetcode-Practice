8ba50d29fd824f28eb6615648b5289464d59f24a
Yan Zhao <zhao.y4@northeastern.edu>
2026-08-03 16:27:11 -0700

    Add solution for Course Schedule

--- LeetCode/Course-Schedule/Solution.py
+++ LeetCode/Course-Schedule/Solution.py
@@ -1,10 +1,29 @@
-2 
-[[1,0]]
-2
-[[1,0],[0,1]]
-5
-[[1,4],[2,4],[3,1],[3,2]]
-6
-[[1,0],[1,2],[3,1],[2,3],[2,4],[4,5],[2,5]]
-6
-[[1,0],[1,2],[3,1],[3,2],[2,4],[4,5],[2,5]]
+1class Solution:
+2    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+3        # course,pre_course
+4        dict=defaultdict(list)
+5        indegree=[0]*numCourses
+6        for course,pre_course in prerequisites:
+7            dict[pre_course].append(course)
+8            indegree[course]+=1
+9        
+10        q=deque()
+11        for course_no in range(numCourses):
+12            if indegree[course_no]==0:
+13                q.append(course_no)
+14
+15        count=0
+16        while q:
+17            cur=q.popleft()
+18            count+=1
+19            if count==numCourses:
+20                return True
+21            for nxt in dict[cur]:
+22                indegree[nxt]-=1
+23                if indegree[nxt]==0:
+24                    q.append(nxt)
+25
+26        return count==numCourses
+27
+28        
+29
