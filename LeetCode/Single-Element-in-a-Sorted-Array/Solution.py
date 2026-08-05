8a2f956a6ff6473078effc1db961f66416666201
Yan Zhao <zhao.y4@northeastern.edu>
2026-08-04 20:11:58 -0700

    Add solution for Single Element in a Sorted Array

--- LeetCode/Single-Element-in-a-Sorted-Array/Solution.py
+++ LeetCode/Single-Element-in-a-Sorted-Array/Solution.py
@@ -1,15 +1,17 @@
 1class Solution:
 2    def singleNonDuplicate(self, nums: List[int]) -> int:
-3        l,r=0,len(nums)-1
-4        while l<r:
-5            mid=(l+r)//2
-6            if mid%2==1:
-7                mid-=1
-8            if nums[mid]!=nums[mid+1]:
-9                r=mid
-10            else:
-11                l=mid+2
-12        
-13        return nums[l]
-14    
-15  
+3        if len(nums)==1:
+4            return nums[0]
+5
+6        l,r=0,len(nums)-1
+7        while l<r:
+8            mid=(l+r)//2
+9            if mid%2==1:
+10                mid-=1
+11            if nums[mid]!=nums[mid+1]:
+12                r=mid
+13            else:
+14                l=mid+2
+15        
+16        return nums[l]
+17
