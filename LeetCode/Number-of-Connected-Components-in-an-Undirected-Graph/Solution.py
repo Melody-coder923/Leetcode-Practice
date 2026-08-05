ffca15531cb2667d756de5069f5b28a1ecf7a48e
Yan Zhao <zhao.y4@northeastern.edu>
2026-08-04 22:56:35 -0700

    Add solution for Number of Connected Components in an Undirected Graph

--- LeetCode/Number-of-Connected-Components-in-an-Undirected-Graph/Solution.py
+++ LeetCode/Number-of-Connected-Components-in-an-Undirected-Graph/Solution.py
@@ -1,26 +1,27 @@
 1class Solution:
 2    def countComponents(self, n: int, edges: List[List[int]]) -> int:
-3        # build relation- dict  node -[]
-4        dict=defaultdict(list)
-5        for u,v in edges:
-6            dict[u].append(v)
-7            dict[v].append(u)
-8
-9        # dfs  visited -group
-10        visited=set()
-11
-12        def dfs(node):
-13            visited.add(node)
-14            for nei in dict[node]:
-15                if nei not in visited:
-16                    dfs(nei)
-17    
-18
-19        # for +dfs -> group +1
-20        group=0
-21        for node in range(n):
-22            if node not in visited:
-23                dfs(node)
-24                group+=1
-25        
-26        return group
+3        parents=list(range(n)) #[0,1,2,3,5]
+4
+5        def find(x):
+6            if parents[x]!=x:
+7                parents[x]=find(parents[x])
+8            return parents[x]
+9        
+10        def union(x,y):
+11            root_x=find(x)
+12            root_y=find(y)
+13            if root_x==root_y:
+14                return False
+15            
+16            else:
+17                parents[root_y]=root_x
+18                return True
+19        
+20        
+21        for u,v in edges:
+22            if union(u,v):
+23                n-=1
+24        
+25        return n
+26
+27
