7da38633e0013f6401a4feafeffbc692805cfe4a
Yan Zhao <zhao.y4@northeastern.edu>
2026-08-04 22:03:55 -0700

    Add solution for Set Matrix Zeroes

--- LeetCode/Set-Matrix-Zeroes/Solution.py
+++ LeetCode/Set-Matrix-Zeroes/Solution.py
@@ -1,7 +1,32 @@
-[[1,1,1],[1,0,1],[1,1,1]]
-[[0,1,2,0],[3,4,5,2],[1,3,1,5]]
-[[1],[0]]
-[[0,1,1],[1,1,1],[1,0,0]]
-[[-4,-2147483648,6,-7,0],[-8,6,-8,-6,0],[2147483647,2,-9,-6,-10]]
-[[2147483647],[2],[3]]
-[[1,2,3,4],[5,0,7,8],[0,10,11,12],[13,14,15,0]]
+1class Solution:
+2    def setZeroes(self, matrix: List[List[int]]) -> None:
+3        """
+4        Do not return anything, modify matrix in-place instead.
+5        """
+6        # 遍历 找0 然后 做记录哪些行和列受到影响
+7        m,n=len(matrix),len(matrix[0])
+8        firstrow=False
+9        firstcol=False
+10        for i in range(m):
+11            if matrix[i][0]==0:
+12                firstcol=True
+13        for j in range(n):
+14            if matrix[0][j]==0:
+15                firstrow=True
+16        for i in range(1,m):
+17            for j in range(1,n):
+18                if matrix[i][j]==0:
+19                    matrix[i][0]=0
+20                    matrix[0][j]=0
+21        for i in range(1, m):
+22            for j in range(1, n):
+23                if matrix[i][0]==0 or matrix[0][j]==0:
+24                    matrix[i][j]=0
+25        if firstcol:
+26            for i in range(m):
+27                matrix[i][0]=0
+28        if firstrow:
+29            for i in range(n):
+30                matrix[0][i]=0
+31    
+32
