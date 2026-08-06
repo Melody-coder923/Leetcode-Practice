cb5f7994c0ec79c13d1c0cf3b8b643ec06f9b15e
Yan Zhao <zhao.y4@northeastern.edu>
2026-08-05 21:53:05 -0700

    Add solution for Insert Interval

--- LeetCode/Insert-Interval/Solution.py
+++ LeetCode/Insert-Interval/Solution.py
@@ -1,24 +1,23 @@
 1class Solution:
 2    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-3        res = []
-4
-5        for start, end in intervals:
-6            # 情况1：当前区间在 newInterval 左边
-7            if end < newInterval[0]:
-8                res.append([start, end])
-9
-10            # 情况2：当前区间在 newInterval 右边
-11            elif start > newInterval[1]:
-12                res.append(newInterval)
-13                newInterval = [start, end]
-14
-15            # 情况3：有重叠，合并到 newInterval
+3        #intervals = [[1,3],[6,9]], newInterval = [2,5]
+4        res=[]
+5        for i in range(len(intervals)):
+6            #前面
+7            if newInterval[1]<intervals[i][0]:
+8                res.append(newInterval)
+9                res.extend(intervals[i:])
+10                return res
+11            #重合
+12            elif newInterval[0]<=intervals[i][1]:
+13                newInterval[0]=min(intervals[i][0],newInterval[0])
+14                newInterval[1]=max(intervals[i][1],newInterval[1])
+15            #后面
 16            else:
-17                newInterval[0] = min(newInterval[0], start)
-18                newInterval[1] = max(newInterval[1], end)
-19
-20        res.append(newInterval)
+17                res.append(intervals[i])
+18        
+19        res.append(newInterval)
+20
 21        return res
 22
-23
-24
+23    
