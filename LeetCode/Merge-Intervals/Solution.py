94088f948caa29ce8aecc49cc952cc01ad3b8200
Yan Zhao <zhao.y4@northeastern.edu>
2026-08-05 22:13:54 -0700

    Add solution for Merge Intervals

--- LeetCode/Merge-Intervals/Solution.py
+++ LeetCode/Merge-Intervals/Solution.py
@@ -1,14 +1,20 @@
 1class Solution:
 2    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-3        intervals.sort()
-4        prev_end=intervals[0][-1]
-5        res=[intervals[0]]
-6        for start,end in intervals[1:]:
-7            if start>prev_end:
-8                res.append([start,end])
-9            else:
-10                res[-1][1]=max(end,res[-1][1])
-11            prev_end=res[-1][-1]
-12        
-13        return res
-14
+3        n=len(intervals)
+4        if n<=1:
+5            return intervals
+6        intervals.sort()
+7        res=[intervals[0]]
+8        for i in range(1,n):
+9            #overlap
+10            if intervals[i][0]<=res[-1][1]:
+11                res[-1][0]=min(intervals[i][0],res[-1][0])
+12                res[-1][1]=max(intervals[i][1],res[-1][1])
+13
+14            #non-overlap
+15            else:
+16                res.append(intervals[i])
+17        
+18        return res
+19
+20
