190459c5fe1f5f45a1f6199f3b49e25d31a3c787
Yan Zhao <zhao.y4@northeastern.edu>
2026-09-03 20:05:16 -0700

    Add solution for Majority Element

--- LeetCode/Majority-Element/Solution.py
+++ LeetCode/Majority-Element/Solution.py
@@ -1,15 +1,19 @@
 1class Solution:
 2    def majorityElement(self, nums: List[int]) -> int:
-3        count=1
-4        res=nums[0]
-5
-6        for num in nums[1:]:
-7            if count==0:
-8                res=num
-9
-10            if num==res:
+3        n=len(nums)
+4        if n==1:
+5            return nums[0]
+6        
+7        win=nums[0]
+8        count=1
+9        for num in nums[1:]:
+10            if num==win:
 11                count+=1
 12            else:
 13                count-=1
-14
-15        return res
+14                if count==0:
+15                    win=num
+16                    count=1
+17        
+18        return win
+19
