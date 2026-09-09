053aa3fa23799d6e16cd95cb6488816454cd7713
Yan Zhao <zhao.y4@northeastern.edu>
2026-09-09 15:29:00 -0700

    Add solution for Contains Duplicate

--- LeetCode/Contains-Duplicate/Solution.py
+++
@@ -1,8 +0,0 @@
-1class Solution:
-2    def containsDuplicate(self, nums: List[int]) -> bool:
-3        count=Counter(nums)
-4        for key in count:
-5            if count[key]>=2:
-6                return True
-7        
-8        return False
