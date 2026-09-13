8679aca063a5b50aa220d35b34ac6f0a0718a7f4
Yan Zhao <zhao.y4@northeastern.edu>
2026-09-12 18:37:21 -0700

    Add solution for Find All Anagrams in a String

--- LeetCode/Find-All-Anagrams-in-a-String/Solution.py
+++ LeetCode/Find-All-Anagrams-in-a-String/Solution.py
@@ -1,26 +1,25 @@
 1class Solution:
 2    def findAnagrams(self, s: str, p: str) -> List[int]:
-3        if len(s) < len(p):
-4            return []
-5
-6        target = [0] * 26
-7        window = [0] * 26
-8
-9        for c in p:
-10            target[ord(c) - ord('a')] += 1
-11
-12        res = []
-13        k = len(p)
-14
-15        for r in range(len(s)):
+3        m,n=len(s),len(p)
+4        if m<n:
+5            return []
+6        
+7        target=[0]*26
+8        window=[0]*26
+9
+10        for char in p:
+11            target[ord(char)-ord("a")]+=1
+12        
+13        res=[]
+14    
+15        for r in range(m):
 16            window[ord(s[r]) - ord('a')] += 1
 17
-18            # 窗口超过 p 长度，移除左边
-19            if r >= k:
-20                window[ord(s[r - k]) - ord('a')] -= 1
-21
-22            # 窗口长度达到 k
-23            if r >= k - 1 and window == target:
-24                res.append(r - k + 1)
-25
-26        return res
+18            if r >= n:
+19                window[ord(s[r - n]) - ord('a')] -= 1
+20
+21            # 窗口长度达到 k
+22            if r >= n - 1 and window == target:
+23                res.append(r - n + 1)
+24
+25        return res
