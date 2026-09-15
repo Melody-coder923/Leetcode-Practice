2b2990dfd18d8e8aa3b7938193dbd16fdc5f5d4e
Yan Zhao <zhao.y4@northeastern.edu>
2026-09-14 21:57:41 -0700

    Add solution for Min Stack

--- LeetCode/Min-Stack/Solution.py
+++ LeetCode/Min-Stack/Solution.py
@@ -1,35 +1,34 @@
 1class MinStack:
 2
 3    def __init__(self):
-4        self.min_stack=[]
-5        self.stack=[]
-6    
-7    def push(self, val: int) -> None:
-8        self.stack.append(val)
-9        if not self.min_stack or val <= self.min_stack[-1]:
-10            self.min_stack.append(val)
+4        self.stack=[]
+5        self.minstack=[]
+6
+7    def push(self, value: int) -> None:
+8        self.stack.append(value)
+9        if not self.minstack or value <= self.minstack[-1]:
+10            self.minstack.append(value)
 11        
-12    def pop(self) -> None:
-13        if not self.stack:
-14            return None
-15        val = self.stack.pop()
-16        if val == self.min_stack[-1]:
-17            self.min_stack.pop()
-18        
+12
+13    def pop(self) -> None:
+14        if self.stack:
+15            val=self.stack.pop()
+16            if self.minstack and val ==self.minstack[-1]:
+17                self.minstack.pop()
+18
 19    def top(self) -> int:
-20        if not self.stack:
-21            return None
-22        return self.stack[-1]
+20        if self.stack:
+21            return self.stack[-1]
+22        
 23
 24    def getMin(self) -> int:
-25        if not self.min_stack:
-26            return None
-27        return self.min_stack[-1]
+25        if self.minstack:
+26            return self.minstack[-1]
+27
 28
-29
-30# Your MinStack object will be instantiated and called as such:
-31# obj = MinStack()
-32# obj.push(val)
-33# obj.pop()
-34# param_3 = obj.top()
-35# param_4 = obj.getMin()
+29# Your MinStack object will be instantiated and called as such:
+30# obj = MinStack()
+31# obj.push(value)
+32# obj.pop()
+33# param_3 = obj.top()
+34# param_4 = obj.getMin()
