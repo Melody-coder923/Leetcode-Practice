ddbffb5b698bda7442c2f95a1fe9c3469b64398c
Yan Zhao <zhao.y4@northeastern.edu>
2026-09-16 21:53:30 -0700

    Add solution for IPO

--- LeetCode/IPO/Solution.py
+++ LeetCode/IPO/Solution.py
@@ -20,19 +20,20 @@
 20                    break
 21                i+=1
 22            
-23            # heap pop  count<k, change w
+23            # 如果当前capital no projec could invest :break
 24            if not heap:
 25                break
 26
-27            neg_profit=heapq.heappop(heap)
-28            w-=neg_profit
-29
-30            count+=1
-31            if count==k:
-32                break
-33        
-34        return w
-35
-36            
-37        
-38
+27            # heap pop  count<k, change w
+28            neg_profit=heapq.heappop(heap)
+29            w-=neg_profit
+30
+31            count+=1
+32            if count==k:
+33                break
+34        
+35        return w
+36
+37            
+38        
+39
